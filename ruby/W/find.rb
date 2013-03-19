d8cc65937785c3a4c124e9a4a41fd65f36406828
c <carmen@whats-your.name>
2013-03-19 08:52:54 +0000

    \

--- ruby/W/find.rb
+++ ruby/W/find.rb
@@ -1,22 +1,23 @@
-watch __FILE__
+#watch __FILE__
 class E
 
   fn 'set/find',->e,q,m{
     t=q['day'] && q['day'].match(/^\d+$/) && '-ctime -'+q['day']
     s=q['size'] && q['size'].match(/^\d+$/) && '-size +'+q['size']+'M'
     r=q['q'] && '-iregex ' + ('.*'+q['q']+'.*').sh
-    `find #{e.sh} #{t} #{s} #{r}`.lines.map{|p|p[Blen+1..-1].unpath}}
+    `find #{e.sh} #{t} #{s} #{r} | head -n 1024`.lines.map{|p|
+      p[Blen+1..-1].unpath}}
   
   fn 'graph/find',->e,q,m{
     (Fn 'set/find', e,q,m).do{|f|
-      if f.size < 255
+      if f.size < 256
         f.map{|r|r.fromStream m,:tripleSourceNode,false}
       else
         f.map{|r|m[r.uri]=r}
       end}}
 
   fn 'view/find',->i,e{
-    {_: :form, method: :GET, action: i.uri.t,
+    {_: :form, method: :GET, action: e['REQUEST_PATH'].t, style: 'float: right',
       c: [{_: :input, name: :graph, value: :find, type: :hidden},
           {_: :input, name: :view, value: :ls, type: :hidden},
           {_: :input, name: :q}]}}
