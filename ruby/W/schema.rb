362ea93aaa3450f6ef29f426da5bcaef91d698e4
c <carmen@whats-your.name>
2013-03-25 12:43:28 +0000

    \

--- ruby/W/schema.rb
+++ ruby/W/schema.rb
@@ -4,22 +4,20 @@ class E
   def E.cacheSchemas; c = {}    
     puts "parsing gromgull's data"
     '/predicates.2010'.E.read.each_line{|e| # usage data http://gromgull.net/2010/09/btc2010data/predicates.2010.gz
-      print "%d%% " % (c.size / 953.0) if c.size % 10000 == 0 # info
+      print "%d%% " % (c.size / 953.0) if c.size % 10000 == 0 # progress
       e.match(/(\d+)[^<]+<([^>]+)>/).       # occurrence count
       do{|r| c[r[2]] = r[1].to_i }}         # to hash-table
-      puts "\n#{c.size} properties"
-    'http://localhost/css/i/prefix.cc.txt'.E. # schema list
-      read.split("\n").grep(/^[^#]/).map{|c|  # uncommented lines
-      c.split(/\t/).do{|f| Hash['uri', f[1],  # parse prefix table
-                              'prefix',f[0]]}}.
-      map{|b| b.E.cacheTurtle.           # cache schema locally
-              ln "/prefix/"+b['prefix']} # prefix shortcut
-    s = E['/prefix/*'].glob      # docs
-    s.map{|s| s.size < 2e5 &&    # each schema
-      (s.indexFrag 'schema'      # index document
-       s.readlink.linkDefined)}  # link slash-URIs to defining doc
+      'http://localhost/css/i/prefix.cc.txt'.E. # schema list
+      read.split("\n").grep(/^[^#]/).map{|c|    # parse prefix table
+        s = c.split(/\t/)[1].E;      puts s     # schema
+        c = s.cacheTurtle                       # cache reference
+      }
+
+      s.map{|s| s.size < 2e5 &&    # each schema
+        (s.indexFrag 'schema'      # index document
+         s.readlink.linkDefined)}  # link slash-URIs to defining doc
       s.map{|r|m = {}; r.graph.map{|u,_|          # each predicate in schema
-       c[u] && m[u]={'uri'=>u,'/frequency'=>c[u]}}# annotate with frequency
+          c[u] && m[u]={'uri'=>u,'/frequency'=>c[u]}}# annotate with frequency
         r.appendNT m unless m.empty?}             # store in NTriples file
     end
     
@@ -61,7 +59,7 @@ class E
         m.map{|u,r|
           r[RDFs+'isDefinedBy'].do{|d|
             prop = (u[-1]=='/' ? u[0..-2] : u).E
-            prop.dirname.dir;  puts "d #{prop.dirname} #{prop}"
+            prop.dirname.dir
             ln prop }}}
     end
 
