d115818ab499a64d2671f6a39b807890dae62369
c <carmen@whats-your.name>
2013-04-23 06:40:31 +0000

    \

--- ruby/Es.rb
+++ ruby/Es.rb
@@ -119,24 +119,23 @@ end
 
 class Pathname
 
-  Infinity = 1.0/0
-  def take s=Infinity,v=:desc,o=nil# count, direction, offset
-    i = to_s.size # comparison-offset index
-    l = false # is element a leaf?
-    r=[] # result set
+  def take s=1000,v=:desc,o=nil # count, direction, offset
+    i = to_s.size # comparison offset-index
+    l = false     # in-range indicator
+    r=[]          # result set
     v,m={asc: [:id,:>=], desc: [:reverse,:<=]}[v] # asc/desc operator lookup
-    a=->n{s=s-1; r.push n} # add to result-set, decrement count
-    g=->b{b.sort_by(&:to_s).send(v).each{|n| # each child-element
-        return if 0 >= s # stop if count reaches 0
+    a=->n{s=s-1; r.push n}                        # add to result-set, decrement count
+    g=->b{b.sort_by(&:to_s).send(v).each{|n|      # each child-element
+        return if 0 >= s                          # stop if count reaches 0
         (l || !o || n.to_s[i..i+o.size-1].send(m,o[0..(n.to_s.size - i - 1)])) && # in range?
-        (if !(c=n.c).empty? # has children?
-           g.(c) # children
+        (if !(c=n.c).empty?  # has children?
+           g.(c)             # include children
          else
-           a.(n) # add resource
-           l = true unless l # leaf
+           a.(n)             # add resource
+           l = true unless l # iterator in range
         end)}}
     g.(c) # start 
-    r # results
+    r     # results
   end
 
 end
