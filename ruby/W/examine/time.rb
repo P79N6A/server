166e0ec3ff9b46ec19de1a97f5e6cd3e8dea22e8
c <carmen@whats-your.name>
2013-05-04 18:55:22 +0000

    \

--- ruby/W/examine/time.rb
+++ ruby/W/examine/time.rb
@@ -1,11 +1,11 @@
 require 'element/W/examine/history'
-#watch __FILE__
+watch __FILE__
 class E
 
   fn 'filter/tg',->e,m{
     m.values.group_by{|r|[*r[e['group']]][0]}.map{|g,v|
       p=nil
-      v.sort_by{|r|[*r[e['xaxis']||Date]][0].to_time}.map{|r|
+      v.sort_by{|r|[*r[e['x']||Date]][0].to_time}.map{|r|
         r['/parent']=[p]
         p=r}}}
 
@@ -15,22 +15,20 @@ class E
   
   fn 'view/tg/base',->d,e,c{ e[:graph]||=d
     g={} # groups
-    a=e.q['xaxis']||Date
+    a=e.q['x']||Date
     v=d.map{|_,r|r[a]}.flatten.compact.map(&:to_time).map &:to_f # time axis
     max = v.max
     min = v.min
     scale=100/((max - min).do{|v|v==0 ? 100 : v}||100) # scale time
-    o=d.map{|_,r|r[e.q['yaxis']]}.flatten.compact.map &:to_f # other axis
+    o=d.map{|_,r|r[e.q['y']]}.flatten.compact.map &:to_f # other axis
     omax = o.max; omin = o.min
     oscale=100/((omax - omin).do{|v|v==0 ? 100 : v}||100) # scale other
     d.map{|_,r| r[a] &&
       (t = e.q['group'].do{|g|[*r[g.expand]][0]}||[*r[Title]][0].sub(/^[rR][eE][^A-Za-z]./,'') # group id
        r['left']=[*r[a]][0].do{|v|(v.to_time.to_f-min)*scale}||0 # x position
-       r['top']=e.q['yaxis'].do{|a|[*r[a]][0].do{|v|(omax - v.to_f)*oscale}||0}|| rand(100) # y position
-       # group options
-       r['group'] = (g[t] ||=
-                 {c: '#%06x' % rand(16777216),
-                   a: r.url,
+       r['top']=e.q['y'].do{|a|[*r[a]][0].do{|v|(omax - v.to_f)*oscale}||0}|| rand(100) # y position
+       r['group'] = (g[t] ||= # group config
+                 {c: '#%06x' % rand(16777216), a: r.url,
                    t: r['top'], l: r['left']}))}
 
     [H.css('/css/time'),
@@ -41,7 +39,7 @@ class E
                 class: :gtitle,
                   style: "background-color: #{g[:c]};top:#{g[:t]/4.0}em;left:#{g[:l]}%"}}]}]}
 
-  fn 'view/tg/item',->r,x{ r[x.q['xaxis']||Date] &&
+  fn 'view/tg/item',->r,x{ r[x.q['x']||Date] &&
     (t = r['top'].to_s+'%'
      l = r['left'].to_s+'%'
      [r[x.q['arc'].expand||'/parent'].map{|e|
