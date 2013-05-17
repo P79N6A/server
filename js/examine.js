dc9c7ac9b10bc6338121556cac782c774b5fd0ee
c <carmen@whats-your.name>
2013-05-17 00:08:53 +0000

    \

--- js/examine.js
+++ js/examine.js
@@ -2,23 +2,28 @@ var e = function(){
     qa('.facet span.name').click(function(e){
 
 	// predicate-object tuple
-	var f = e.target
+	var t = e.target
+
 	// predicate
-	var a = f.parentNode.parentNode
-	var facet = a.attr('facet')
+	var a = t.parentNode.parentNode
+	var f = a.attr('facet')
+
+	console.log('facet',a.attr('title'),':',t.innerHTML)
 
 	// visual selection status
-	if(f.attr('on')){f.removeAttribute('on'); f.style.backgroundColor=''
-	            } else { f.attr('on','true'); f.style.backgroundColor='#0f0'}
+	if(t.attr('on')){t.removeAttribute('on'); t.style.backgroundColor=''
+	            } else { t.attr('on','true'); t.style.backgroundColor='#0f0'}
 
 	// construct selection rules
 	var s = [], on = a.querySelectorAll('.name[on=true]')
 	if(on.length > 0) {
-	    s.push('.'+facet+'{display:none}')
-	    on.map(function(){s.push('.'+facet+'.'+this.attr('title')+'{display:inline}')})}
+	    s.push('.'+f+'{display:none}')
+	    on.map(function(){
+		console.log('on',this.attr('title'))
+		s.push('.'+f+'.'+this.attr('title')+'{display:inline}')})}
 
 	// create selected-facet stylesheet
-	q('style.'+facet).remove()
-	q('body').append(el('style').attr('class',facet).txt(s.join('\n')))})};
+	q('style.'+f) && q('style.'+f).remove()
+	q('body').append(el('style').attr('class',f).txt(s.join('\n')))})};
 
 document.addEventListener("DOMContentLoaded", e, false);
