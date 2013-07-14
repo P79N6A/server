6ae37d938de865324a014d6e7b51e4698c534760
c <carmen@whats-your.name>
2013-07-14 23:47:19 +0000

    \

--- sh/news/boston.rb
+++ sh/news/boston.rb
@@ -1,8 +1,31 @@
 #!/usr/bin/env ruby
 require 'element/W'
+
 unless h = ARGV[0]
   puts "missing hostname arg"
   exit  
 end
 
-'/s/boston.u'.E.graph.keys.tail.map{|u|u.E.getFeed h}
+uris = '/s/boston.u'.E.graph.keys.tail.map &:E
+
+if ARGV[1]
+
+  puts "checking URIs"
+  r = uris.map{|u|
+    r = `curl -Is "#{u}"`.lines.to_a[0].match(/\d{3}/)[0].to_i
+    c = [r,u]
+    puts c.join ' '
+    c
+  }
+
+  puts "\n\n"
+
+  r.map{|c|
+    puts c unless c[0] == 200
+  }
+
+else
+
+  uris.map{|u|u.getFeed h}
+
+end
