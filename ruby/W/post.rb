ffda47f0c2a885d6ffd9e19b5fe8e552b7e17419
c <carmen@whats-your.name>
2013-07-18 23:03:35 +0000

    \

--- ruby/W/post.rb
+++ ruby/W/post.rb
@@ -1,11 +1,10 @@
 #watch __FILE__
 class E
 
-  F['view/'+SIOC+'Post']=->d,e{
-    [H.once(e,'post',H.css('/css/post')),
-     d.map{|u,r|
-       r[Content].do{|c|
-         [r.except(Content).html,
-          {_: :article, c: c}]}}]}
+  F['view/'+SIOC+'Post']=->g,r{
+    g.map{|u,r|
+      r[Content].do{|c|
+        [r.except(Content).html,
+         {_: :article, c: c}]}}}
 
 end
