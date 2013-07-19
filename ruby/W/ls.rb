49c0dc7766f8b79a1dfca8739530ae431436cdfa
c <carmen@whats-your.name>
2013-07-19 08:03:09 +0000

    \

--- ruby/W/ls.rb
+++ ruby/W/ls.rb
@@ -31,10 +31,10 @@ class E
 
     # item thumbnail / link
     a = -> i { e = i.E
-      [' ',{_: :a, href: l[e.uri],
+      {_: :a, href: l[e.uri],
         c: e.uri.match(/(gif|jpe?g|png)$/i) ? {_: :img, src: i.uri+'?233x233'} :
         e.uri.sub(/.*\//,'')
-      }]}
+      }}
 
     [(H.once e, 'dir', (H.css '/css/ls')),
      i.map{|u,r| r['fs:child'] ? # directory?
