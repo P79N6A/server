55df9192c5255a1a68a973ac6bb617b9a030e2e0
c <carmen@whats-your.name>
2013-07-23 07:56:25 +0000

    remove URI specialcase from table view

--- ruby/W/table.rb
+++ ruby/W/table.rb
@@ -1,4 +1,4 @@
-#watch __FILE__
+watch __FILE__
 class E
   
   # property selector toolbar
@@ -41,26 +41,24 @@ class E
   F['view/tab']=F['view/table']
 
   fn 'table',->es,q=nil{
-    ks = {}
-    es.map{|e|e.respond_to?(:keys) && e.keys.map{|k|ks[k]=true}}
-    keys = ks.keys - ['uri']
+    ks = {} # predicate table
+    es.map{|e|e.respond_to?(:keys) &&
+              e.keys.map{|k|ks[k]=true}}
+    keys = ks.keys
     keys.empty? ? (es.html false) :
     H({_: :table,:class => :tab,
         c: [{_: :tr,
-              c: [{_: :td},
-                  *keys.map{|k|
-                    {_: :td, class: :label,
-                      c: q ? {_: :a,
-                        href: q['REQUEST_PATH']+q.q.except('reverse').merge({sort: k}).merge(q.q.member?('reverse') ? {} : {'reverse'=>true}).qs,
-                        c: (Fn 'abbrURI',k)} : k}}]},
+              c: keys.map{|k|
+                {_: :td, class: :label,
+                  c: q ? {_: :a,
+                    href: q['REQUEST_PATH']+q.q.except('reverse').merge({sort: k}).merge(q.q.member?('reverse') ? {} : {'reverse'=>true}).qs,
+                    c: (Fn 'abbrURI',k)} : k}}},
             *es.map{|e|
               {_: :tr, about: e.uri, c:
-                [{_: :td, property: :uri,
-                   c: e['uri'].do{|e|e.E.html}},
-                 *keys.map{|k|
-                   {_: :td, property: k, c: e[k].do{|v|
-                       (v.class==Array ? v : [v]).map(&:html).join ' '}}}]}}]})}
-
+                keys.map{|k|
+                  {_: :td, property: k, c: e[k].do{|v|
+                      (v.class==Array ? v : [v]).map(&:html).join ' '}}}}}]})}
+  
   fn 'table/elements',->d{ m={}
     g='t:group'.expand
     p='t:period'.expand
