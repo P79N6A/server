d2539ca89dd35ecbb16b22caf2f785224a28f989
c <carmen@whats-your.name>
2013-08-01 12:10:59 +0000

    make triplrStdOut more readable

--- ruby/W/shell.rb
+++ ruby/W/shell.rb
@@ -7,12 +7,22 @@ class E
   end
 
   # util, prefix -> tripleStream
-  def triplrStdOut e,f='',g=nil,a=sh;g||=/^\s*(.*?)\s*$/
-    `#{e} #{a}|grep :`.each_line{|i|i=i.split /:/
-    yield uri,
-     (f+(i[0].match(g)||[nil,i[0]])[1].gsub(/\s/,'_')),
-      i.tail.join(':').strip.do{|v|v.match(/^[0-9\.]+$/) ? v.to_f : v}
-    };nil
+  def triplrStdOut e,f='/',g=nil,a=sh
+
+    # leading/trailing whitespace expression
+    g ||= /^\s*(.*?)\s*$/
+
+    # exec command
+    `#{e} #{a}|grep :`.each_line{|i|
+
+      # key/val separator
+      i = i.split /:/
+
+    yield uri, # subject
+     (f+(i[0].match(g)||[nil,i[0]])[1].gsub(/\s/,'_')), # predicate
+      i.tail.join(':').strip.do{|v|v.match(/^[0-9\.]+$/) ? v.to_f : v} # object
+    }
+    nil
   rescue
     nil
   end
