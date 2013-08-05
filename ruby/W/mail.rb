86a411ac7c5f9589fcdf429cca305847ff16cb1f
c <carmen@whats-your.name>
2013-08-05 13:32:42 +0000

    comment thread-overview view

--- ruby/W/mail.rb
+++ ruby/W/mail.rb
@@ -1,58 +1,32 @@
 # -*- coding: utf-8 -*-
 #watch __FILE__
-class String; def is_binary_data?; true; end; end
-module TMail
-  class Mail
-    def unicode_body
-      unquoted_body.to_utf8
-    end
-    def decentBody
-      unHTML=->t{t.split(/<body[^>]*>/)[-1].split(/<\/body>/)[0]}
-      if multipart?
-        parts.collect{ |part|
-          c = part["content-type"]
-          if part.multipart?
-            part.decentBody
-          elsif header.nil?
-            ""
-          elsif !attachment?(part) && c.sub_type != 'html'
-            part.unicode_body.hrefs(true)
-          else
-            (c["name"]||'attach').do{|a|
-              message_id ? (message_id[1..-2]+'/'+a).E.do{|i|
-                i.w part.body if !i.e
-                '<a href='+i.url+'>'+(part.main_type=='image' ? '<img src="'+i.url+'">' : a)+"</a><br>\n"
-              } : ""};end
-        }.join
 
-      else
-        unicode_body.do{|b|content_type&&content_type.match(/html/) ? unHTML.(b) : b.hrefs(true)}
-      end
-    rescue
-      ''
-    end 
-  end
-end
 class E
-  F["?"]||={};F["?"].update({
-   'thread'=>{'graph'=>'thread','sort' => 'dc:date','reverse' => nil,'view' => 'mail'},
-   'ann'=>{'view'=>'threads','match' => /[^a-zA-Z][Aa][Nn][nN]([oO][uU]|[^a-zA-Z])/,'matchP' => 'dc:title'}})
 
-  def triplrMail
-    require 'tmail'
+  # querystring aliases
+  F["?"] ||= {}; F["?"].update({
+   'thread'=>{'graph'=>'thread','sort' => 'dc:date','reverse' => nil,'view' => 'mail'},
+      'ann'=>{'view'=>'threads','match' => /[^a-zA-Z][Aa][Nn][nN]([oO][uU]|[^a-zA-Z])/,'matchP' => 'dc:title'}})
+
+  def triplrMail; require 'tmail'
+
+    # read message
     i = -> i {E i[1..-2]}                 # Message-ID -> E
     (TMail::Mail.load node).do{|m|        # parse
       d = m.message_id; return unless d   # parse successful?
       e = i[d]                            # Message resource
       e.e || (                            # Message-ID locatable?
-              ln e
-       %w{in_reply_to references}.map{|p| # message arcs
-          m.send(p).do{|o|                # index connections (fs)
-            o.map{|o| e.index SIOC+'reply_of', i[o] }}}
-               e.ln '/' + m.date.iso8601[0..12].gsub(/[^\d]/,'/') + '/' + e.uri # date/hour index
-                   self.index Creator, m.from[0].E # index From
-                   self.index      To, m.to[0].E   # index To
-               )
+
+       # index previously unseen mail
+       ln e                               # create message-id path 
+       %w{in_reply_to references}.map{|p| # reference arcs
+        m.send(p).do{|o| o.map{|o|        # lookup references
+         e.index SIOC+'reply_of', i[o]}}} # index references
+       e.ln '/' + m.date.iso8601[0..12].gsub(/[^\d]/,'/') + '/' + e.uri # date/hour index tree
+       self.index Creator, m.from[0].E    # index From
+       self.index      To, m.to[0].E )    # index To
+
+      # yield triples
       yield e.uri, Type,    E[SIOCt+'MailMessage']
       yield e.uri, Date,    m.date.iso8601    if m.date
       yield e.uri, Content, m.decentBody
@@ -67,37 +41,69 @@ class E
      [:in_reply_to,SIOC+'reply_of',true,true],
       [:references,SIOC+'reply_of',true,true],
         ].each{|a| m.send(a[0]).do{|o| [*o].map{|o|
-            yield e.uri,a[1],(a[2] ? (a[3] ? i[o] : o.E) : o.to_utf8) unless o.match(/\A[, \n]*\Z/)}}}}
+            yield e.uri,a[1],                        # skip empty String values 
+            (a[2] ? (a[3] ? i[o] : o.E) : o.to_utf8) unless o.match(/\A[, \n]*\Z/)}}}}
+
   rescue Exception => e
     puts [:mail,uri,e].join(' ')
   end
 
   fn 'graph/thread',->d,_,m{d.walk SIOC+'reply_of',m}
 
-  # an overview of messages in a resource set
-  fn 'view/threads',->d,env{g={}
+  # overview of a message set
+  fn 'view/threads',->d,env{
+
+    # occurrence-count statistics
+    g = {}
     d.map{|_,m|
       m[To].map{|t|
         g[t.uri]||=0
         g[t.uri]=g[t.uri].succ}}
+    
+    # CSS
     [(H.css '/css/mail'),'<table>',
-     d.values.group_by{|r|[*r[Title]][0].sub(/^[rR][eE][^A-Za-z]./,'')
-     }.group_by{|r,k|
+
+     # subgroup by title
+     d.values.group_by{|r|[*r[Title]][0].sub(/^[rR][eE][^A-Za-z]./,'')}.
+
+     # group by recipient
+     group_by{|r,k|
+
+       # show most-popular first
        k[0].do{|k|
-         k[To].do{|o|o.sort_by{|t|g[t.uri]}.reverse.head.uri}}
-     }.map{|e| c='#%06x' % rand(16777216)
+         k[To].do{|o|o.sort_by{|t|g[t.uri]}.reverse.head.uri}}}.
+
+     # display
+     map{|e|
+       # recipient-group color
+       c = '#%06x' % rand(16777216)
        ['<tr><td class=subject>',
+        
+        # show most-popular groups first
         e[1].sort_by{|m|m[1].size}.reverse.map{|t|
+
+          # link to thread
           [{_: :a, property: Title, :class => 'thread', style: "border-color:#{c}", href: t[1][0].url+'??=thread',
              c: t[0].to_s.gsub(/[<>]/,'_').gsub(/\[([a-z\-A-Z0-9]+)\]/,'<span class=g>\1</span>')},
+
+           # link to individual messages
            (t[1].size > 1 &&
-            ['<br>',t[1].map{|s|
-               [{_: :a, property: Creator,href: s.url,:class => 'sender', style: 'background-color:'+c,
+            ['<br>', t[1].map{|s|
+
+               # author name and RDFa
+               [{_: :a, property: Creator, href: s.url, :class => 'sender', style: 'background-color:'+c,
                   c: s[SIOC+'name'][0].split(/\W/,2)[0]},' ']}]),'<br>']},'</td>',
-        {_: :td, class: :group, property: To, c: {_: :a, :class => :to, style: 'background-color:'+c,
-          c: e[0] && e[0].split(/@/)[0], href: e[0] && e[0].E.url+'?,=sioc:addressed_to&view=page&v=threads'}},
+
+        # recipient group, Mailing List
+        {_: :td, class: :group, property: To,
+          c: {_: :a, :class => :to, style: 'background-color:'+c, c: e[0] && e[0].split(/@/)[0],
+            href: e[0] && e[0].E.url+'?,=sioc:addressed_to&view=page&v=threads'}},
+
         '</tr>']},'</table>',
+
+     # link to show full content of entire message-set
      {_: :a, :class => :show, c: :content,href: env['REQUEST_PATH']+env.q.except('v').update({'view' => 'mail'}).qs}]}
+
 
   # show a set of messages
   fn 'view/mail',->d,e{
@@ -123,15 +129,7 @@ class E
         
         {:class => :mail,
           
-          c: [# title
-              m[Title].do{|t|
-                # only show if changed from previous
-                title != t[0] && (
-                 title = t[0] # update title
-                 [{:class => :title, c: '███'+t.html, _: :a, href: m.url, style: "color: #{E.c}"},
-                  '<br clear=all>'])},
-
-              # link to self
+          c: [# link to self
               {_: :a, href: m.url, rel: :permalink, title: :link, c: ' '},
               
               # To:, From: index search links
@@ -180,11 +178,55 @@ class E
                        (l.size > 64 &&
                         {_: :a, class: :line, href: '#'+f,c: '↵'}),
 
-                     "\n" ]}}}}]}]}]} # collate
+                     "\n" ]}}}}, # collate lines
+
+              # title
+              m[Title].do{|t|
+                # only show if changed from previous
+                title != t[0] && (
+                 title = t[0] # update title
+                 [{:class => :title, c: t.html, _: :a, href: m.url, style: "color: #{E.c}"},
+                  '<br clear=all>'])}]}]}]}
   
-  # default view for these MIME and SIOC types
+  # set a default view for MIME and SIOC types
   [MIMEtype+'message/rfc822',
    SIOCt+'MailMessage'].
     map{|m| F['view/'+m] = F['view/mail'] }
 
 end
+
+module TMail
+  class Mail
+    def unicode_body
+      unquoted_body.to_utf8
+    end
+    def decentBody
+      unHTML=->t{t.split(/<body[^>]*>/)[-1].split(/<\/body>/)[0]}
+      if multipart?
+        parts.collect{ |part|
+          c = part["content-type"]
+          if part.multipart?
+            part.decentBody
+          elsif header.nil?
+            ""
+          elsif !attachment?(part) && c.sub_type != 'html'
+            part.unicode_body.hrefs(true)
+          else
+            (c["name"]||'attach').do{|a|
+              message_id ? (message_id[1..-2]+'/'+a).E.do{|i|
+                i.w part.body if !i.e
+                '<a href='+i.url+'>'+(part.main_type=='image' ? '<img src="'+i.url+'">' : a)+"</a><br>\n"
+              } : ""};end
+        }.join
+
+      else
+        unicode_body.do{|b|content_type&&content_type.match(/html/) ? unHTML.(b) : b.hrefs(true)}
+      end
+    rescue
+      ''
+    end 
+  end
+end
+
+# TMail calls this nonexistent method on strings
+class String; def is_binary_data?; true; end; end
