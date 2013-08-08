eb8504319caa5089718e79fad2c938aabf894488
c <carmen@whats-your.name>
2013-08-08 23:54:18 +0000

    update SERVER_NAME for use in other functins

--- ruby/Th.rb
+++ ruby/Th.rb
@@ -91,17 +91,17 @@ class E
   def E.call e
     dev         # check for changed source code
     e.extend Th # request-related utility functions
-    host = e['HTTP_X_FORWARDED_HOST'] || e['SERVER_NAME']    # hostname
+    e['HTTP_X_FORWARDED_HOST'].do{|h| e['SERVER_NAME'] = h } # hostname
    (e['REQUEST_PATH'].force_encoding('UTF-8').do{|u|         # path
       CGI.unescape(u.index(Prefix)==0 ? u[Prefix.size..-1] : # non-local or non-HTTP URI
-      'http://' + host + u.gsub('+','%2B'))                  # HTTP URI
+      'http://' + e['SERVER_NAME'] + u.gsub('+','%2B'))      # HTTP URI
     }.E.env(e).jail.do{|r|           # valid path?
       r.send e.fn                    # continue
     } || [403,{},['invalid path']]). # reject
       do{|response|        # inspect response
         puts [e.fn,        # method
               response[0], # response code
-              ['http://', host, e['REQUEST_URI']].join,# URL
+              ['http://', e['SERVER_NAME'], e['REQUEST_URI']].join,# URL
               e['HTTP_USER_AGENT'],
               e['HTTP_REFERER'],
              ].join ' '
