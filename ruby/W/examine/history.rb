21cee3fcf8f747aa32d29dde21bdd1a9c58f616e
cdr <carmen@whats-your.name>
2013-09-10 19:50:10 +0000

    http://m/moz/places.sqlite?graph=|&|=triplrMozHist&view=h&hv=title&a=dc:date&bins=96&c=960&q=reddit&t=2013-09-08

--- ruby/W/examine/history.rb
+++ ruby/W/examine/history.rb
@@ -4,8 +4,8 @@ class E
 
   def triplrMozHist
     c = @r.q['c'].match(/[0-9]+/) ? @r.q['c'] : '18'
-    q = @r.q['q'].do{|m|m.match(/[a-zA-Z\-_\.\/]+/) &&
-        "and p.url like '#{@r.q.has_key?('full')?'':'%'}#{m}%'"}
+    q = @r.q['q'].do{|m|m.match(/^[a-zA-Z0-9\-_\.\/]+$/) &&
+        "and p.url like '%#{m}%'"}
     t = @r.q['t'].do{|t| "and h.visit_date < #{Time.parse(t).to_f*1e6}"}
 
     query = "select p.url, pr.url, h.visit_date, hr.visit_date from moz_places as p, moz_places as pr, moz_historyvisits as h, moz_historyvisits as hr where p.id = h.place_id and pr.id = hr.place_id and hr.id = h.from_visit #{q} #{t} order by h.visit_date desc limit #{c}"
