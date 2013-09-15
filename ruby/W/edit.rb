0232f42ad4dc3b364f5c1f4e4a9977e2c026275a
_ <carmen@whats-your.name>
2013-09-15 05:23:34 +0000

    enter the YAGNI j

--- ruby/W/edit.rb
+++ ruby/W/edit.rb
@@ -3,12 +3,14 @@ class E
 
   fn 'view/edit',->g,e{
     [(H.once e, 'edit', (H.css '/css/edit')),
-     g.map{|u,r|
+     g.map{|uri,s|
        {class: :resource,
-         id: u,
-         c: [{_: :a, class: :uri, c: u, href: r.url}]}}]}
+         c: [{_: :a, class: :uri, id: uri, c: uri, href: s.url},
+             s.map{|p,o|
+              {property: p, c: p}
+             }]}}]}
 
-  fn 'view/editor',->g,env{
+  fn 'view/edit/html',->g,env{
      {_: :form, name: :editor, c: 'edit'}
   }
 
