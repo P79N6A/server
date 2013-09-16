3453ee9f9db131949e5172360207d51f9cbc9ff1
_ <carmen@whats-your.name>
2013-09-16 02:13:17 +0000

    clean up 404

--- ruby/Th/404.rb
+++ ruby/Th/404.rb
@@ -8,26 +8,24 @@ class E
     u = e.uri     # response URI
     g = {u => {}} # response graph
     s = g[u]      # resource pointer
-    # add request data to response graph
+    # request environment vars to response graph
     r.map{|k,v| s[k] = [v] }
     s[Type] = [E[HTTP+'404']]
     s['uri'] = u
     s['QUERY'] = [r.q]
     s['ACCEPT']= [r.accept]
-    s['SERVER_SOFTWARE']=[('//'+r['SERVER_NAME']).E]
     s['http://buzzword.org.uk/rdf/personal-link-types#edit']=[E[u+'?view=edit&graph=_']]
     %w{CHARSET LANGUAGE ENCODING}.map{|a|s['ACCEPT_'+a] = [(r.accept_ '_' + a)]}
     # output
-    r.q.delete 'view' # use 404 view if HTML
+    r.q.delete 'view'
     [404,{'Content-Type'=> r.format},[e.render(r.format,g,r)]]}
 
-  # qs y=404 to force a 404 response
   F['req/404'] = F[E404]
 
   fn 'view/'+HTTP+'404',->d,e{
     [H.css('/css/404'),{_: :h1, c: '404'},d.html]}
 
-  # 404.css if fs content is missing
+  # 404.css fallback
   fn '/css/404.css/GET',->e,r{
     [200,{'Content-Type'=>'text/css'},
 ["body {background-color:#000;color:#fff; font-family: sans-serif}
@@ -36,20 +34,5 @@ td.key {text-align:right}
 td.key .frag {font-weight:bold;background-color:#0f0;color:#000;padding-left:.2em;border-radius:.38em 0 0 .38em}
 td.key .abbr {color:#eee;font-size:.92em}
 td.val {border-style:dotted;border-width:0 0 .1em 0;border-color:#00f;}"]]}
-
-  # show response-codes for a list of URIs
-  def checkURIs
-    r = uris.select{|u|u.to_s.match /^http/}.map{|u|
-      c = [`curl -IsA 404? "#{u}"`.lines.to_a[0].match(/\d{3}/)[0].to_i,u] # HEAD
-     #c = [`curl -s -o /dev/null -w %{http_code} "#{u}"`.chomp.to_i,u] # GET
-      puts c.join ' ' 
-      c # status, uri tuple
-    }
-    puts "\n\n"
-    r.map{|c|
-      # show anomalies
-      puts c.join(' ') unless c[0] == 200
-    }
-  end
 
 end
--- ruby/Th/local.rb
+++ ruby/Th/local.rb
@@ -19,4 +19,19 @@ class E
   fn '/ps/GET',->e,r{
     [200,{'Content-Type'=>'text/plain'},[`ps aux`]]}
 
+  # show response-codes for a list of URIs (.u)
+  def checkURIs
+    r = uris.select{|u|u.to_s.match /^http/}.map{|u|
+      c = [`curl -IsA 404? "#{u}"`.lines.to_a[0].match(/\d{3}/)[0].to_i,u] # HEAD
+     #c = [`curl -s -o /dev/null -w %{http_code} "#{u}"`.chomp.to_i,u] # GET
+      puts c.join ' ' 
+      c # status, uri tuple
+    }
+    puts "\n\n"
+    r.map{|c|
+      # show anomalies
+      puts c.join(' ') unless c[0] == 200
+    }
+  end
+
 end
