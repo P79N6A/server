523bae7e267fb763817973d0bc5a72b1333501b7
_ <carmen@whats-your.name>
2013-09-18 07:03:12 +0000

    HTMLform

--- ruby/W/edit.rb
+++ ruby/W/edit.rb
@@ -3,7 +3,7 @@ class E
 
   # the editable graph on FS triplestore
   fn 'graph/editable',->resource,env,graph{
-    # stub graph so request reaches edit-view even if resource is empty
+    # minimum graph so request reaches edit-view even if resource is empty
     Fn 'graph/_',resource,env,graph
     # current resource state
     resource.fromStream graph, :triplrFsStore}
@@ -11,16 +11,15 @@ class E
   fn 'view/edit',->g,e{
     puts "edit #{g.keys}"
     [(H.once e, 'edit', (H.css '/css/edit')),
-     g.map{|uri,s|
+     g.map{|uri,s| uri &&
        {class: :resource,
          c: [{_: :a, class: :uri, id: uri, c: uri, href: s.url},
              s.map{|p,o|
               {class: :property,
                  c: [{_: :a, class: :uri, c: p, href: p},
-                     {_: :span, class: :edit, c: :edit},
+                     {_: :a, class: :edit, c: :edit, href: e['REQUEST_PATH']+'?graph=editable&filter=p&view=edit/form&p=uri,'+CGI.escape(p)},
                      (case p
                       when 'uri'
-                        #                       uri
                       when Content
                         {_: :pre, c: o}
                       else
@@ -28,8 +27,13 @@ class E
                       end
                       )]}}]}}]}
 
-  fn 'view/edit/html',->g,env{
-     {_: :form, name: :editor, c: 'edit'}
-  }
+  fn 'view/edit/form',->g,env{
+     {_: :form,
+      name: :editor,
+      c: g.map{|uri,s|
+        s.map{|p,os|
+          os.map{|o|
+            ['<br>',p,'<br>',
+             {_: :input, name: p, value: o}]}}}}}
 
 end
