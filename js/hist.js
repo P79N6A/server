48b0c1e7409652c8876e1f2f0b28e07952dfeb98
_ <carmen@whats-your.name>
2013-10-05 08:55:32 +0000

    less drastic current bin styles

--- js/hist.js
+++ js/hist.js
@@ -7,7 +7,7 @@ var histogram = function(){
 	             window.scrollByLines(-5)}
 	var s = document.querySelector('.histogram style')
 	 if (s) {s.remove() }
-	document.querySelector('.histogram').append(el('style').txt('.histBin.'+b+'{background-color:#0f0;color:#000}'))
+	document.querySelector('.histogram').append(el('style').txt('.histBin.'+b+'{border-color:#0f0;border-width:.5em;border-style:solid;}'))
     }
 
     var bins = document.querySelectorAll('table.histogram td');
