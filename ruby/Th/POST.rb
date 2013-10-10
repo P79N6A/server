9af040bb3280510dc4a5bcf6a8027873994f60d8
_ <carmen@whats-your.name>
2013-10-10 18:27:05 +0000

    peel

--- ruby/Th/POST.rb
+++ ruby/Th/POST.rb
@@ -5,16 +5,28 @@ class E
     r = Rack::Request.new @r
     p = nil
     r.params.map{|k,v|
-      s,p,o = (CGI.unescape k).split(S).map &:unpath; p = p.uri[0..-2].E
-      puts "POST  s #{s} p #{p} o #{o.class} #{o} v #{v.class} #{v}"
+
+      # path-ized Triple components
+      sP,pP,oP = (CGI.unescape k).split S
+      # original triple
+      s,p,o = [sP,pP,oP].map{|c|c.unpath true}
+      p = p.uri[0..-2].E if p.uri[-1] == '/'
+      
+      puts "s.#{s} p.#{p} :"
+      puts "object  #{oP} #{o}"
+      puts "objectV #{v}"
+
+      # edit triple
       s[p] = v
+
+      
     }
-    @r.q.update({
-                  'view' => 'editPO',
+
+    # parameters for editor
+    @r.q.update({ 'view' => 'editPO',
                   'graph' => 'editable',
                   'p' => p.uri,
-                  'nocache' => 'true', 
-                })
+                  'nocache' => 'true'})
     self.GET
   rescue Exception => x
     Fn 'backtrace',x,@r
