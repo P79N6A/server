9d6e8239f6402e0e2205239fc60d4d87c263a6c1
_ <carmen@whats-your.name>
2013-10-15 23:29:55 +0000

    o

--- ruby/Th/500.rb
+++ ruby/Th/500.rb
@@ -3,11 +3,11 @@ class E
 
   fn 'backtrace',->x,r{
   [500,{'Content-Type'=>'text/html'},
-   ['<html><head><title>500</title></head><body style="margin:0;font-family: sans-serif;background-color:#000;color:#fff"><h1 style="float:left;height:97%;padding:.2em;margin-right:.5em;background-color:#f00;color:#fbb1;">500</h1><b style="background-color:#fff;color:#000;padding:.1em .25em .1em .25em">' + x.class.to_s,'</b> ' +
+   ['<html><head><title>500</title><style>div {display:inline}'+"\n"+'.frag {background-color:'+E.cs+';color:#000;font-weight:bold}'+"\n"+'.abbr {color:#000;background-color:#fff}</style></head><body style="margin:0;font-family: sans-serif;background-color:#000;color:#fff"><h1 style="padding:.2em;background-color:#f00;color:#000;margin:0">500</h1><b style="background-color:#fff;color:#000;padding:.1em .25em .1em .25em;">' + x.class.to_s,'</b> ' +
     x.message + '<br><table>' +
     x.backtrace.map{|l|
       l.split(/:/).do{|p|
-        ['<tr><td style="text-align:right">',p[0],'</td><td>',p[1],'</td><td style="color:#000;background-color:#fff;padding:0 0 0 .33em">',p[2].sub(/^in ./,''),'</td></tr>'].join
+        ['<tr><td style="text-align:right">',F['abbrURI'][p[0]],'</td><td style="text-align:right">',p[1],'&nbsp;</td><td style="color:#000;background-color:#fff;padding:0 0 0 .33em">',p[2].sub(/^in ./,''),'</td></tr>'].join
       }
     }.join +
      '</table></body></html>']]}
