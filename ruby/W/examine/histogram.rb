1c5355654eca5b9f8d149d13ce1ad57da664aa59
_ <carmen@whats-your.name>
2013-10-17 14:10:52 +0000

    default to DC Date if no attribute supplied to histogram

--- ruby/W/examine/histogram.rb
+++ ruby/W/examine/histogram.rb
@@ -1,35 +1,34 @@
-watch __FILE__
+#watch __FILE__
 class E
 
   fn 'view/histogram',->d,e{
 
     # a :: attribute to chart
-    a = e.q['a'].do{|e|e.expand}
+    a = e.q['a'].do{|e|e.expand} || Date
 
-    !a && '<b>a</b>ttribute required' ||
-    (# bins :: number of buckets
-     n = e.q['bins'].do{|b| b.to_f.max(999.0).min(1)} || 64.0
-
-     # hv :: bin template 
-     v = F['view/'+(e&&e.q['hv']||'tab')]
-
-     # construct histogram bins
-     (Fn 'view/histogram/bins',d,a,n).do{|h,m|
-
-       [H.css('/css/hist'),%w{mu hist}.map{|s|H.js('/js/'+s)},
-        (Fn 'view/histogram/render',h),{style: "width: 100%; height: 5em"},
-        h.map{|b,r|
-          # skip empty bins
-          r.empty? ? ' ' :
-          (x = m[:min] + m[:bw] * b
-           from = a == Date ? Time.at(x).to_s : x.to_s
-           to = a == Date ? Time.at(x + m[:bw]).to_s : (x + m[:bw]).to_s
-           # wrap bin
-           { class: 'histBin b'+b.to_s,
-             c: [# label bin
-                 {_: :h3, c: from + ' &rarr; ' + to },
-                 # bin children view
-                 v.(r,e)]})}]})}
+    # bins :: number of buckets
+    n = e.q['bins'].do{|b| b.to_f.max(999.0).min(1)} || 64.0
+    
+    # hv :: bin template 
+    v = F['view/'+(e&&e.q['hv']||'title')]
+    
+    # construct histogram bins
+    (Fn 'view/histogram/bins',d,a,n).do{|h,m|
+      
+      [H.css('/css/hist'),%w{mu hist}.map{|s|H.js('/js/'+s)},
+       (Fn 'view/histogram/render',h),{style: "width: 100%; height: 5em"},
+       h.map{|b,r|
+         # skip empty bins
+         r.empty? ? ' ' :
+         (x = m[:min] + m[:bw] * b
+          from = a == Date ? Time.at(x).to_s : x.to_s
+          to = a == Date ? Time.at(x + m[:bw]).to_s : (x + m[:bw]).to_s
+          # wrap bin
+          { class: 'histBin b'+b.to_s,
+            c: [# label bin
+                {_: :h3, c: from + ' &rarr; ' + to },
+                # bin children view
+                v.(r,e)]})}]}}
 
   F['view/h']=F['view/histogram']
 
