1b85cd1cfcd00d7d3d548c00fb5b8b14ec5525f5
_ <carmen@whats-your.name>
2013-10-25 17:07:28 +0000

    du

--- ruby/W/examine/protovis.rb
+++ ruby/W/examine/protovis.rb
@@ -26,5 +26,5 @@ class E
     [H.js('/js/protovis/protovis-r3.2'),{id: :fig},
      {_: :script,type: 'text/javascript+protovis',
        c: ['var d='+(Fn e.q['protovis.data'],d,e).to_json,
-           E['http://'+e['SERVER_NAME']+'/js/protovis/'+e.q['protovis.view']+'.js'].r].cr}]}
+           E['/js/protovis/'+e.q['protovis.view']+'.js'].r].cr}]}
 end
