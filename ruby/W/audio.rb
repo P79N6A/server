dc68ad0d35d46b6eec53f14a470580990f8cf349
_ <carmen@whats-your.name>
2013-10-26 21:47:46 +0000

    ok

--- ruby/W/audio.rb
+++ ruby/W/audio.rb
@@ -1,8 +1,4 @@
-watch __FILE__
 class E
-
-  F["?"]||={}; F["?"].update({'a'=>{'set' => 'audio','view' => 'audio',triplr: :id},
-                              'v'=>{'set' => 'video','view' => 'audio',triplr: :id, 'video'=>true}})
 
   def audioNodes;take.select &:audioNode end;def audioNode;true if ext.match /(aif|wav|flac|mp3|m4a|aac|ogg)/i end
   def videoNodes;take.select &:videoNode end;def videoNode;true if ext.match /(avi|flv|mkv|mpg|mp4|wmv)/i end
@@ -12,35 +8,22 @@ class E
 
   AudioInfo = %w{Album-Movie-Show_title Lead_performers-Soloists Title-songname-content_description}.map{|a|Audio+a}.concat [Stat+'mtime',Stat+'size']
 
-  fn 'set/findaudio',->e,q,m{
-    F['set/find'][e,q,m,'\(aif\|flac\|m4a\|mp3\|aac\|ogg\|wav\)']}
-
-  fn 'view/audio/item',->m,e{
-    {_: :tr,
-      c: [{_: :td, c: {_: :a, class: :entry, href: '#'+m.uri.gsub('%','%25').gsub('#','%23'), c: m.E.bare}},
-          AudioInfo.map{|k|{_: :td, c: m[k].html}},
-         ]}}
-
-  fn 'view/audio/base',->d,e,c=nil{
-    [(H.once e,:mu,(H.js '/js/mu')),
-     (H.once e,:audio,
-      {_: :span, id: :jump,c: '&#x10c2;&nbsp;&nbsp;&nbsp;'},
-      {_: :span, id: :rand,r: :true,c: :r},
-      {id: :data},
-      (H.js '/js/audio'),
-      (H.css '/css/audio'),
-      (H.css '/css/table'),
-      {_: e.q.has_key?('video') ? :video : :audio, id: :player, controls: true}),
-     {_: :table, class: :playlist,
-       c: [{_: :tr,
-             c: [{_: :td, c: :id},
-                 AudioInfo.map{|k|
-                   {_: :td, c: k}}]},c.()]}]}
-
   fn 'view/audio',->d,e{
     i = F['view/audio/item']
     Fn 'view/audio/base',d,e,->{
       d.sort_by{|_,r|r[Stat+'mtime'][0].to_s}.reverse.map{|_,r|i.(r,e)}}}
+
+  fn 'view/audio/base',->d,e,c=nil{
+    [(H.once e,:mu,(H.js '/js/mu')),
+     (H.once e,:audio,(H.js '/js/audio'),(H.css '/css/audio'),(H.css '/css/table'),
+      {_: :span, id: :rand,r: :true,c: :r},{_: :span, id: :jump,c: '&rarr;'},
+      {_: e.q.has_key?('video') ? :video : :audio, id: :player, controls: true}),
+     {_: :table, class: :playlist, c: c.()}]}
+
+  fn 'view/audio/item',->m,e{
+    {_: :tr,
+      c: [{_: :td, c: {_: :a, class: :entry, href: '#'+m.uri.gsub('%','%25').gsub('#','%23'), c: m.E.bare}},
+          AudioInfo.map{|k|{_: :td, c: m[k].html}}]}}
 
   def audioScan
     a('.png').e ||
