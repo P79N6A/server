fc7b01936cc8ceea90244767a2fa4d79275721a2
_ <carmen@whats-your.name>
2013-10-27 03:24:30 +0000

    ok

--- ruby/W/audio.rb
+++ ruby/W/audio.rb
@@ -1,26 +1,23 @@
 class E
 
-  AudioInfo = %w{Album-Movie-Show_title Lead_performers-Soloists Title-songname-content_description}.map{|a|Audio+a}.concat [Stat+'mtime',Stat+'size']
-
   fn 'set/audio',->d,e,m{d.take.select{|e|e.ext.match /(aif|wav|flac|mp3|m4a|aac|ogg)/i}}
   fn 'set/video',->d,e,m{d.take.select{|e|e.ext.match /(avi|flv|mkv|mpg|mp4|wmv)/i}}
 
   fn 'view/audio',->d,e{
-    i = F['view/audio/item']
     Fn 'view/audio/base',d,e,->{
       d.sort_by{|_,r|r[Stat+'mtime'][0].to_s}.
-      reverse.map{|_,r|i.(r,e)}}}
+      reverse.map{|_,r|F['view/audio/item'][r,e]}}}
 
   fn 'view/audio/base',->d,e,c=nil{
     [(H.once e,:mu,(H.js '/js/mu')),
      (H.once e,:audio,(H.js '/js/audio'),(H.css '/css/audio'),(H.css '/css/table'),
       {_: :span, id: :rand,r: :true,c: :r},{_: :span, id: :jump,c: '&rarr;'},
-      {_: e.q.has_key?('video') ? :video : :audio, id: :player, controls: true}),
+      {_: e.q.has_key?('video') ? :video : :audio, id: :player, controls: true}),'<br clear=all>',
      {_: :table, class: :playlist, c: c.()}]}
 
   fn 'view/audio/item',->m,e{
-    {_: :tr,
-      c: [{_: :td, c: {_: :a, class: :entry, href: '#'+m.uri.gsub('%','%25').gsub('#','%23'), c: m.E.bare}},
-          AudioInfo.map{|k|{_: :td, c: m[k].html}}]}}
+    {_: :tr, c: [{_: :td, c: {_: :a, class: :entry, href: '#'+m.uri.gsub('%','%25').gsub('#','%23'), c: m.E.bare}},
+          %w{Album-Movie-Show_title Lead_performers-Soloists Title-songname-content_description}.map{|a|Audio+a}.
+          concat([Stat+'mtime',Stat+'size']).map{|k|{_: :td, c: m[k].html}}]}}
 
 end
