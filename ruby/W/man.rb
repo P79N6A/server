e6c798318b168a3ad46cee1aa37592d6cce1bf4e
_ <carmen@whats-your.name>
2013-10-29 01:10:37 +0000

    ok

--- ruby/W/man.rb
+++ ruby/W/man.rb
@@ -8,9 +8,12 @@ class E
   end
 
   fn '/man/GET',->e,r{
-    puts "Maaan"
-    [200,{},[]]
-  }
+    e.pathSegment.uri.match(/\/man\/(.*)/).do{|m|
+      m = Shellwords.escape m[1]
+      mp = `man -w #{m}`.chomp
+      `zcat #{mp} | groff -T html -man`.hR
+    } || F[E404][e,r]}
+
 
 
 end
