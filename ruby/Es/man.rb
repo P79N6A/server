845b9bfa072ff32303e085aea2a0b0b1559bb510
carmen <_@whats-your.name>
2013-11-04 21:54:08 +0000

    ok

--- ruby/Es/man.rb
+++ ruby/Es/man.rb
@@ -15,8 +15,13 @@ class E
       m.match(/^([0-9])\/(.*)/).do{|p|
         s, m = p[1], p[2] }
       
+      locale = r.q['lang'] || r['HTTP_ACCEPT_LANGUAGE'].do{|a| a.split(/,/)[0] }
+      localization = locale.do{|l| '-L ' + l }
+      lang = locale.do{|l| "?lang=" + l }
+      
       # source
-      man = `man -w #{s} #{Shellwords.escape m}`.chomp
+      puts "man #{localization} -w #{s} #{Shellwords.escape m}"
+      man = `man #{localization} -w #{s} #{Shellwords.escape m}`.chomp
       unless man.empty?
 
         roff = man.E
@@ -26,12 +31,11 @@ class E
         cached = false
         unless cached
           puts " #{man} -> #{html}"
-          locales = Pathname('/usr/share/man').c.select{|p|
-            !p.to_s.match /man[^\/]+$/
-          }.map{|p|File.basename p}
 
-          # base HTML from groff
-          page = `zcat #{man} | groff -T html -man -P -D -P /dev/null`
+          locales = Pathname('/usr/share/man').c.select{|p|p.basename.to_s.do{|b| !b.match(/^man/) && !b.match(/\./) }}.map{|p|File.basename p}
+
+          # basic HTML from groff
+          page = `zcat #{man} | groff -T html -man -P -D -P /dev/null`.to_utf8
           page = Nokogiri::HTML.parse page
           body = page.css('body')[0]
           
@@ -40,20 +44,23 @@ class E
           body.add_child H[{_: :style, c: "a {background-color:#{E.cs}}"}]
           
           # add localization links
-          body.css('h1')[0].add_previous_sibling H locales.map{|l|{_: :a, class: :lang, href: r['REQUEST_PATH']+'?lang='+l, c: l}}
+          (body.css('h1')[0] ||
+           body.css('p')[0]
+           ).add_previous_sibling H locales.map{|l|
+            {_: :a, class: :lang, href: r['REQUEST_PATH']+'?lang='+l, c: l}}
           
-          # markup plaintext commands in SEE ALSO
+          # markup commands in SEE ALSO
           page.css('a[name="SEE ALSO"]')[0].do{|a|
             also = a.parent.next_element
             also.inner_html = also.text.gsub /\b([^<>\s(]+)\(/mi, '<b>\1</b>('}
           
-          # convert commands to links
+          # href-ize commands
           page.css('b').map{|b|
             b.next.do{|n|
               n.to_s.match(/\(([0-9])\)(.*)/).do{|section|
                 name, s = b.inner_text, section[1]
                 n.replace section[2]
-                b.replace " <a href='/man/#{s}/#{name}'><b>#{name}</b>(#{s})</a>"}}}
+                b.replace " <a href='/man/#{s}/#{name}#{lang}'><b>#{name}</b>(#{s})</a>"}}}
 
           html.w page
         end
