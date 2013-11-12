ea0012a00413174f229cbe9b1cd46699e4f7403d
carmen <_@whats-your.name>
2013-11-12 09:03:13 +0000

    r

--- ruby/H/audio.rb
+++ ruby/H/audio.rb
@@ -4,22 +4,21 @@ class E
   fn 'set/audio',->d,e,m{d.take.select{|e|e.ext.match AudioFiles}}
   fn 'set/video',->d,e,m{d.take.select{|e|e.ext.match VideoFiles}}
 
-  AudioP = {}
+  AudioInfo = {}
   %w{Album-Movie-Show_title Lead_performers-Soloists Title-songname-content_description}.map{|a|Audio + a}.concat(['uri',Stat+'mtime', Stat+'size']).map{|p|
-    AudioP[p] = true}
+    AudioInfo[p] = true}
 
-puts AudioP
   fn 'view/audio',->d,e{
-    d.values.map{|r|
-      r.delete_if{|p,o|
-        !AudioP[p]}}
-    puts d
+
+    # trim rare ID3-tags so table is reasonably-sized
+    d.values.map{|r| r.delete_if{|p,o| !AudioInfo[p]}}
+
     [(H.once e, :mu, (H.js '/js/mu')),
      (H.once e, :audio, (H.js '/js/audio'), %w{audio table}.map{|c|(H.css '/css/'+c)},
       {id: :rand, c: :r},
       {id: :jump, c: '&rarr;'},
       {id: :info},
-      {_: e.q.has_key?('video') ? :video : :audio, id: :media, controls: true}),
+      {_: e.q.has_key?('video') ? :video : :audio, id: :media, controls: true}),'<br>',
      F['view/table'][d,e]]}  
 
 end
