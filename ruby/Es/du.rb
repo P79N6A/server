6cc521904f59ca4ddcd4e203106b336b212ec699
carmen <_@whats-your.name>
2013-11-12 04:58:43 +0000

    remove cruft from du.rb

--- ruby/Es/du.rb
+++ ruby/Es/du.rb
@@ -1,13 +1,4 @@
-#watch __FILE__
 class E
-
-  F["?"]||={}
-  F["?"].update({'du'=>{
-                   'graph' => 'du',
-                    'view' => 'protovis',
-                    'protovis.data' => 'protovis/du',
-                    'protovis.view' => 'starburst'
-                  }})
 
   fn 'protograph/du',->d,_,m{
     e = [d,d.pathSegment].compact.find &:e
@@ -15,21 +6,11 @@ class E
     rand.to_s.h }
 
   fn 'graph/du',->e,_,m{
-    `du -a #{m.values[0].sh}`.lines.to_a[0..-2].map{|p|
-      s,p = p.chomp.split /\t/ # size, path
-      p = p.unpathURI    # path -> URI
-      m[p.uri]={'uri'=>p.uri,'size'=>s.to_i}}
+    `du -a #{m.values[0].sh}`.each_line{|l|
+      s,p = l.chomp.split /\t/ # size, path
+      p = p.unpathURI          # path -> URI
+      m[p.uri] = {'uri' => p.uri,
+            Stat+'size' => [s.to_i]}}
     m }
-
-  fn 'protovis/du',->e,c{
-    m={}        # model
-    e.map{|u,r| # each resource
-      s = u.sub(/http:..[^\/]+./,'').split '/' # split path
-      p = m            # pointer
-      s[0..-2].map{|s| # each path segment
-        p[s] = {} if !p[s] || p[s].class != Hash # create branch
-        p = p[s]}      # advance pointer down tree
-      p[s[-1]]||=r['size']} # append size to leaf
-    m}
 
 end
