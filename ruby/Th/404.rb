f0c3366a503923ac99229734133d367d50071161
carmen <_@whats-your.name>
2013-11-22 13:31:51 +0000

    tidy 404

--- ruby/Th/404.rb
+++ ruby/Th/404.rb
@@ -7,27 +7,21 @@ class E
    id = e.uri     # response URI
     g = {id=>{}}  # response graph
     s = g[id]     # resource pointer
+   fn = r['REQUEST_METHOD']
 
-    # link request-environment data
-    fn = r['REQUEST_METHOD']
-    r.map{|k,v| 
-      s[Header + k] = k == 'uri' ? v : [v] }
-    s[Type] = [E[HTTP+'Response']]
-    s[HTTP+'statusCodeValue']=[404]
-    s['#query'] = [r.q]
-    s[Header+'ACCEPT'] = [r.accept]
-    %w{CHARSET LANGUAGE ENCODING}.map{|a|
-      s[Header+'ACCEPT_'+a] = [(r.accept_ '_' + a)]}
-    s[HTTP+'MTHD'] = [H[{_: :a, c: fn, style: 'font-weight: bold', href: 'http://www.w3.org/Protocols/HTTP/Methods/'+fn+'.html'}]]
+    # request environment -> graph
+r.map{|k,v| s[Header + k] = k == 'uri' ? v : [v] }
+  %w{CHARSET LANGUAGE ENCODING}.map{|a|
+    s[Header+'ACCEPT_'+a] = [r.accept_('_' + a)]}
+       s[Header+'ACCEPT'] = [r.accept]
+                  s[Type] = [E[HTTP+'Response']]
+s[HTTP+'statusCodeValue'] = [404]
     s[Header+'HTTP_HOST'] = [E['http://' + s[Header+'HTTP_HOST'][0]]]
+                  s[Edit] = [E[r['REQUEST_PATH']+'?view=edit&graph=editable']]
+              s['#query'] = [r.q]
+            s['#seeAlso'] = [e.parent,*e.a('*').glob]
+              r.q['view'] = '404'
 
-    # link to editable resource
-    s[Edit]=[E[r['REQUEST_PATH']+'?view=edit&graph=editable']]
-
-    # link to nearby resources
-    s['#seeAlso']=[e.parent,*e.a('*').glob]
-
-    r.q['view'] = '404'
     [404,{'Content-Type'=> r.format},[e.render(r.format,g,r)]]}
   
   fn 'view/404',->d,e{
