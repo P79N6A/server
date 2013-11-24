36ccd27d2c608728ca659a809daa200c36041d7a
carmen <_@whats-your.name>
2013-11-24 11:49:01 +0000

    o

--- ruby/Th/POST.rb
+++ ruby/Th/POST.rb
@@ -3,15 +3,13 @@ class E
 
   def POST
     (Rack::Request.new @r).params.map{|k,v|
-      s,p,o = (CGI.unescape k).split S
-      if s&&p&&o
-        oP = o
+      s, p, o = (CGI.unescape k).split S
+      if (s && p && o)
+        oP = o # object path
         s,p,o = [s,p,o].map &:unpath
         s = s.uri[0..-2].E if s.uri[-1] == '/'
         p = p.uri[0..-2].E if p.uri[-1] == '/'
-        vU = E.literal v
-        oU = oP.E
-        unless oU.uri == vU.uri
+        unless oP.E == (E.literal v)
           s[p,o,v] # edit
         end
       end}
