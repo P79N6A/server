722d7c97482de104363167965b2a311bff7efb58
carmen <_@whats-your.name>
2013-11-24 00:01:57 +0000

    p

--- ruby/Th/POST.rb
+++ ruby/Th/POST.rb
@@ -2,35 +2,22 @@ watch __FILE__
 class E
 
   def POST
-    r = Rack::Request.new @r
-    p = nil
-    # each triple
-    r.params.map{|k,v|
+    (Rack::Request.new @r).params.map{|k,v|
+      s,p,o = (CGI.unescape k).split S
+      if s&&p&&o
+        oP = o
+        s,p,o = [s,p,o].map &:unpath
+        s = s.uri[0..-2].E if s.uri[-1] == '/'
+        p = p.uri[0..-2].E if p.uri[-1] == '/'
+        
+        # object-delta
+        vU = E.literal v
+        oU = oP.E
+        unless oU.uri == vU.uri
+          s[p,o,v] # edit
+        end
+      end}
 
-      # path-format triple
-      sP,pP,oP = (CGI.unescape k).split S
-
-      # original triple
-      s,p,o = [sP,pP,oP].map &:unpath
-      puts [:POST,:s,s,:p,p,:o,o.class,o].join(' ')
-
-      # nuke trailing slash(es)
-      s = s.uri[0..-2].E if s.uri[-1] == '/'
-      p = p.uri[0..-2].E if p.uri[-1] == '/'
-
-      # object-delta URIs
-      vU = E.literal v
-      oU = oP.E
-
-      # change detected?
-      unless oU.uri == vU.uri
-        # edit triple
-        s[p,o,v]
-      end
-
-      # snapshot current resource state
-      
-    }
     @r.q.update({'view' => 'editPO',
                  'graph' => 'editable'})
     self.GET
