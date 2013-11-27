ffec6cd47d759d3cf896070ca0cc9ed56e517d1b
carmen <_@whats-your.name>
2013-11-27 08:36:01 +0000

    tweak UI

--- js/facets.js
+++ js/facets.js
@@ -13,33 +13,37 @@ var e = function(){
 	var po = pon.attr('facet')
 
 	// visual selection status
-	if(pon.attr('on')){pon.removeAttribute('on'); pon.style.backgroundColor=''
-	           } else {pon.attr('on','true'); pon.style.backgroundColor='#fff'}
+	if(pon.attr('on')){
+	    pon.removeAttribute('on');
+	    pon.style.backgroundColor='';
+	    pon.style.color='';
+	} else {
+	    pon.attr('on','true');
+	    pon.style.backgroundColor='#0af';
+	    pon.style.color='#fff';
+	}
 
-	// construct selection rules
+	// selection-status rules
 	var s = [], on = pn.querySelectorAll('[on=true]')
 	if(on.length > 0) {
 	    s.push('.'+p+'{display:none}') // lower specificity for predicate class
 	    on.map(function(){
 		s.push('.'+p+'.'+this.attr('facet')+'{display:inline}')})} // p+o higher specificity
 
-	// create selected-facet stylesheet
-	q('style.'+p) && q('style.'+p).remove()
+	// apply rules
+	q('style.' + p) && q('style.' + p).remove()
 	q('body').append(el('style').attr('class',p).txt(s.join('\n')))
-	e.preventDefault();
-	return false;
     })
 
+    // hide controls
     qa('.facet .predicate').click(function(e){
 	var t = e.target.parentNode
 	t.style.display='none'
-	q('.selector[facet='+t.attr('facet')+']').style.display='inline'
-    })
-
+	q('.selector[facet='+t.attr('facet')+']').style.display='inline'})
+    // show
     qa('.selector').click(function(e){
 	e.target.style.display='none'
-	q('.facet[facet='+e.target.attr('facet')+']').style.display='block'
-    })
+	q('.facet[facet='+e.target.attr('facet')+']').style.display='block'})
 
 
 };
