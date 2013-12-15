85c7510377f14931fdb26b734b191a3ee3a2a13f
carmen <_@whats-your.name>
2013-12-15 08:41:58 +0000

    basic CRUD

--- ruby/POST.rb
+++ ruby/POST.rb
@@ -6,12 +6,16 @@ class E
       s, p, o = (CGI.unescape k).split S
       if (s && p && o)
         oP = o # object path
-        s,p,o = [s,p,o].map &:unpath
-        s = s.uri[0..-2].E if s.uri[-1] == '/'
-        p = p.uri[0..-2].E if p.uri[-1] == '/'
-        unless oP.E == (E.literal v)
-          puts ["POST",:s,uri,:p,p,:o,o,o.class,:oV,v,v.class].join ' '
-          s[p,o,v] # edit
+        begin
+          s,p,o = [s,p,o].map &:unpath
+          s = s.uri[0..-2].E if s.uri[-1] == '/'
+          p = p.uri[0..-2].E if p.uri[-1] == '/'
+          unless oP.E == (E.literal v)
+            puts ["POST",:s,uri,:p,p,:o,o,o.class,:oV,v,v.class].join ' '
+            s[p,o,v] # edit
+          end
+        rescue  Exception => x
+          puts x
         end
       end}
 
