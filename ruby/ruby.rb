86330dd66d6dce251b0c56b30b3040dccbb7d00e
carmen <_@whats-your.name>
2013-12-22 05:56:06 +0000

    ok

--- ruby/ruby.rb
+++ ruby/ruby.rb
@@ -1,15 +1,26 @@
-class Object
-  def id; self end
-  def do; yield self end
+class Array
+  def head; self[0] end
+  def tail; self[1..-1] end
+  def random; self[rand length] end
+  def h; join.h end
+  def intersperse i
+    inject([]){|a,b|a << b << i}[0..-2]
+  end
+  def cr; intersperse "\n" end
 end
 
-class String
-  def h; Digest::SHA1.hexdigest self end
-  def hsub h; map{|e|h[e]||e} end
-  def map; each_char.map{|l| yield l}.join end
-  def tail; self[1..-1] end
-  def to_utf8; encode('UTF-8', undef: :replace) end
-  def t; match(/\/$/) ? self : self+'/' end
+class FalseClass
+  def do; false end
+end
+
+class Fixnum
+  def max i; i > self ? self : i end
+  def min i; i < self ? self : i end
+end
+
+class Float
+  def max i; i > self ? self : i end
+  def min i; i < self ? self : i end
 end
 
 class Hash
@@ -28,34 +39,21 @@ class Hash
   end
 end
 
-class Array
-  def head; self[0] end
-  def tail; self[1..-1] end
-  def snd;  self[1] end
-  def random; self[rand length] end
-  def h; join.h end
-  def intersperse i
-    inject([]){|a,b|a << b << i}[0..-2]
-  end
-  def sum; inject 0, &:+ end
-  def cr; intersperse "\n" end
-end
-
-class Float
-  def max i; i > self ? self : i end
-  def min i; i < self ? self : i end
-end
-
-class Fixnum
-  def max i; i > self ? self : i end
-  def min i; i < self ? self : i end
-end
-
-class  FalseClass
-  def do; false end
-end
-
 class NilClass
   def do; nil end
   def html e=nil; "" end
+end
+
+class Object
+  def id; self end
+  def do; yield self end
+end
+
+class String
+  def h; Digest::SHA1.hexdigest self end
+  def hsub h; map{|e|h[e]||e} end
+  def map; each_char.map{|l| yield l}.join end
+  def tail; self[1..-1] end
+  def to_utf8; encode('UTF-8', undef: :replace) end
+  def t; match(/\/$/) ? self : self+'/' end
 end
