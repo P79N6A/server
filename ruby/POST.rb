24f32b0678dcd9aa82df3f1ad8bfb82d6181e34b
carmen <_@whats-your.name>
2013-12-26 22:20:48 +0000

    ok

--- ruby/POST.rb
+++ ruby/POST.rb
@@ -2,29 +2,40 @@ watch __FILE__
 class E
 
   def POST
-    (Rack::Request.new @r).params.map{|k,v|
-      s, p, o = (CGI.unescape k).split S
-      if s && p && o
-        oP = o # object path
-        begin
-          s,p,o = [s,p,o].map &:unpath
-          s = s.uri[0..-2].E if s.uri[-1] == '/'
-          p = p.uri[0..-2].E if p.uri[-1] == '/'
-          unless oP.E == (E.literal v)
-            puts ["POST",:s,uri,:p,p,:o,o,o.class,:oV,v,v.class].join ' '
-            s[p,o,v] # edit
+    puts [:POST,@r['CONTENT_TYPE']].join ' '
+
+    case @r['CONTENT_TYPE']
+    when /^application\/sparql-update/
+      puts :SPARQL
+      puts @r['rack.input'].read
+
+    when /^application\/x-www-form-urlencoded/
+      puts "form"
+      (Rack::Request.new @r).params.map{|k,v|
+        puts "POST #{uri} #{k} #{v}"
+        s, p, o = (CGI.unescape k).split S
+        if s && p && o
+          oP = o # object path
+          begin
+            s,p,o = [s,p,o].map &:unpath
+            s = s.uri[0..-2].E if s.uri[-1] == '/'
+            p = p.uri[0..-2].E if p.uri[-1] == '/'
+            unless oP.E == (E.literal v)
+              puts ["POST",:s,uri,:p,p,:o,o,o.class,:oV,v,v.class].join ' '
+              s[p,o,v] # edit
+            end
+          rescue  Exception => x
+            puts x
           end
-        rescue  Exception => x
-          puts x
-        end
-      end}
+        end} if false
 
-    # update graph snapshot
-    g = {}
-    fromStream g, :triplrFsStore
-    ef.w g, true
+      # update graph snapshot
+      g = {}
+      fromStream g, :triplrFsStore
+      ef.w g, true
+    end
 
-    [303,{'Location'=>uri},[]]
+    [303,{'Location'=>uri},[]]    
   end
 
 end
