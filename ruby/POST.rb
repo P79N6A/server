cfa4048602c105d42b972156173e22d0b81c19dd
carmen <_@whats-your.name>
2013-12-27 10:10:44 +0000

    ok

--- ruby/POST.rb
+++ ruby/POST.rb
@@ -2,14 +2,16 @@ watch __FILE__
 class E
 
   def POST
-    puts [:POST,@r['CONTENT_TYPE']].join ' '
+    type = @r['CONTENT_TYPE']
+    puts [:POST,type,uri].join ' '
 
-    case @r['CONTENT_TYPE']
+    case type
     when /^application\/sparql-update/
       puts :SPARQL_UDATE
       puts @r['rack.input'].read
 
     when /^application\/x-www-form-urlencoded/
+      ch = nil
       (Rack::Request.new @r).params.map{|k,v|
         s, p, o = (CGI.unescape k).split S
         if s && p && o 
@@ -18,17 +20,20 @@ class E
             s, p, o = [s, p, o].map &:unpath
             s = s.uri[0..-2].E if s.uri[-1] == '/'
             p = p.uri[0..-2].E if p.uri[-1] == '/'
-            unless oP.E == (E.literal v)
-              puts ["POST",:s,uri,:p,p,:o,o,o.class,:oV,v,v.class].join ' '
+            if oP.E == (E.literal v) && s.uri.match(/^http/) && p.uri.match(/^http/)
+              puts "POST <#{s}> <#{p}> <#{o}>"
               s[p,o,v]
-              g = {}
-              fromStream g, :triplrFsStore
-              ef.w g, true
+              ch = true
             end
           rescue  Exception => x
             puts x
           end
-        end} if false
+        end}
+      if ch # state changed
+        g = {}
+        fromStream g, :triplrFsStore
+        ef.w g, true
+      end
     end
 
     [303,{'Location'=>uri},[]]    
