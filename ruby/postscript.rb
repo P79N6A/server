4d0e56279ffcc7040f1161533c0a3bc85de0a775
anonymous <_@whats-your.name>
2014-01-06 15:20:18 +0000

    ok

--- ruby/postscript.rb
+++ ruby/postscript.rb
@@ -1,10 +1,21 @@
 watch __FILE__
 class E
 
-# gs -dSAFER -dBATCH -dNOPAUSE -sDEVICE=png16m -r300 -sOutputFile='p-%00d.png' -dTextAlphaBits=4 sock.ps 
-
   def triplrPS
-    
+    p = E[dirname + '/.' + File.basename(path) + '/']
+    unless p.e && p.m > m 
+      p.mk
+      `gs -dSAFER -dBATCH -dNOPAUSE -sDEVICE=png16m -r300 -sOutputFile='#{p.sh}%03d.png' -dTextAlphaBits=4 #{sh}`
+    end
+    yield uri, Content, `ps2txt #{sh}`.hrefs
+    p.a('*.png').glob.map{|i|
+      yield uri, DC+'Image', i }
   end
+
+  F['view/'+MIMEtype+'application/postscript']=->r,_{
+    r.values.map{|d|
+      d[DC+'Image'].do{|is|
+        is.sort_by(&:uri).map{|i|
+          {_: :img, style:'float:left;max-width:100%', src: i.url}}}}}
 
 end
