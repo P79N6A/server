377273f94e760d3af0d040f46cc348983ec2f11d
anonymous <_@whats-your.name>
2014-01-08 09:43:17 +0000

    pager n/p key mapping

--- ruby/page.rb
+++ ruby/page.rb
@@ -8,11 +8,13 @@ class E
     u = d[''] || {}
     {style: "float:left",
       c: [u[Prev].do{|p|{_: :a, rel: :prev, style: 'background-color:white;color:black;font-size:2em',href: p.uri, c: '&larr;'}},
-          u[Next].do{|n|{_: :a, rel: :next, style: 'font-size:2em',href: n.uri, c: '&rarr;'}},
+          u[Next].do{|n|{_: :a, rel: :next, style: 'font-size:2em',href: n.uri, c: '&rarr;'}},'<br>',
           u[DC+'hasFormat'].do{|fs|
             fs.map{|f|
               [{_: :a, href: f,
                 c: (f.uri.match(/n3$/) ? {_: :img, src: '/css/misc/cube.png', style: 'height:2em'} :
-                    {style: 'background-color:white;color:black;display:inline;padding:.2em', c: f.label.split('%')[0]})},'<br>']}}]}}
+                    {style: 'background-color:white;color:black;display:inline;padding:.2em', c: f.label.split('%')[0]})},'<br>']}},
+          (H.js '/js/pager'),(H.once e,:mu,(H.js '/js/mu')) # n/p key mapping
+         ]}}
 
 end
