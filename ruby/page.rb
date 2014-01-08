01c03a94974df56ee0228ed89319b0a4a3bc98b2
anonymous <_@whats-your.name>
2014-01-08 09:45:36 +0000

    ok

--- ruby/page.rb
+++ ruby/page.rb
@@ -8,7 +8,7 @@ class E
     u = d[''] || {}
     {style: "float:left",
       c: [u[Prev].do{|p|{_: :a, rel: :prev, style: 'background-color:white;color:black;font-size:2em',href: p.uri, c: '&larr;'}},
-          u[Next].do{|n|{_: :a, rel: :next, style: 'font-size:2em',href: n.uri, c: '&rarr;'}},'<br>',
+          u[Next].do{|n|{_: :a, rel: :next, style: 'background-color:black;color:white;font-size:2em',href: n.uri, c: '&rarr;'}},'<br>',
           u[DC+'hasFormat'].do{|fs|
             fs.map{|f|
               [{_: :a, href: f,
