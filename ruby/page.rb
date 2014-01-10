e363c0beb2c213aa15d71ac087916525aa03ffec
anonymous <_@whats-your.name>
2014-01-10 07:55:36 +0000

    this sw-cube will link to databrowser skin *tabulator

--- ruby/page.rb
+++ ruby/page.rb
@@ -6,11 +6,8 @@ class E
     {style: "float:left",
       c: [u[Prev].do{|p|{_: :a, rel: :prev, style: 'background-color:white;color:black;font-size:2.3em',href: p.uri, c: '&larr;'}},
           u[Next].do{|n|{_: :a, rel: :next, style: 'background-color:black;color:white;font-size:2.3em',href: n.uri, c: '&rarr;'}},'<br>',
-          u[DC+'hasFormat'].do{|fs|
-            fs.map{|f|
-              [{_: :a, href: f,
-                c: (f.uri.match(/n3$/) ? {_: :img, src: '/css/misc/cube.png', style: 'height:2em', alt: :n3, title: :n3} :
-                    {style: 'background-color:white;color:#888;display:inline;padding:.2em', c: f.label.split('%')[0]})},'<br>']}},
+          {_: :a, href: e['uri'].E.url+'?view=data',
+            c: {_: :img, src: '/css/misc/cube.png', style: 'height:2em'}},'<br>',
           (H.js '/js/pager'),(H.once e,:mu,(H.js '/js/mu')) # n/p key mapping
          ]}}
 
