e0ee320bbf256d52364671d5ca606988ebeb78ed
anonymous <_@whats-your.name>
2014-01-13 03:12:07 +0000

    input.focus() onload slides up keyboad on mobile

--- js/search.js
+++ js/search.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(){
     var q=document.querySelector('input[name=q]')
     q.addEventListener('keydown',function(e){e.stopPropagation()},false)
-    q.focus()
+//    q.focus()
 }, false);
 
--- ruby/page.rb
+++ ruby/page.rb
@@ -3,8 +3,8 @@ class E
 
   fn 'view/'+HTTP+'Response',->d,e{
     u = d['#']
-    [u[Prev].do{|p|{_: :a, rel: :prev, style: 'background-color:white;color:black;font-size:2.3em;float: left;clear:both',href: p.uri, c: '&larr;'}},
-     u[Next].do{|n|{_: :a, rel: :next, style: 'background-color:black;color:white;font-size:2.3em;float:right;clear:both',href: n.uri, c: '&rarr;'}},
+    [u[Prev].do{|p|{_: :a, rel: :prev, style: 'background-color:white;color:black;font-size:2.3em;float: left;clear:both;margin-right:.3em',href: p.uri, c: '&larr;'}},
+     u[Next].do{|n|{_: :a, rel: :next, style: 'background-color:black;color:white;font-size:2.3em;float:right;clear:both;border-radius:0',href: n.uri, c: '&rarr;'}},
      {_: :a, href: e['uri'].E.docBase.a('.n3?')+e['QUERY_STRING'],
        c: {_: :img, src: '/css/misc/cube.png', style: 'height:2em;background-color:white;padding:.54em;border-radius:1em;margin:.2em'}},
      (H.js '/js/pager'),(H.once e,:mu,(H.js '/js/mu')) # n/p key mapping
