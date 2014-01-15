2de3314dd064186afd9235ff048f596432ce7985
anonymous <_@whats-your.name>
2014-01-15 20:27:59 +0000

    ok

--- ruby/search.rb
+++ ruby/search.rb
@@ -6,12 +6,12 @@ class E
 
   F['/search.n3/GET'] = F['/search/GET']
 
-  fn 'view/'+Search,-> q=nil,e { q||={}
+  fn 'view/'+Search,-> d,e {
     [H.css('/css/search'),H.js('/js/search'),
      {:class => :form,
-      c: {_: :form, action: e['REQUEST_PATH'],
-        c: [{_: :input, name: :q, value: q['q']}, # search box
-            q.except('q','start'). # new query & offset for this search
+      c: {_: :form, action: '/search',
+        c: [{_: :input, name: :q, value: e.q['q']}, # search box
+            e.q.except('q','start'). # new query & offset for this search
             map{|a,s|
               {_: :input, name:  a, value: s, :type => :hidden}}]}}]}
 
