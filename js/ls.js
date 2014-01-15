d446ff38cc02909e9c1547945c4db38449896a4e
anonymous <_@whats-your.name>
2014-01-15 21:55:57 +0000

    remove JS files

--- js/ls.js
+++
@@ -1,7 +0,0 @@
-var ls = function(){
-    qa('a').click(function(){
-	qa('a').map(function(){this.style.backgroundColor='#233';this.style.color='#fff';})
-	this.style.backgroundColor='#fff';this.style.color='#000';
-    })
-};
-document.addEventListener("DOMContentLoaded", ls, false);
--- js/normal.js
+++
@@ -1,7 +0,0 @@
-var normal = function(){
-    q('form').on('change',function(e){
-	e.target.style.backgroundColor = e.target.value == e.target.attr('name') ? '' : '#8f8'
-    })
-}
-document.addEventListener("DOMContentLoaded", normal, false);
-
