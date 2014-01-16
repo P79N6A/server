7eb5571616fe705dcdc1add179b853d103028b22
anonymous <_@whats-your.name>
2014-01-16 11:24:27 +0000

    provide summary of CSV file and link to full

--- ruby/csv.rb
+++ ruby/csv.rb
@@ -7,6 +7,8 @@ class E
     open(node).readlines.map{|l|l.chomp.split(d) rescue []}.do{|lines|
       lines[0].do{|fields| # ok, we have at least one line..
         yield uri+'#', Type, E[CSV+'Table']
+        yield uri+'#', CSV+'rowCount', lines.size
+        yield uri+'#', COGS+'View', E[uri+'?view=table']
         lines[1..-1].each_with_index{|row,line|
           row.each_with_index{|field,i|
             id = uri + '#row:' + line.to_s
