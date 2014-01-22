751915e092451acae370b18956fa6f9f43eb96ed
anonymous <_@whats-your.name>
2014-01-22 08:02:03 +0000

    ok

--- ruby/page.rb
+++ ruby/page.rb
@@ -6,10 +6,10 @@ class E
 
     [u[Prev].do{|p|
        {_: :a, rel: :prev, href: p.uri, c: '&larr;',
-         style: 'color:#ccc;background-color:#eee;font-size:2.3em;float:left;clear:both;margin-right:.3em'}},
+         style: 'color:#fff;background-color:#eee;font-size:2.3em;float:left;clear:both;margin-right:.3em'}},
      u[Next].do{|n|
        {_: :a, rel: :next, href: n.uri, c: '&rarr;',
-         style: 'color:#ccc;background-color:#eee;font-size:2.3em;float:right;clear:both;border-radius:0'}},
+         style: 'color:#fff;background-color:#eee;font-size:2.3em;float:right;clear:both;border-radius:0'}},
 
      {_: :a, href: e['REQUEST_PATH'].sub(/\.html$/,'') + e.q.merge({'view'=>'data'}).qs, # data browser
        c: {_: :img, src: '/css/misc/cube.png', style: 'height:2em;background-color:white;padding:.54em;border-radius:1em;margin:.2em'}},
