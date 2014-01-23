6504ccbb32571abe9bc0894be46cc5380e483eb4
anonymous <_@whats-your.name>
2014-01-23 08:49:26 +0000

    escape HTML in triplrStdOut (and HREFize links)

--- ruby/sh.rb
+++ ruby/sh.rb
@@ -5,13 +5,11 @@ class E
 
     `#{e} #{a}|grep :`.each_line{|i|
 
-      # split keys from vals
       i = i.split /:/
 
-    yield uri,
-    (f + (i[0].match(g)||[0,i[0]])[1]. # id cleaner
-     gsub(/\s/,'_').gsub(/\//,'-').gsub(/[\(\)]+/,'')),                # predicate
-      i.tail.join(':').strip.do{|v|v.match(/^[0-9\.]+$/) ? v.to_f : v} # object
+    yield uri, (f + (i[0].match(g)||[0,i[0]])[1].                            # s
+     gsub(/\s/,'_').gsub(/\//,'-').gsub(/[\(\)]+/,'')),                      # p
+      i.tail.join(':').strip.do{|v|v.match(/^[0-9\.]+$/) ? v.to_f : v.hrefs} # o
     }
     nil
   rescue
