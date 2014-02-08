081732ee56ea1d742056e1f9745154fd70368c37
anonymous <_@whats-your.name>
2014-02-08 20:42:47 +0000

    switch to mail.rb even though it is insanely slow, since waiting for it to finish is faster than fixing TMail

--- ruby/mail.rb
+++ ruby/mail.rb
@@ -6,22 +6,21 @@ class E
   rescue LoadError => e
   end
 
-  MessagePath = ->id{ h = id.h # hash identifier
-    '/msg/' + h[0..2] + '/' + id}
+  MessagePath = ->id{'/msg/' + id.h[0..2] + '/' + id}
 
   GREP_DIRS.push /^\/m\/[^\/]+\// # allow grep within a single address
 
   F['/m/GET'] = -> e,r{
+    # set overview(summary) view and start a depth-first view of message tree of this address
     if m = e.pathSegment.uri.match(/^\/m\/([^\/]+)$/)
-      r.q['set'] = 'depth'
+      r.q['set'] ||= 'depth'
       r.q['view'] ||= 'threads'
        e.response
     else
       false      
     end}
 
-  def triplrTmail &f
-
+  def triplrTmail &f; require 'tmail'
     (TMail::Mail.load node).do{|m|      # load
       d = m.message_id; return unless d # parse successful?
       id = d[1..-2]                     # message-ID
@@ -73,7 +72,52 @@ class E
           doc.ln E[rel.uri.split('#')[0]+st]}}}}
 
   def triplrMailMessage &f
-    addDocs :triplrTmail, @r['SERVER_NAME'], [SIOC+'reply_of'], IndexMail, &f
+    addDocs :triplrMail, @r['SERVER_NAME'], [SIOC+'reply_of'], IndexMail, &f
+    # TMail is significantly faster (50* in 1.2.7 v. 2.5.4) but C-extension based quasi-abandonware that barely worked on 1.9, and no on 2.0 or 2.1...
+    # debian's patched ruby-tmail (until it rolls into oblivion) has mostly worked (about 1% of mails fail to parse due to encoding issues) for a decade
+    # a mail will only be parsed by Mail.rb once (cached) but leaving triplrTmail in case you've a need for speed 
+#    addDocs :triplrTmail, @r['SERVER_NAME'], [SIOC+'reply_of'], IndexMail, &f
+  end
+
+  def triplrMail &f
+    (TMail::Mail.load node).do{|m|      # load
+      d = m.message_id; return unless d # parse successful?
+      id = d[1..-2]                     # message-ID
+      e = MessagePath[id]               # webized ID
+      from = m.from[0].to_utf8          # author
+      creator = '/m/'+from+'#'+from     # author URI
+      yield e, DC+'identifier', id      # original ID
+      yield e, DC+'source', self        # original file
+      yield e, Type, E[SIOCt + 'MailMessage']
+      yield e, Type, E[SIOC  + 'Post']
+      yield e, Date, m.date.iso8601 if m.date
+      yield e, Title, m.subject.to_utf8
+      yield e, Creator, E[creator]
+      yield e, SIOC+'has_discussion', E[e+'?graph=thread&view=timegraph#discussion']
+      yield creator, Name, m.friendly_from.to_utf8
+      yield creator, DC+'identifier', E['mailto:'+from]
+      yield e, SIOC+'reply_to',
+      E[URI.escape("mailto:#{m.header['x-original-to']||from}?References=<#{id}>&In-Reply-To=<#{id}>&Subject=#{m.subject.to_utf8}&")+'#reply']
+
+      %w{to cc bcc}.map{|to|
+        m.send(to).do{|to| to.map{|to|
+            to = to.to_utf8
+            yield e, To, E['/m/'+to+'#'+to]
+          }}}
+
+      %w{in_reply_to references}.map{|ref|
+        m.send(ref).do{|refs| refs.map{|r|
+          yield e, SIOC+'reply_of', E[MessagePath[r[1..-2]]]}}}
+      m.in_reply_to.do{|refs| refs.map{|r|yield e, SIOC+'has_parent', E[MessagePath[r[1..-2]]]}}
+
+      # RDF:HTML message-body
+      yield e, Content,
+      H([{_: :pre, class: :mail, style: 'white-space: pre-wrap',
+           c: m.concat_message(e.E,0,&f).gsub(/^\s*(&gt;)(&gt;|\s)*\n/,"").lines.to_a.map{|l| # skip quoted empty-lines
+             l.match(/(^\s*(&gt;|On[^\n]+(said|wrote))[^\n]*)\n/) ? {_: :span, class: :q, depth: l.scan(/(&gt;)/).size, c: l} : l # quotes
+           }},(H.css '/css/mail',true)])}
+  rescue Exception => e
+    puts e
   end
 
   F['view/'+MIMEtype+'message/rfc822'] = NullView # hide containing file in default render
