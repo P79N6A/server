dd443064ba6112f5ae8b5748c52930fe8e9ee0b5
anonymous <_@whats-your.name>
2014-03-06 08:46:00 +0000

    clean up cert

--- ruby/uid.rb
+++ ruby/uid.rb
@@ -3,9 +3,14 @@ class R
 
   F['/whoami/GET'] = -> d,e {
     e['HTTP_SSL_CLIENT_CERT'].do{|pem|
+      lines = pem.split ' '
+      head = lines[0..1].join ' '
+      foot = lines[-2..-1].join ' '
+      body = lines[2..-3].join "\n"
+      pem = [head,body,foot].join "\n"
       x509 = OpenSSL::X509::Certificate.new pem
       pubkey = x509.public_key
-       puts pubkey
+      puts pubkey
      }
   }
 
