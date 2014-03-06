b55fd5403d112f82f796ab0c04b650b3d3dd1718
anonymous <_@whats-your.name>
2014-03-06 19:40:11 +0000

    peel out exponent/modulus - link to reference implementation (LDPHP?)

--- ruby/uid.rb
+++ ruby/uid.rb
@@ -1,20 +1,20 @@
 watch __FILE__
 class R
 
+  # https://github.com/linkeddata/ldphp/blob/master/www/inc/webid.lib.php
+
   F['/whoami/GET'] = -> d,e {
-    e['HTTP_SSL_CLIENT_CERT'].do{|pem|
-      lines = pem.split ' '
-      pem = [lines[0..1].join(' '),
-             lines[2..-3],
-             lines[-2..-1].join(' ')
-            ].join "\n"
+    e['HTTP_SSL_CLIENT_CERT'].do{|v|
+      p = v.split /[\s\n]/ # linebreaks sometimes converted to spaces upstream
+      pem = [p[0..1].join(' '),p[2..-3],p[-2..-1].join(' ')].join "\n"
       x509 = OpenSSL::X509::Certificate.new pem
       pubkey = x509.public_key
+      subject = nil
       x509.extensions.find{|x|x.oid=='subjectAltName'}.do{|altName|
-        s = altName.value.sub(/^URI:/,'')
-        puts s
-      }
+        subject = altName.value.sub /^URI:/, ''}
+      m = pubkey.n
+      e = pubkey.e
     }
-  }
+    nil }
   
 end
