d8502e21e986907bebeb6ed49a27daeb157c311b
anonymous <_@whats-your.name>
2014-03-07 07:11:09 +0000

    match

--- ruby/uid.rb
+++ ruby/uid.rb
@@ -7,17 +7,21 @@ class R
     e['HTTP_SSL_CLIENT_CERT'].do{|v|
       p = v.split /[\s\n]/ # linebreaks sometimes munged into spaces upstream
       unless p.size < 2
-        pem = [p[0..1].join(' '),p[2..-3],p[-2..-1].join(' ')].join "\n" # reformat
-        OpenSSL::X509::Certificate.new(pem).do{|x509|
-          x509.extensions.find{|x|x.oid == 'subjectAltName'}.do{|altName|
+        pem = [p[0..1].join(' '), # header
+               p[2..-3],          # body
+               p[-2..-1].join(' ')].join "\n" # format
+        OpenSSL::X509::Certificate.new(pem).do{|x509| # parse
+          x509.extensions.find{|x|x.oid == 'subjectAltName'}.do{|altName| # user URI
             uri = altName.value.sub /^URI:/, ''
             pubkey = x509.public_key
-            m = pubkey.n
-            e = pubkey.e
-            graph = RDF::Repository.load(uri)
+            m = pubkey.n # modulus
+            e = pubkey.e # exponent
+            graph = RDF::Repository.load uri
             query = "PREFIX : <http://www.w3.org/ns/auth/cert#> SELECT ?m ?e WHERE { <#{uri}> :key [ :modulus ?m; :exponent ?e; ] . }"
             SPARQL.execute query, graph do |result|
-              puts result.class
+              mCert = m.to_i
+              mWeb = result[:m].value.to_i(16)
+              puts mCert == mWeb
             end
           }
         }
