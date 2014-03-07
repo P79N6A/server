b5c87b66fe9835196069ed794a3e92c266f1755a
anonymous <_@whats-your.name>
2014-03-07 02:54:52 +0000

    query

--- ruby/uid.rb
+++ ruby/uid.rb
@@ -5,17 +5,24 @@ class R
 
   F['/whoami/GET'] = -> d,e {
     e['HTTP_SSL_CLIENT_CERT'].do{|v|
-      p = v.split /[\s\n]/ # linebreaks may be converted to spaces upstream
-      pem = [p[0..1].join(' '),p[2..-3],p[-2..-1].join(' ')].join "\n"
-      OpenSSL::X509::Certificate.new(pem).do{|x509|
-        x509.extensions.find{|x|x.oid == 'subjectAltName'}.do{|altName|
-          uri = altName.value.sub /^URI:/, ''
-          pubkey = x509.public_key
-          m = pubkey.n
-          e = pubkey.e
-          puts [:webid,uri,m,e].join ' '
-          q = "PREFIX : <http://www.w3.org/ns/auth/cert#> SELECT ?m ?e WHERE { <#{uri}> :key [ :modulus ?m; :exponent ?e; ] . }"
-          puts q }}}
-    nil }
+      p = v.split /[\s\n]/ # linebreaks sometimes munged into spaces upstream
+      unless p.size < 2
+        pem = [p[0..1].join(' '),p[2..-3],p[-2..-1].join(' ')].join "\n" # reformat
+        OpenSSL::X509::Certificate.new(pem).do{|x509|
+          x509.extensions.find{|x|x.oid == 'subjectAltName'}.do{|altName|
+            uri = altName.value.sub /^URI:/, ''
+            pubkey = x509.public_key
+            m = pubkey.n
+            e = pubkey.e
+            graph = RDF::Repository.load(uri)
+            query = "PREFIX : <http://www.w3.org/ns/auth/cert#> SELECT ?m ?e WHERE { <#{uri}> :key [ :modulus ?m; :exponent ?e; ] . }"
+            r = SPARQL.execute query,graph
+            puts r
+          }
+        }
+      end
+    }
+    nil
+  }
   
 end
