5b5ea5460d1719f25a6b0f6ae1cccd8d1412aa8a
anonymous <_@whats-your.name>
2014-03-07 07:40:09 +0000

    whoami?

--- ruby/uid.rb
+++ ruby/uid.rb
@@ -1,17 +1,25 @@
 watch __FILE__
 class R
 
+  # based on
   # https://github.com/linkeddata/ldphp/blob/master/www/inc/webid.lib.php
 
-  F['/whoami/GET'] = -> d,e {
+  F['/whoami/GET'] = -> d,e { response = nil
+
     e['HTTP_SSL_CLIENT_CERT'].do{|v|
+
       p = v.split /[\s\n]/ # linebreaks sometimes munged into spaces upstream
-      unless p.size < 2
+
+      unless p.size < 5
+
         pem = [p[0..1].join(' '), # header
                p[2..-3],          # body
                p[-2..-1].join(' ')].join "\n" # format
+
         OpenSSL::X509::Certificate.new(pem).do{|x509| # parse
-          x509.extensions.find{|x|x.oid == 'subjectAltName'}.do{|altName| # user URI
+
+          x509.extensions.
+          find{|x|x.oid == 'subjectAltName'}.do{|altName| # user URI
             uri = altName.value.sub /^URI:/, ''
             pubkey = x509.public_key
             m = pubkey.n # modulus
@@ -20,14 +28,15 @@ class R
             query = "PREFIX : <http://www.w3.org/ns/auth/cert#> SELECT ?m ?e WHERE { <#{uri}> :key [ :modulus ?m; :exponent ?e; ] . }"
             SPARQL.execute query, graph do |result|
               mCert = m.to_i
-              mWeb = result[:m].value.to_i(16)
-              puts mCert == mWeb
-            end
-          }
-        }
-      end
-    }
-    nil
-  }
-  
+              mWeb = result[:m].value.to_i 16
+              if mCert == mWeb
+                body = RDF::Writer.for(:n3).buffer do |writer|
+                  writer << graph
+                end
+                response = [200,{'Content-Type'=>'text/n3; charset=utf-8'},[body]]
+              end
+            end}}
+      end}
+    response }
+
 end
