3217e78ed2d2f798162c316b6d9922b9eef4bed5
anonymous <_@whats-your.name>
2014-03-09 05:49:08 +0000

    trim

--- ruby/code.rb
+++ ruby/code.rb
@@ -2,18 +2,11 @@ class R
   
   def triplrSourceCode
     m = mime.split(/\//)[-1].sub(/^x-/,'')
-    # show line numbers?
-    n = (@r && @r.q.has_key?('n')) ? "--line-number-ref=#{uri.sh}:" : ""
-    if size < 512e3
-      yield uri,Content, `source-highlight -f html -s #{m} #{n} -i #{sh} -o STDOUT`
-    end
+    yield uri,Content, `source-highlight -f html -s #{m} -i #{sh} -o STDOUT` if size < 512e3
  end
 
-  fn 'view/code',->d,e{[{_: :style, c: 'body{background-color:white;color:black}'},
-    d.values.map{|r|[r.R.do{|e|[{_: :a,name: e.uri},e.html(e.base,true)]},'<br>',
-                      r[Content]]}]}
-
   # ls /usr/share/source-highlight/*.lang | xargs -i basename {} .lang | tr "\n" " "
+
   %w{ada applescript asm awk bat bib bison caml changelog c clipper cobol conf cpp csharp css
  desktop diff d erlang errors flex fortran function glsl haskell haskell_literate haxe html java
  javascript key_string langdef latex ldap lisp log logtalk lsm lua m4 makefile manifest nohilite
@@ -22,12 +15,9 @@ class R
     .map{|l|
     ma = 'application/' + l
     mt = 'text/x-' + l
-    # extension -> MIME
-    MIME[l.to_sym] ||= ma
-    # triplr/view mappings
-    [ma,mt].map{|m|
-      MIMEsource[m] ||= [:triplrSourceCode]
-      fn 'view/'+m, F['view/code']}}
+    MIME[l.to_sym] ||= ma # extension mapping
+    [ma,mt].map{|m| # triplr/view mappings
+      MIMEsource[m] ||= [:triplrSourceCode]}}
 
   MIMEsource['text/css'] ||= [:triplrSourceCode] # i hear CSS is Turing complete now, http://inamidst.com/whits/2014/formats
 
