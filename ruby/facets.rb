74da61c1accf16788aeab62e2ce0dd7ad15182a2
anonymous <_@whats-your.name>
2014-03-09 14:49:37 +0000

    ok

--- ruby/facets.rb
+++ ruby/facets.rb
@@ -1,14 +1,11 @@
-#watch __FILE__
+watch __FILE__
 class R
-=begin
-   faceted-filter, implemented via dynamically-generated style-sheets
-   query-string:
-     fv = (name)  render singleton-resource w/ itemview lambda
-=end
 
-  fn 'facets',->a,m,e{
-    # facets
-    a = Hash[(a.split ',').map{|a|[a,{}]}]
+  # facet-filtering - dynamic CSS
+
+  fn 'view/facets',->m,e{
+
+    a = Hash[(e.q['a'].split ',').map{|a|[a.expand,{}]}]
 
     # facet stats
     m.map{|s,r| a.map{|p,_|
@@ -25,18 +22,6 @@ class R
     view = e.q['fv'].do{|fv| F['itemview/'+fv] } || ->r,e{
       {_: :a, class: :title, href: r.R.url, c: r[Title] || r.uri.abbrURI} if (r.class == R || r.class == Hash) && r.uri}
 
-    resources=->{
-      m.map{|u,r| # each resource
-        a.map{|p,_| # each facet
-          [n[p], r[p].do{|o| # value
-               o.justArray.map{|o|
-               n[o.to_s] # identifier
-             }}].join ' '
-        }.do{|f|
-          [f.map{|o|'<div class="'+o+'">'}, # facet wrapper
-           view[r,e], # resource
-           (0..f.size-1).map{|c|'</div>'}]}}}
-
     [(H.css'/css/facets'),(H.js'/js/facets'),(H.js'/js/mu'),
 
      a.map{|b,_|{_: :style, class: n[b]}},
@@ -52,15 +37,15 @@ class R
                  {facet: n.(k.to_s), # predicate-object tuple
                    c: [{_: :span, class: :count, c: v},
                        {_: :span, class: :name, c: k.label}]}}]}}},     
-     (e.q['fv'].do{|fv|F['baseview/'+fv]} || ->m,e,r{r.()}).(m,e,resources)]}
-  
-  fn 'view/facetSelect',->m,e{
-    [(H.js '/js/facets.select'),(H.js '/js/mu'),(H.css '/css/facets'),
-     R.graphProperties(m).map{|e|[{c: e},' ']},
-     {_: 'button', c: 'Go'}]}
-
-  fn 'view/facets',->m,e{
-    e.q['a'].do{|a|F['facets'][a,m,e]} ||
-    F['view/facetSelect'][m,e]}
+     m.map{|u,r| # each resource
+       a.map{|p,_| # each facet
+         [n[p], r[p].do{|o| # value
+            o.justArray.map{|o|
+              n[o.to_s] # identifier
+            }}].join ' '
+       }.do{|f|
+         [f.map{|o|'<div class="'+o+'">'}, # facet wrapper
+          view[r,e], # resource
+          (0..f.size-1).map{|c|'</div>'}]}}]}
 
 end
