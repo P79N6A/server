0e072606e9863f3b8658a113ee252a7890f69310
anonymous <_@whats-your.name>
2014-03-17 04:04:36 +0000

    what

--- ruby/chan.rb
+++ ruby/chan.rb
@@ -8,12 +8,19 @@ class R
     e.q['view'] ||= 'chan'
     nil} # just add some ambient configuration
 
-  F['/chan/POST'] = -> d,e {
-    puts "Chajd"
+  F['/chan/POST'] = -> d,e{
     req = Rack::Request.new d.env
     p = req.params
     puts p
-  }
+    content = p['content']
+    if content && !content.empty?
+      name = p['title'].do{|t|t.gsub /[?#\s\/]/,'_'} || ''
+      uri = '/' + Time.now.iso8601[0..18].gsub('-','/') + name
+      if p['file']
+        puts (p['file'].class)
+      end
+      [303,{'Location' => uri},[]]
+    end}
 
   F['set/chan'] = -> d,r,m {
     s = F['set/depth'][R['/chan'],r,m]
@@ -25,7 +32,7 @@ class R
     br = '<br>'
     post = {_: :form, method: :POST,
       c: [{_: :input, title: :title, name: :title, size: 32},br,
-          {_: :textarea, rows: 8, cols: 32, name: :comment},br,
+          {_: :textarea, rows: 8, cols: 32, name: :content},br,
           {_: :input, type: :file, name: :file},
           {_: :input, type: :submit, value: 'post '}
          ]}
