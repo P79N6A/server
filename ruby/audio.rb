728b61b7f60d67b006070880d328a321273c93f5
anonymous <_@whats-your.name>
2014-04-13 06:53:16 +0000

    ok

--- ruby/audio.rb
+++ ruby/audio.rb
@@ -2,14 +2,11 @@ class R
 
   View['audio'] = ->d,e {
     [(H.once e, :audio,
-      (H.js '/js/audio'),
-      (H.css '/css/audio'),
+      (H.js '/js/audio'), (H.css '/css/audio'),
       (H.once e, :mu, (H.js '/js/mu')),
-      {id: :rand, c: :r},
-      {id: :jump, c: '&gt;'},
-      {id: :info, target: :_blank, _: :a},
+      {id: :rand, c: :rnd}, {id: :info, target: :_blank, _: :a},
       {_: e.q.has_key?('video') ? :video : :audio, id: :media, controls: true},
-      {_: :iframe, id: :infoPane}),
+      {id: :jump, c: '&rarr;'}, {_: :iframe, id: :infoPane}),
      d.map{|u,_|{_: :a, class: :track, href: URI.escape(u), c: u.split(/\//)[-1].sub(/\.(flac|mp3|wav)$/,'')}}]}
 
   %w{aif wav mpeg mp4}.map{|a|View[MIMEtype+'audio/'+a]=View['audio']}
