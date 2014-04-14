098b6aef6504e82a93aeea1f7b17bf3a6438524e
anonymous <_@whats-your.name>
2014-04-14 05:35:05 +0000

    ok

--- ruby/audio.rb
+++ ruby/audio.rb
@@ -7,7 +7,9 @@ class R
       {id: :info, target: :_blank, _: :a},
       {_: e.q.has_key?('video') ? :video : :audio, id: :media, controls: true},
       {id: :jump, c: '&rarr;'}, {id: :rand, c: :rand}),
-     d.map{|u,_|{_: :a, class: :track, href: URI.escape(u), c: u.split(/\//)[-1].sub(/\.(flac|mp3|wav)$/,'')}}]}
+     d.map{|u,_|
+       [{_: :a, class: :track, href: URI.escape(u), c: u.split(/\//)[-1].sub(/\.(flac|mp3|wav)$/,'')},"\n"]
+     }]}
 
   %w{aif wav mpeg mp4}.map{|a|View[MIMEtype+'audio/'+a]=View['audio']}
 end
