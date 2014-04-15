232e657d54b5a20b2343d9fa99696ca364645780
anonymous <_@whats-your.name>
2014-04-15 05:25:20 +0000

    ok

--- ruby/audio.rb
+++ ruby/audio.rb
@@ -5,7 +5,7 @@ class R
       (H.js '/js/audio'), (H.css '/css/audio'),
       (H.once e, :mu, (H.js '/js/mu')),
       {id: :info, target: :_blank, _: :a},
-      {_: e.q.has_key?('video') ? :video : :audio, id: :media, controls: true},
+      {_: e.q.has_key?('video') ? :video : :audio, id: :media, controls: true},'<br clear=all>',
       {id: :jump, c: '&rarr;'}, {id: :rand, c: :rand, on: 1}),
      d.map{|u,_|
        {_: :a, class: :track, href: URI.escape(u), c: u.split(/\//)[-1].sub(/\.(flac|mp3|wav)$/,'')}}]}
