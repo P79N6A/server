f64a358f1ffc3e4305585ea9fb6703b446c1ef0b
anonymous <_@whats-your.name>
2014-04-18 01:27:00 +0000

    add RDFa flag

--- ruby/GET.rb
+++ ruby/GET.rb
@@ -67,11 +67,12 @@ class R
     end
 
     condResponse ->{                        puts [uri, @r['HTTP_USER_AGENT'], @r['HTTP_REFERER']].compact.join(' ')
-      if !%w{text/html}.member?(@r.format) && format = RDF::Writer.for(:content_type => @r.format)
+      writer = RDF::Writer.for :content_type => @r.format
+      if writer && ((@r.format != 'text/html') || q.has_key?('rdfa'))
         graph = RDF::Graph.new             # graph
-        set.map{|r| graph.load r.rdfDoc.d, :host => @r['SERVER_NAME']} # add resources
-        @r[:Response][:Triples] = graph.size.to_s # count
-        graph.dump format.to_sym           # RDF
+        set.map{|r| graph.load r.rdfDoc.d, :host => @r['SERVER_NAME']}
+        @r[:Response][:Triples] = graph.size.to_s # triple-count
+        graph.dump writer.to_sym           # RDF
       else
         set.map{|r|r.setEnv(@r).fileToGraph m}
         Render[@r.format][m, @r] # HTML
