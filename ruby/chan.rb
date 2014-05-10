68e247aea841219fc521c4b429ddbf76ffdd5cd5
anonymous <_@whats-your.name>
2014-05-10 06:44:57 +0000

    post

--- ruby/chan.rb
+++ ruby/chan.rb
@@ -5,36 +5,39 @@ class R
 
   GET['/ch'] = -> r,e {
     path = r.justPath.uri.sub(/^\/(board|ch|forum)\/*/,'/').tail
-    if path.match(/^[^\/]*\/?$/)
-      if path.empty? # chan/forum list
+    if path.match(/^[^\/]*\/?$/) # root-level or child
+      if path.empty? # list subs
         e.q['view'] ||= 'title'
         r.descend.setEnv(e).response
-      else # paged posts of (sub)forum
-        r.q['c'] ||= 16
-        r.q['set']  ||= 'page'
-        r.q['view'] ||= 'threads'
+      else # sub
+        r.q['set'] = 'ch'
+        e['sub'] = path
         nil
       end
-    elsif path.match /\/post$/# new
-      e.q['view'] = 'board_post_form'
+    elsif p = path.match(/([^\/]+)\/post$/) # new
+      e.q['view'] = 'boardPost-form'
+      e['sub'] = p[1]
       e.htmlResponse({})
     else # post
       
     end}
 
+  FileSet['ch'] = ->d,e,m{
+    m['#new'] = {Type => R['newBoardPost']}
+    FileSet['page'][d,e,m]}
+
   POST['/ch'] = -> d,e{
     p = (Rack::Request.new d.env).params # parse input
     content = p['content']
     if content && !content.empty?
-      host = '//' + e['SERVER_NAME']
-      path = Time.now.iso8601[0..18].gsub(/[-T]/,'/') + '.' + ( p['title'].do{|t|t.gsub /[?#\s\/]/,'_'} || rand.to_s.h[0..3] )
-      uri = host + '/' + path
+      uri = '//' + e['SERVER_NAME'] + '/' +
+        Time.now.iso8601[0..18].gsub(/[-T]/,'/') + '.' +
+        ( p['title'].do{|t|t.gsub /[?#\s\/]/,'_'} || rand.to_s.h[0..3] )
 
-      post = { # post as Hash+JSON
-        'uri' => uri,
+      post = {'uri' => uri,
         Type => R[SIOCt+'BoardPost'],
-        Content => CleanHTML[content],
-      }
+        Content => CleanHTML[content]}
+
       p['title'].do{|t| post[Title] = t.hrefs}
 
       # optional attachment
@@ -43,19 +46,19 @@ class R
         basename = file[:filename]
       end
 
-      doc = R[uri].jsonDoc      # doc
-      doc.w({uri => post},true) # save
+      R[uri].jsonDoc.w({uri=>post},true) # save
 
       [303,{'Location' => uri},[]]
     else
       [303,{'Location' => d.uri},[]]
     end}
 
-  View['board_post_form'] = -> d,e {
-    {_: :form, method: :POST, enctype: "multipart/form-data",
-      c: [{_: :input, title: :title, name: :title, size: 32},'<br>',
-          {_: :textarea, rows: 8, cols: 32, name: :content},'<br>',
-          {_: :input, type: :file, name: :file},
-          {_: :input, type: :submit, value: 'post '}]}}
+  View['newBoardPost'] = -> d,e {
+    ['post on ',{_: :b, c: e['sub'].hrefs},
+     {_: :form, method: :POST, enctype: "multipart/form-data",
+       c: [{_: :input, title: :title, name: :title, size: 32},'<br>',
+           {_: :textarea, rows: 8, cols: 32, name: :content},'<br>',
+           {_: :input, type: :file, name: :file},
+           {_: :input, type: :submit, value: 'post '}]}]}
 
 end
