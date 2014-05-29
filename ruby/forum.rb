c3fd01f307e1031ce50241aa7591e274dc60ca09
anonymous <_@whats-your.name>
2014-05-29 05:15:38 +0000

    ok

--- ruby/forum.rb
+++ ruby/forum.rb
@@ -16,7 +16,7 @@ class R
     elsif n = path.match(/^[^\/]+\/\d{4}\/\d\d\/\d\d\/([^\/]+)\/?$/) # thread
       e.q['set'] = 'page'
       e.q['view'] ||= 'timegraph'
-      r.descend.child('.p').setEnv(e).response # paginated osts
+      r.descend.child('.p').setEnv(e).response # paginated posts
     else
       nil
     end}
@@ -25,58 +25,68 @@ class R
     p = (Rack::Request.new d.env).params
     pathSegs = d.path.sub(/^\/forum/,'').tail.split '/'
 
-    sub = '//' + e['SERVER_NAME'] + '/forum/' + pathSegs[0] + '/'
-  title = p['title']
-content = p['content']
+    sub = '//' + e['SERVER_NAME'] + '/forum/' + pathSegs[0]
+  title = p['title'].do{|t| !t.empty? && t.hrefs}
+content = CleanHTML[p['content']]
    date = Time.now.iso8601
-
-    post = {
-      Date => date,
-      Type => R[SIOC+'Thread'],
-      Title => title && !title.empty? && title.hrefs || 'untitled',
-    }
 
     if sub && content && !content.empty?
 
-      if pathSegs.size == 1 # new thread
+      if pathSegs.size == 1 # post thread to subforum container
 
-        uri = sub + date[0..10].gsub(/[-T]/,'/') + (p['title'].do{|t|t.gsub /[?#\s\.\/]+/,'_'} || rand.to_s.h[0..3])
-        thread = post['uri'] = uri # minted URI of date + title
+        thread = sub + '/' + date[0..10].gsub(/[-T]/,'/') + (p['title'].do{|t|t.gsub /[?#\s\.\/]+/,'_'} || rand.to_s.h[0..3])
+        info = {
+          'uri' => thread,
+          Date => date,
+          Type => R[SIOC+'Thread'],
+          Title => title||'untitled',
+          SIOC+'has_container' => R[sub]}
 
-        uri.R.jsonDoc.w({uri=>post},true) # store thread info
+        info.R.jsonDoc.do{|d| d.w({thread => info},true) unless d.e}
 
+      else # thread container
+        thread = sub + '/' + pathSegs[1..4].join('/')
+      end
+
+      sig = content.h[0..8]
+      posts = thread + '/.p/'
+
+      if !R[posts+'*'+sig+'.e'].glob.empty? # duplicate
+        puts "duplicate post"
       else
-        thread = sub + pathSegs[1..4].join('/')
+        uri = posts + date.gsub(/\D/,'.') + sig
+        post = {
+          'uri' => uri,
+          Date => date,
+          Type => R[SIOCt+'BoardPost'],
+          SIOC+'has_container' => R[thread],
+          Content => content}
+        post[Title] = title if title
+
+        file = p['file'] # optional attachment
+        if file && file[:type].match(/^image/)
+          basename = file[:filename]
+        end
+
+        doc = post.R.jsonDoc
+        doc.w({uri=>post},true)
       end
-
-      uri = thread + '/.p/' + date.gsub(/\D/,'.')
-      post['uri'] = uri
-      post[Type] = R[SIOCt+'BoardPost']
-      post[SIOC+'has_container'] = R[thread]
-      post[Content] = CleanHTML[content]
-
-      file = p['file'] # optional attachment
-      if file && file[:type].match(/^image/)
-        basename = file[:filename]
-      end
-
-      uri.R.jsonDoc.w({uri=>post},true) # store post
 
       [303,{'Location' => thread},[]]
-    else # noop
+    else # content empty, abort
       [303,{'Location' => d.uri},[]]
     end}
-
+=begin
   View[SIOCt+'BoardPost'] = -> d,e {
     d.resourcesOfType(SIOCt+'BoardPost').map{|post|
       t = post[Title] || '#'
       {class: :boardPost, style: 'float: left',
         c: [{_: :a, href: post.uri, c: {_: :h3, c: t}}, post[Content]
            ]}}}
-
+=end
   View['subforum'] = -> d,e {
     [H.css('/css/forum', true),View[LDP+'Resource'][d,e],
-     d.resourcesOfType(SIOCt+'BoardPost').map{|post|
+     d.resourcesOfType(SIOC+'Thread').map{|post|
        {class: :post_info,
          c: [{_: :a, class: :title, href: post.uri, c: post[Title]},
              {class: :time, c: post[Date]},
