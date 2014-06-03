0312076fa2fbcaccec04628295c5628cbdf82f85
anonymous <_@whats-your.name>
2014-06-03 16:40:37 +0000

    ok

--- ruby/WebID.rb
+++ ruby/WebID.rb
@@ -1,36 +1,36 @@
-#watch __FILE__
+watch __FILE__
 class R
 
-  GET['/whoami'] = -> d,e { response = nil
+  GET['/whoami'] = -> d, e { r = nil
+    e.cert.do{|pem|
+      OpenSSL::X509::Certificate.new(pem).do{|x509| # parse
+        x509.extensions.find{|x|x.oid == 'subjectAltName'}.do{|user|
+          user = user.value.sub /^URI./, ''
+          graph = RDF::Repository.load user
+          query = "PREFIX : <http://www.w3.org/ns/auth/cert#> SELECT ?m ?e WHERE { <#{user}> :key [ :modulus ?m; :exponent ?e; ] . }"
+          SPARQL.execute query, graph do |result|
+            if x509.public_key.n.to_i == result[:m].value.to_i(16)
+              r = [302,{'Location'=>user},[]]
+            end
+          end}}}
+  r }
 
-    e['HTTP_SSL_CLIENT_CERT'].do{|v|
+end
 
-      p = v.split /[\s\n]/ # linebreaks sometimes munged into spaces upstream
+module Th
 
+  def cert
+    self['HTTP_SSL_CLIENT_CERT'].do{|v|
+      p = v.split /[\s\n]/
       unless p.size < 5
-
-        pem = [p[0..1].join(' '), # header
-               p[2..-3],          # body
-               p[-2..-1].join(' ')].join "\n" # format
-
-        OpenSSL::X509::Certificate.new(pem).do{|x509| # parse
-
-          x509.extensions.
-          find{|x|x.oid == 'subjectAltName'}.do{|altName| # user URI
-            uri = altName.value.sub /^URI:/, ''
-            pubkey = x509.public_key
-            m = pubkey.n # modulus
-            e = pubkey.e # exponent
-            graph = RDF::Repository.load uri
-            query = "PREFIX : <http://www.w3.org/ns/auth/cert#> SELECT ?m ?e WHERE { <#{uri}> :key [ :modulus ?m; :exponent ?e; ] . }"
-            SPARQL.execute query, graph do |result|
-              mCert = m.to_i
-              mWeb = result[:m].value.to_i 16
-              if mCert == mWeb
-                response = [302,{'Location'=>uri},[]]
-              end
-            end}}
+        p = [p[0..1].join(' '), # header
+             p[2..-3],          # body
+             p[-2..-1].join(' ')].join "\n" # format
+        puts p
+        p
+      else
+        nil
       end}
-    response }
+  end
 
 end
