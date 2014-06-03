4169a1b78155366f9fd2f83beeea4dd634d04dfa
anonymous <_@whats-your.name>
2014-06-03 16:51:00 +0000

    ok

--- ruby/WebID.rb
+++ ruby/WebID.rb
@@ -1,36 +1,30 @@
 watch __FILE__
 class R
 
-  GET['/whoami'] = -> d, e { r = nil
-    e.cert.do{|pem|
+  GET['/whoami'] = -> d,e {e.webID.do{|id|[303,{'Location'=>id},[]]}}
+
+end
+
+module Th
+
+  def webID
+    cert.do{|pem|
       OpenSSL::X509::Certificate.new(pem).do{|x509| # parse
         x509.extensions.find{|x|x.oid == 'subjectAltName'}.do{|user|
           user = user.value.sub /^URI./, ''
           graph = RDF::Repository.load user
           query = "PREFIX : <http://www.w3.org/ns/auth/cert#> SELECT ?m ?e WHERE { <#{user}> :key [ :modulus ?m; :exponent ?e; ] . }"
           SPARQL.execute query, graph do |result|
-            if x509.public_key.n.to_i == result[:m].value.to_i(16)
-              r = [302,{'Location'=>user},[]]
-            end
+            return user if x509.public_key.n.to_i == result[:m].value.to_i(16)
           end}}}
-  r }
-
-end
-
-module Th
+    nil
+  end
 
   def cert
     self['HTTP_SSL_CLIENT_CERT'].do{|v|
       p = v.split /[\s\n]/
-      unless p.size < 5
-        p = [p[0..1].join(' '), # header
-             p[2..-3],          # body
-             p[-2..-1].join(' ')].join "\n" # format
-        puts p
-        p
-      else
-        nil
-      end}
+      return [p[0..1].join(' '), p[2..-3], p[-2..-1].join(' ')].join "\n" unless p.size < 5}
+    nil
   end
 
 end
