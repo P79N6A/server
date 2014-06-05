9522b73d0cbae30ef1d843f1b7619721b0f84bc5
anonymous <_@whats-your.name>
2014-06-05 18:43:54 +0000

    ok

--- ruby/PUT.rb
+++ ruby/PUT.rb
@@ -3,8 +3,13 @@ class R
   def PUT
     mime = @r['CONTENT_TYPE'].split(';')[0]
     ext = '.' + MIME.invert[mime].to_s
-    doc = stripDoc.a ext 
+    versions = docroot.child('.v')
+    doc = versions.child Time.now.iso8601.gsub(/\W/,'') + ext 
     doc.w @r['rack.input'].read
+    cur = stripDoc.a(ext)
+    FileUtils.mv cur.pathPOSIX, versions.child('0'+ext).pathPOSIX if cur.node.file? && !cur.node.symlink?
+    cur.delete if cur.e
+    doc.ln_s cur
     [201,{ 'Location' => uri},[]]
   end
 end
