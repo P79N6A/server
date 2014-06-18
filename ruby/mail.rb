5cd73da95d4f2d17a940779c55d0b17bad7ab3f1
anonymous <_@whats-your.name>
2014-06-18 15:37:08 +0000

    ok

--- ruby/mail.rb
+++ ruby/mail.rb
@@ -22,7 +22,7 @@ class R
 
     v = r.q['view'] ||= "timegraph"
     r[:Response]['Access-Control-Allow-Origin'] = r['HTTP_ORIGIN'].do{|o|o.match(HTTP_URI) && o } || '*'
-    r[:Response]['Content-Type'] = r.format
+    r[:Response]['Content-Type'] = r.format + '; charset=UTF-8'
     r[:Response]['ETag'] = [(View[v] && v), m.keys.sort, r.format].h
 
     e.condResponse ->{Render[r.format][m, r]}}
