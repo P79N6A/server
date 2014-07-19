88406e78a8caa4bf5a2b0f491c82227ba255eb1e
anonymous <_@whats-your.name>
2014-07-19 04:12:53 +0000

    ok

--- ruby/ui.rb
+++ ruby/ui.rb
@@ -1,16 +1,11 @@
 watch __FILE__
 class R
-
-=begin  third-party UIs
-
- as default view on a subtree,
-
- GET['/'] = -> d,e { e.q['view'] ||= 'tabulate'; nil }
+=begin 
+ third-party UIs
 
 =end
 
   # https://github.com/linkeddata/tabulator
-  # generic data-browser
   View['tabulate'] = ->d=nil,e=nil {
     src = 'https://w3.scripts.mit.edu/tabulator/'
     [(H.css src + 'tabbedtab'),(H.js 'https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min'),(H.js src + 'js/mashup/mashlib'),
@@ -24,11 +19,19 @@ class R
      {class: :TabulatorOutline, id: :DummyUUID},{_: :table, id: :outline}]}
 
   # https://github.com/linkeddata/ldphp
-  # directory viewer and ACL-editor
   View['fm'] = ->d=nil,e=nil {
-    [
-     {class: :editor, id: :editor},
-     {class: 'wac-editor', id: 'wac-editor'},
+    [%w{
+//cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min
+//cdnjs.cloudflare.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min
+//cdnjs.cloudflare.com/ajax/libs/angular.js/1.1.5/angular.min
+//cdnjs.cloudflare.com/ajax/libs/angular-ui/0.4.0/angular-ui.min
+//w3.scripts.mit.edu/rdflib.js/dist/rdflib
+/common/js/prototype
+/common/js/common
+/js/fm
+}.map{|s| H.js s},
+     {class: :editor, id: :editor, c: "editor"},
+     {class: 'wac-editor', id: 'wac-editor', c: "wac-edit"},
      {class: :cloudactions},
     ]}
 
