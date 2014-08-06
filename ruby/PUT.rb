8f3086b1e2a4006406e1cae080763ae4c38da366
anonymous <_@whats-your.name>
2014-08-06 21:48:15 +0000

    accept PUT for whitelisted suffixes

--- ruby/PUT.rb
+++ ruby/PUT.rb
@@ -10,21 +10,24 @@ class R
   end
 
   def inPUT
-    ext = '.' + MIME.invert[@r['CONTENT_TYPE'].split(';')[0]].to_s # suffix from MIME
+    ext = MIME.invert[@r['CONTENT_TYPE'].split(';')[0]].to_s # suffix from MIME
+    return [406,{},[]] unless %q{gif html jpg json jsonld pdf png n3 ttl txt}.member? ext
 
-    versions = docroot.child '.v' # container for states
+    # container for states
+    versions = docroot.child '.v'
 
     # identifier for current version
-    doc = versions.child Time.now.iso8601.gsub(/\W/,'') + ext 
+    doc = versions.child Time.now.iso8601.gsub(/\W/,'') + '.' + ext 
 
     body = @r['rack.input'].read
-    puts body
+#    puts body
     doc.w body # create version
 
-    cur = stripDoc.a(ext) # resource URI
+    cur = stripDoc.a('.' + ext)
 
-    cur.delete if cur.e # wipe obsolete target
+    cur.delete if cur.e # unlink obsolete-version
     doc.ln_s cur        # link current
+
     [201,{
        'Location' => uri,
        'Access-Control-Allow-Origin' => @r['HTTP_ORIGIN'].do{|o|o.match(HTTP_URI) && o } || '*',
