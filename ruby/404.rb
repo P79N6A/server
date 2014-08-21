3bc56c1c88672ad428c6e1c1a47cec8463c741ed
anonymous <_@whats-your.name>
2014-08-21 21:43:37 +0000

    turn paths + URIs in 404-headers into resources

--- ruby/404.rb
+++ ruby/404.rb
@@ -2,16 +2,24 @@ class R
 
   E404 = -> e,r,g=nil {
     g ||= {}            # graph
-    s = g[e.uri] ||= {} # resource
-    path = e.justPath   # path
-    s[Title] = '404'    # title
+    s = g[e.uri] ||= {} # request-resource
+
+    s[Title] = '404'
     s[Edit] = R[e.docroot+'?view=edit']
-    s[RDFs+'seeAlso'] = [e.parentURI, path.a('*').glob, e.a('*').glob] unless path.to_s == '/'
-    s['#query'] = Hash[r.q.map{|k,v|[k.to_s.hrefs,v.to_s.hrefs]}]
+    s[RDFs+'seeAlso'] = e.parentURI unless e.justPath.to_s == '/'
+    s[Header+'query-string'] = Hash[r.q.map{|k,v|[k.to_s.hrefs,v.to_s.hrefs]}]
     s[Header+'accept'] = r.accept
-    %w{CHARSET LANGUAGE ENCODING}.map{|a| s[Header+'accept-'+a.downcase] = r.accept_('_'+a)}
-    r.map{|k,v|
-      s[Header+k.to_s.sub(/^HTTP_/,'').downcase.gsub('_','-')] = v unless [:Links,:Response].member?(k)}
+
+  %w{CHARSET LANGUAGE ENCODING}.map{|a|
+    s[Header+'accept-'+a.downcase] = r.accept_('_'+a)}
+
+  r.map{|k,v|
+    s[Header+k.to_s.sub(/^HTTP_/,'').downcase.gsub('_','-')] = v unless [:Links,:Response].member?(k)}
+
+    %w{path-info referer request-path request-uri}.map{|p|
+       k = Header + p
+    s[k] = R[s[k]] if s[k]}
+
     r.q['view'] = 'HTML'
     [404,{'Content-Type'=> 'text/html'},[Render['text/html'][g,r]]]}
 
