2c3dcad7a16ec2ba0b1a9fb341cda70209e93c44
anonymous <_@whats-your.name>
2014-09-15 03:51:53 +0000

    feeds

--- ruby/user.rb
+++ ruby/user.rb
@@ -5,12 +5,13 @@ class R
   GET['/whoami'] = -> d,e { # direct to user URI
     e.user.do{|u|[303,{'Location'=>u.uri},[]]}}
 
-  GET['/login'] = -> e,r {
-    form = {_: :form, action: '/login', method: :POST,
+  LoginForm = {_: :form, action: '/login', method: :POST,
       c: [{_: :input, name: :user, placeholder: :username},
           {_: :input, name: :passwd, type: :password, placeholder: :password},
           {_: :input, type: :submit, value: :login}]}
-    graph = {e.uri => {'uri' => e.uri, Content => H(form)}}
+
+  GET['/login'] = -> e,r {
+    graph = {e.uri => {'uri' => e.uri, Content => H(LoginForm)}}
     [200, {'Content-Type' => r.format + '; charset=UTF-8'},
      [Render[r.format][graph,r]]]}
 
