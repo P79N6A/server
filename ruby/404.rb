46d188972f966dd63727786e4320a82a1595013e
anonymous <_@whats-your.name>
2014-10-03 10:40:36 +0000

    ok

--- ruby/404.rb
+++ ruby/404.rb
@@ -3,10 +3,6 @@ class R
   E404 = -> e,r,g=nil {
     g ||= {}            # graph
     s = g[e.uri] ||= {} # request-resource
-
-    s[Title] = '404'
-    s['User'] = r.user
-    s[RDFs+'seeAlso'] = e.parentURI unless e.justPath.to_s == '/'
     s[Header+'query-string'] = Hash[r.q.map{|k,v|[k.to_s.hrefs,v.to_s.hrefs]}]
     s[Header+'accept'] = r.accept
 
