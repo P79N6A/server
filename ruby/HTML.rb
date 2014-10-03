ffa366058286ede02e15c4f896d87bfc20e41b61
anonymous <_@whats-your.name>
2014-10-03 08:35:31 +0000

    ok

--- ruby/HTML.rb
+++ ruby/HTML.rb
@@ -191,7 +191,7 @@ class R
        currentDir = path == e.R.justPath
        {class: 'dir ' + (currentDir ? 'thisdir' : ''), style: "background-color: #{R.cs}",
          c: [({_: :a, class: :up, c: '&uarr;', href: path.parentURI.descend} if currentDir && path != '/'),
-             {_: :a, c: resource.abbr, href: resource.uri, rel: :nofollow},
+             {_: :a, c: resource.abbr, href: resource.uri},
              r[RDFs+'member'].do{|c|c.map{|c| c = c.R
                  {_: :a, href: c.uri, class: :member, c: e[:Graph][c.uri].do{|r|r[Label]} || c.abbr}}}]}}]}
 
