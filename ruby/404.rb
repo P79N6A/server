2ddbaa9c9118ea7c4c90960326c3acacc040b3d0
anonymous <_@whats-your.name>
2014-10-14 04:23:22 +0000

    ok

--- ruby/404.rb
+++ ruby/404.rb
@@ -3,12 +3,13 @@ class R
   E404 = -> e,r,g=nil {
     g||={} # graph
     s = g[e.uri] ||= {} # resource
-
-    s[Header+'query-string'] = Hash[r.q.map{|k,v|[k.to_s.hrefs,v.to_s.hrefs]}]
-    s[Header+'accept'] = r.accept
-    %w{CHARSET LANGUAGE ENCODING}.map{|a| s[Header+'accept-'+a.downcase] = r.accept_('_'+a)}
-    r.map{|k,v| s[Header+k.to_s] = v}
-
+    s['#query-string'] = Hash[r.q.map{|k,v|[k.to_s.hrefs,v.to_s.hrefs]}]
+    s['#accept'] = r.accept
+    %w{CHARSET LANGUAGE ENCODING}.map{|a|
+      s['#accept-'+a.downcase] = r.accept_('_'+a)}
+    r.map{|k,v| s['#'+k.to_s] = v}
+    s['#SERVER_NAME'] = R['//'+s['#SERVER_NAME']]
+    s['#uri'] = R[s['#uri']]
     r.q.delete 'view'
     [404, r[:Response].update({'Content-Type'=>'text/html'}), [Render['text/html'][g,r]]]}
 
