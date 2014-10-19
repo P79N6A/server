de4dfddd6e1bec09fb500a35fdfe6b930c5a30d9
anonymous <_@whats-your.name>
2014-10-19 12:30:37 +0000

    move graph-serializers to graph.rb

--- ruby/graph.rb
+++ ruby/graph.rb
@@ -108,6 +108,27 @@ class R
     doc
   end
 
+  # graph -> RDF format
+  def R.renderRDF d,f,e
+    (RDF::Writer.for f).buffer{|w| # init writer
+      d.triples{|s,p,o|            # structural triples of Hash::Graph
+        s && p && o &&             # all fields non-nil
+        (s = RDF::URI s            # subject-URI
+         p = RDF::URI p            # predicate-URI
+         o = (if [R,Hash].member? o.class
+                RDF::URI o.uri     # object URI ||
+              else                 # object Literal
+                l = RDF::Literal o
+                l.datatype=RDF.XMLLiteral if p == Content
+                l
+              end) rescue nil
+         (w << (RDF::Statement.new s,p,o) if o) rescue nil )}}
+  end
+
+  [['application/ld+json',:jsonld],['application/rdf+xml',:rdfxml],['text/plain',:ntriples],['text/turtle',:turtle],['text/n3',:n3]].
+    map{|mime|
+    Render[mime[0]] = ->d,e{ R.renderRDF d, mime[1], e}}
+
 end
 
 class Hash
