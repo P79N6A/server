dbb18dfaadf85d521495a397d7f7af327734f616
anonymous <_@whats-your.name>
2014-11-02 13:36:20 +0000

    ok

--- ruby/404.rb
+++ ruby/404.rb
@@ -2,20 +2,24 @@
 class R
 
   E404 = -> e,r,g=nil {
-    r[:Response].update({'Content-Type'=>'text/html'})
-    g||={} # graph
-    s = g[e.uri] ||= {} # resource
-    s['#query-string'] = Hash[r.q.map{|k,v|[k.to_s.hrefs,v.to_s.hrefs]}]
-    s['#accept'] = r.accept
-    %w{CHARSET LANGUAGE ENCODING}.map{|a|
-      s['#accept-'+a.downcase] = r.accept_('_'+a)}
-    r.map{|k,v| s['#'+k.to_s] = v.class==Hash ? v.dup : v}
-    s['#SERVER_NAME'] = R['//'+s['#SERVER_NAME']]
-    s['#uri'] = R[s['#uri']]
-    s['#HTTP_REFERER'].do{|r|s['#HTTP_REFERER']=R[r]}
-    s['#Response'].do{|r|r.delete 'Content-Type'}
-    r.q.delete 'view'
-    [404, r[:Response], [Render['text/html'][g,r]]]}
+    r[:Response].update({'Content-Type'=>r.format})
+    if r.format == 'text/html'
+      g||={} # graph
+      s = g[e.uri] ||= {} # resource
+      s['#query-string'] = Hash[r.q.map{|k,v|[k.to_s.hrefs,v.to_s.hrefs]}]
+      s['#accept'] = r.accept
+      %w{CHARSET LANGUAGE ENCODING}.map{|a|
+        s['#accept-'+a.downcase] = r.accept_('_'+a)}
+      r.map{|k,v| s['#'+k.to_s] = v.class==Hash ? v.dup : v}
+      s['#SERVER_NAME'] = R['//'+s['#SERVER_NAME']]
+      s['#uri'] = R[s['#uri']]
+      s['#HTTP_REFERER'].do{|r|s['#HTTP_REFERER']=R[r]}
+      s['#Response'].do{|r|r.delete 'Content-Type'}
+      r.q.delete 'view'
+      [404, r[:Response], [Render['text/html'][g,r]]]
+    else
+      [404, r[:Response], []]
+    end}
 
   def status
     uris.map{|u|
