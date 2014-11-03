973793be7840ab13877ee59ac33649d62c7d53ee
anonymous <_@whats-your.name>
2014-11-03 21:52:00 +0000

    ok

--- ruby/ls.rb
+++ ruby/ls.rb
@@ -11,44 +11,44 @@ class R
     sortType = [mtime,Stat+'size'].member?(sort) ? :to_i : :to_s
     entries = d.values.sort_by{|v|(v[sort].justArray[0] || 0).send sortType}.send(asc ? :id : :reverse)
 
-    [{_: :table, class: :ls,
-       c: [{_: :tr, c: keys.map{|k| # header row
-              {_: :th, class: (k == sort ? 'this' : 'that'),
-               property: k, c: {_: :a, href: path+'?sort='+k.shorten+(asc ? '' : '&asc=asc'), c: k.R.fragment || k.R.basename}}}},
-           entries.map{|e| # entries
-             types = e.types
-             directory = types.include?(Stat+'Directory')
-             file = types.include?(Stat+'File')
-             re = file ? e.R.stripDoc.a('.html') : e.R
-             {_: :tr, uri: re.uri,
-               c: keys.map{|k|
-                 {_: :td, property: k, class: (k == sort ? 'this' : 'that'),
-                   c: case k
-                      when 'uri'
-                        (file ? re : e.R).href(e[Title] || e.R.basename.do{|b| b == '/' ? e.R.host : URI.unescape(b)})
-                      when mtime
-                        e[k].do{|t| Time.at(t[0]).iso8601.sub /\+00:00$/,''}
-                      when Type
-                        if directory
-                          {_: :a, class: :dir, href: e.uri, c: '►'}
-                        elsif types.include?(DC+'Image')
-                          ShowImage[e.uri]
-                        elsif types.size==1 && types[0]==RDFs+'Resource'
-                          {_: :a, class: :resource, href: e.uri, c: '■'}
-                        elsif file
-                          {_: :a, class: :file, href: e.uri, c: '█'}
-                        else
-                          e[k].html
-                        end
-                      when LDP+'contains'
-                        e[k].justArray.map(&:maybeURI).map{|r|r && [r.R.href,' ']}
-                      else
-                        e[k].html
-                      end}}}}]},
+    [{_: :a, class: :warp, href: e.warp, c: '/'},
+     {_: :table, class: :ls,
+      c: [{_: :tr, c: keys.map{|k| # header row
+             {_: :th, class: (k == sort ? 'this' : 'that'),
+              property: k, c: {_: :a, href: path+'?sort='+k.shorten+(asc ? '' : '&asc=asc'), c: k.R.fragment || k.R.basename}}}},
+          entries.map{|e| # entries
+            types = e.types
+            directory = types.include?(Stat+'Directory')
+            file = types.include?(Stat+'File')
+            re = file ? e.R.stripDoc.a('.html') : e.R
+            {_: :tr, uri: re.uri,
+             c: keys.map{|k|
+               {_: :td, property: k, class: (k == sort ? 'this' : 'that'),
+                c: case k
+                   when 'uri'
+                     (file ? re : e.R).href(e[Title] || e.R.basename.do{|b| b == '/' ? e.R.host : URI.unescape(b)})
+                   when mtime
+                     e[k].do{|t| Time.at(t[0]).iso8601.sub /\+00:00$/,''}
+                   when Type
+                     if directory
+                       {_: :a, class: :dir, href: e.uri, c: '►'}
+                     elsif types.include?(DC+'Image')
+                       ShowImage[e.uri]
+                     elsif types.size==1 && types[0]==RDFs+'Resource'
+                       {_: :a, class: :resource, href: e.uri, c: '■'}
+                     elsif file
+                       {_: :a, class: :file, href: e.uri, c: '█'}
+                     else
+                       e[k].html
+                     end
+                   when LDP+'contains'
+                     e[k].justArray.map(&:maybeURI).map{|r|r && [r.R.href,' ']}
+                   else
+                     e[k].html
+                   end}}}}]},
      (H.css '/css/ls', true),
      (H.js '/js/ls', true),
-     ({_: :style, c: "table.ls {width: 100%}"} if e.q['view']=='ls'),
-     {_: :a, class: :warp, href: e.warp, c: '/'}]}
+     ({_: :style, c: "table.ls {width: 100%}"} if e.q['view']=='ls')]}
 
   ViewGroup[Stat+'Directory'] = View['ls']
   ViewGroup[Stat+'File']      = View['ls']
