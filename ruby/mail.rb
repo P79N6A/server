97cf859fc07a629731628c008b63c85ae70291ca
anonymous <_@whats-your.name>
2014-11-09 02:01:07 +0000

    dump unparsed ACCEPT on thread

--- ruby/mail.rb
+++ ruby/mail.rb
@@ -15,8 +15,8 @@ class R
   GET['/mid'] = -> e,r{R[MessagePath[e.basename]].setEnv(r).response} # message-ID lookup
   GET['/msg'] = -> e,r{e.path=='/msg/' ? E404[e,r] : nil}
   
-  GET['/thread'] = -> e, r {
-    puts 'thread ' + r.format
+  GET['/thread'] = -> e,r {
+    puts 'thread',r.format,r['HTTP_ACCEPT']
     return [406,{},[]] unless Render[r.format]
     m = {} # graph
     R[MessagePath[e.basename]].walk SIOC+'reply_of', m # find graph
