c20b554f368c48c30ff48d0438a64012a894845a
anonymous <_@whats-your.name>
2014-11-26 19:18:41 +0000

    ok

--- ruby/vis.rb
+++ ruby/vis.rb
@@ -48,7 +48,7 @@ class R
     [(H.js '//d3js.org/d3.v2'), # D3 library
      {_: :script, c: "var links = #{links.to_json};"}, # graph-arcs to JSON
      H.js('/js/force',true), H.css('/css/force',true), H.css('/css/mail',true),
-     {_: :a, href: hide ? '?' : '?view=unquoted', c: hide ? '&gt;' : '&lt;', title: "#{hide ? 'show' : 'hide'} quotes", style: 'position: fixed; top: .2em; right: .2em; z-index: 2; border-radius: .15em; font-size: 2.3em; color: #aaa; background-color: #fff; border: .05em solid #bbb'},
+     {_: :a, href: hide ? '?' : '?view=unquoted', c: hide ? '&gt;' : '&lt;', title: "#{hide ? 'show' : 'hide'} quotes", style: 'position: fixed; top: .2em; right: .2em; z-index: 2; border-radius: .1em; font-size: 2.3em; color: #aaa; background-color: #fff; border: .05em solid #bbb'},
      DefaultView[Hash[d.sort_by{|u,r| # sort graph by date before rendering
                        r.class==Hash ? r[Date].justArray[0].to_s : ''}.reverse],e]]}
 
