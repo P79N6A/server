c2bd3407969a2dd79cc96a04206d6886e98795a5
anonymous <_@whats-your.name>
2014-11-26 20:57:36 +0000

    ok

--- ruby/ls.rb
+++ ruby/ls.rb
@@ -2,12 +2,12 @@
 #watch __FILE__
 class R
 
-  ViewGroup[Stat+'Directory'] = ViewGroup[Stat+'File'] = ViewGroup[RDFs+'Resource'] = ->d=nil,e=nil {
+  ViewGroup[Stat+'Directory'] = ViewGroup[Stat+'File'] = ViewGroup[RDFs+'Resource'] = ->d,env {
     mtime = Stat+'mtime'
     keys = [Stat+'size', 'uri', Type, mtime, LDP+'contains']
-    path = e['REQUEST_PATH']
-    asc = e.q.has_key? 'asc'
-    sort = e.q['sort'].do{|p|p.expand} || mtime
+    path = env['REQUEST_PATH']
+    asc = env.q.has_key? 'asc'
+    sort = env.q['sort'].do{|p|p.expand} || mtime
     sortType = [mtime,Stat+'size'].member?(sort) ? :to_i : :to_s
     if d['..']
       d.delete '..'
@@ -15,7 +15,7 @@ class R
     end
     entries = d.values.sort_by{|v|(v[sort].justArray[0] || 0).send sortType}.send(asc ? :id : :reverse)
 
-    [({_: :a, class: :up, href: '..', title: Pathname.new(e.R.path).parent.basename, c: '&uarr;'} if up),
+    [({_: :a, class: :up, href: '..', title: Pathname.new(path).parent.basename, c: '&uarr;'} if up),
      !entries.empty? && {_: :table, class: :ls,
         c: [{_: :tr, c: keys.map{|k| # header-row
              {_: :th, class: (k == sort ? 'this' : 'that'),
@@ -31,12 +31,15 @@ class R
                {_: :td, property: k, class: (k == sort ? 'this' : 'that'),
                 c: case k
                    when 'uri'
-                     (file ? re : e.R).href(e[Label] || e[Title] || URI.unescape(e.R.abbr))
+                     href = file ? re : e.R
+                     {_: :a, href: href.uri,
+                      c: e[Label] || e[Title] || (URI.unescape e.R.abbr),
+                      style: path==href.path ? "background-color: #bbb" : nil}
                    when mtime
                      e[k].do{|t| Time.at(t[0]).iso8601.sub /\+00:00$/,''}
                    when Type
                      if directory || container
-                       {_: :a, class: :dir, href: e.uri, c: '►'}
+                       {_: :a, class: :dir, href: e.uri, c: '►', style: path==e.R.path ? "color: #bbb" : nil}
                      elsif types.include?(DC+'Image')
                        ShowImage[e.uri]
                      elsif types.size==1 && types[0]==RDFs+'Resource'
