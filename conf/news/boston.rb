31d10aa0dba5e0b45e5d62ef846b473093118422
anonymous <_@whats-your.name>
2014-12-02 05:11:06 +0000

    ok

--- conf/news/boston.rb
+++ conf/news/boston.rb
@@ -1,8 +1,6 @@
 #!/usr/bin/env ruby
 require 'ww/constants'
 hostname = ARGV[0]
-src = Pathname.new File.expand_path File.dirname __FILE__   # script location
-rel = src.relative_path_from(Pathname.new `pwd`.chomp).to_s # path to scripts
-R(rel+'/boston.tw').tw hostname
+scriptDir = (Pathname.new File.expand_path File.dirname __FILE__).relative_path_from Pathname.new `pwd`.chomp
+R[scriptDir.to_s + '/boston.tw'].tw hostname
 'http://www.reddit.com/r/boston+cambridgema+eastie+roxbury+somerville/new/.rss?sort=new'.R.getFeed hostname
-
