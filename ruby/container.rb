2c904aad35628222739b289f49e4e56ed1b8a22f
anonymous <_@whats-your.name>
2014-12-04 06:15:02 +0000

    ok

--- ruby/container.rb
+++ ruby/container.rb
@@ -38,7 +38,7 @@ class R
 
     [{class: :container, style: "background-color: #{R.cs}",
       c: [{_: :a, class: :uri, href: re.uri,
-           c: r[Label] || (re.path=='/' ? re.host : re.abbr)},
+           c: r[Label] || (re.path=='/' ? re.host : re.abbr)},' ',
           H.once(e,:sortButton,
                  {_: :a, class: :sort, c: sortLabel, href: sortQ, title: s_}), "<br>\n",
           r[LDP+'contains'].do{|c|
@@ -46,7 +46,9 @@ class R
             maxSize = sizes.max
             sized = !sizes.empty? && maxSize > 1
             width = maxSize.to_s.size
-            c.sort_by{|i|((i.class==Hash && i[sort] || i.uri).justArray[0]||0).send sortType}.send((sized || sort==Date) ? :reverse : :id).map{|r|
+            c.sort_by{|i|
+              ((i.class==Hash && i[sort] || i.uri).justArray[0]||0).send sortType}.
+              send((sized || sort==Date) ? :reverse : :id).map{|r|
               data = r.class == Hash
               {_: :a, href: r.R.uri, class: :member,
                c: [(if data && sized && r[size]
@@ -62,7 +64,7 @@ class R
             c: [{_: :input, name: :q},
                 {_: :input, type: :hidden, name: :set, value: :grep}]}
            end)
-         ]},{_: :p, style: 'display: inline'},
+         ]},
      (H.once e, 'container', (H.css '/css/container',true))]}
 
   ViewA[LDP+'Resource'] = -> u,e {
@@ -84,7 +86,6 @@ class R
     sort = 'uri' if sort == Title
     sortType = [mtime,Stat+'size'].member?(sort) ? :to_i : :to_s
 
-    # . and ..
     this = d.delete env.uri if d[env.uri]
     if d['..']
       d.delete '..'
@@ -92,8 +93,9 @@ class R
     end
 
     entries = d.values.sort_by{|v|(v[sort].justArray[0] || 0).send sortType}.send(asc ? :id : :reverse)
-    [({_: :a, class: :up, href: '..', title: Pathname.new(path).parent.basename, c: '&uarr;'} if up), # ..
-     (ViewA[Container][this,env] if this),                                                            # .
+
+    [({_: :a, class: :up, href: '..', title: Pathname.new(path).parent.basename, c: '&uarr;'} if up),
+     (ViewA[Container][this,env] if this),
      ({_: :table, class: :ls,
        c: [{_: :tr, c: keys.map{|k|
               {_: :th, class: (k == sort ? 'this' : 'that'),
@@ -103,6 +105,7 @@ class R
              types = e.types
              container = types.include?(Container)
              directory = types.include?(Stat+'Directory')
+             containerType = container || directory
              file = types.include?(Stat+'File')
 
              {_: :tr, uri: e.uri,
@@ -110,29 +113,33 @@ class R
                 {_: :td, property: k, class: (k == sort ? 'this' : 'that'),
                  c: case k
                     when 'uri'
-                      {_: :a, href: (file ? e.R.stripDoc.a('.html') : e).uri, c: e[Label]||e[Title]||URI.unescape(e.R.abbr)} unless container || directory
+                      unless containerType
+                        {_: :a, href: (file ? e.R.stripDoc.a('.html') : e).uri,
+                         c: e[Label]||e[Title]||URI.unescape(e.R.abbr)}
+                      end
                     when mtime
                       e[k].do{|t| Time.at(t[0]).iso8601.sub /\+00:00$/,''}
                     when Type
-                      if directory || container
+                      if containerType
                         {_: :a, class: :dir, href: e.uri, c: '►'}
-                      elsif types.include?(DC+'Image')
-                        ShowImage[e.uri]
-                      elsif types.size==1 && types[0]==RDFs+'Resource'
-                        {_: :a, class: :resource, href: e.uri, c: '■'}
                       elsif file
                         {_: :a, class: :file, href: e.uri, c: '█'}
+                      elsif types.include?(DC+'Image')
+                        ShowImage[e.uri]
+                      elsif types.include?(RDFs+'Resource')
+                        {_: :a, class: :resource, href: e.uri, c: '■'}
                       else
                         e[k].html
                       end
                     when LDP+'contains'
-                      ViewA[Container][e,env] if container || directory
+                      ViewA[Container][e,env] if containerType
                     when Stat+'size'
                       e[Stat+'size'] unless e[LDP+'contains']
                     else
                       e[k].html
                     end}}}}]} unless entries.empty?),
-     (H.css '/css/ls',true), (H.js '/js/ls',true)]}
+     (H.css '/css/ls',true),
+     (H.js '/js/ls',true)]}
 
   [Container, LDP+'Resource', 'default'].map{|type|
     ViewGroup[type] = -> g,e {g.map{|u,r|ViewA[type][r,e]}}}
