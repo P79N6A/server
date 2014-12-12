3379e3387e3101e23c1aacf3b633114397ff1533
anonymous <_@whats-your.name>
2014-12-12 03:38:01 +0000

    ok

--- ruby/HTTP.rb
+++ ruby/HTTP.rb
@@ -111,7 +111,7 @@ class R
   E404 = -> e,r,graph=nil {
     g = {}
     if graph && r.format=='text/html' && e.path != '/'
-      g['..'] = {'uri' => '..', Type => R[Directory]}
+      g['..'] = {Type => R[LDP+'Resource']}
       g[e.uri] = graph[""]
     end
     [:Links,:Response].map{|p|r.delete p}
