0944af84fe4a9adeff70d134d93bca7b6cb0827e
anonymous <_@whats-your.name>
2014-12-13 13:14:44 +0000

    ok

--- ruby/schema.rb
+++ ruby/schema.rb
@@ -4,7 +4,7 @@ class R
   ViewGroup[RDFClass] =  ViewGroup[OWL+'Class'] = -> g,e {
     ['<br>',{_: :b, style: "font-size:1.6em", c: 'Class'}, ViewGroup[CSVns+'Row'][g,e]]}
 
-  ViewGroup[Property] = -> g,e {
+  ViewGroup[Property] = ViewGroup[OWL+'ObjectProperty'] = -> g,e {
     ['<br>',{_: :b, style: "font-size:1.3em", c: 'Properties'}, ViewGroup[CSVns+'Row'][g,e]]}
 
   ViewGroup[RDFs+'Datatype'] = -> g,e {
