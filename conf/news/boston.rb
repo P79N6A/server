449bdda39005902750743b6d11e8eb9c8f0a7339
anonymous <_@whats-your.name>
2014-12-27 00:48:47 +0000

    skelewu

--- conf/news/boston.rb
+++ conf/news/boston.rb
@@ -3,4 +3,4 @@ require 'ww/constants'
 hostname = ARGV[0]
 scriptDir = (Pathname.new File.expand_path File.dirname __FILE__).relative_path_from Pathname.new `pwd`.chomp
 R[scriptDir.to_s + '/boston.tw'].tw hostname
-'http://www.reddit.com/r/boston+cambridgema+eastie+roxbury+somerville/new/.rss?sort=new'.R.getFeed hostname
+'http://www.reddit.com/r/boston+bikeboston+bostontrees+cambridgeMA+somerville+SouthShore+massachusetts+roxbury+eastie+QuincyMA/new/.rss?sort=new'.R.getFeed hostname
