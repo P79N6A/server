7d5a25ecfaf11b224daf450aa7c69d9716da5663
anonymous <_@whats-your.name>
2015-02-22 06:56:20 +0000

    ok

--- ruby/mail.rb
+++ ruby/mail.rb
@@ -22,6 +22,7 @@ class R
     r[:Response]['ETag'] = [m.keys.sort, r.format].h
     e.condResponse ->{
       r[:thread] = true
+      r[:title] = m.values.find{|r|r.class==Hash&&r[Title]}[Title][0].to_s
       Render[r.format].do{|p|p[m,r]} ||
       m.toRDF.dump(RDF::Writer.for(:content_type => r.format).to_sym, :standard_prefixes => true, :prefixes => Prefixes)}}
 
