eb146d196a295c0b6e5b82918512e4d2ce972c6a
anonymous <_@whats-your.name>
2015-03-09 20:29:29 +0000

    ok

--- js/audio.js
+++ js/audio.js
@@ -3,6 +3,16 @@ document.addEventListener("DOMContentLoaded", function(){
     var a = document.querySelector('#audio')
     a.addEventListener('canplay',a.play,false)
 
+    document.body.addEventListener('keypress',function(e){
+	if(e.keyCode == 32){
+	    if(a.paused){
+		a.play()
+	    } else {
+		a.pause()
+	    }
+	}
+    },false)
+
     var trax = document.querySelectorAll('#sounds .member')
 
     var select = function(){
