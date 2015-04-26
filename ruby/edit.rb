1adace8fbed8f3dc58ed8b17cf9fd93d235bb7bb
anonymous <_@whats-your.name>
2015-04-26 21:02:17 +0000

    ok

--- ruby/edit.rb
+++ ruby/edit.rb
@@ -55,31 +55,7 @@ class R
         content.hrefs
       end}}
 
-  ViewGroup[SIOCt+'WikiArticle'] = -> g,e {g.map{|u,r|ViewA[SIOCt+'WikiArticle'][r,e]}}
-  ViewGroup[SIOCt+'WikiArticleSection'] = -> g,e {g.map{|u,r|ViewA[SIOCt+'WikiArticleSection'][r,e]}}
-
-  ViewA[SIOCt+'WikiArticle'] = -> r,e {
-    doc = r.R.docroot.uri
-    [{_: :h1, c: {_: :a, href: r.uri, c: r[Title]}},
-     Render[WikiText][r[WikiText]],
-     ([{_: :a, style: 'font-size:1.5em',href: r.R.editLink(e), c: R.pencil, title: 'edit article description'},
-       {_: :a, href: doc + '?new&type=sioct:WikiArticleSection', c: '+section', title: 'add section'}] if e.editable),
-    ]}
-
-  ViewA[SIOCt+'WikiArticleSection'] = -> r,e {
-    {class: :section,
-     c: [{_: :a,
-          class: :sectionTitle,
-          href: r.uri,
-          c: r[Title]},
-         '<br>',
-         Render[WikiText][r[WikiText]],
-         ({_: :a,
-           href: r.R.docroot +  '?edit&fragment=' + r.R.fragment,
-           class: :edit,
-           c: '✑'} if e.editable)]}}
-
-  # bind type in query-string
+  # present type-selection dialog
   ViewGroup['#untyped'] = -> graph, e {
     Creatable.map{|c|
       {_: :a, style: 'font-size: 2em; display:block', c: c.R.fragment,
