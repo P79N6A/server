41f25ac3b9a19cf25c7473e6fb07a27aa872d170
anonymous <_@whats-your.name>
2015-05-24 06:37:40 +0000

    ok

--- ruby/mail.rb
+++ ruby/mail.rb
@@ -18,11 +18,12 @@ class R
     ['', 'address', tld, domain[0], domain, *dname[2..-1], '@', id.h[0..1], msg].join('/')}
 
   AddrPath = ->address{ # email-address -> path
+#    puts "address #{address}"
     address = address.downcase
     person, domainname = address.split '@'
-    dname = domainname.split('.').reverse
+    dname = (domainname||'').split('.').reverse
     tld = dname[0]
-    domain = dname[1]
+    domain = dname[1] || 'localdomain'
     ['', 'address', tld, domain[0], domain, *dname[2..-1], person,''].join('/') + person + '#' + person}
 
   GET['/address'] = -> e,r {e.justPath.response} # we dont use the host
@@ -35,7 +36,10 @@ class R
     r[:Response]['ETag'] = [m.keys.sort, r.format].h
     e.condResponse ->{
       r[:thread] = true
-      r[:title] = m.values.find{|r|r.class==Hash&&r[Title]}[Title][0].sub ReExpr, ''
+      m.values.find{|r|
+        r.class == Hash && r[Title]}.do{|t|
+        title = t.justArray[0]
+        r[:title] = title.sub ReExpr, '' if title.class==String}
       Render[r.format].do{|p|p[m,r]} ||
       m.toRDF.dump(RDF::Writer.for(:content_type => r.format).to_sym, :standard_prefixes => true, :prefixes => Prefixes)}}
 
