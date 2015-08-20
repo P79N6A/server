dcb680b314c43a067786dd3294e8d714440b3020
anonymous <_@whats-your.name>
2015-08-20 12:29:30 +0000

    ok

--- conf/local.rb
+++ conf/local.rb
@@ -1,8 +1,8 @@
 # when placed at server-root, this will load on daemon-start
 class R
 
-  # mount man-handler
-  GET['/man'] = Man # on a path
-  GET['man.whats-your.name/'] # on a host
+  # mount man-handler                on
+  GET['/man'] = Man                 # path
+  GET['man.whats-your.name/'] = man # on a host
 
 end
