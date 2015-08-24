7e3f9bafe275678680ad822a64d3007ef9a19e85
anonymous <_@whats-your.name>
2015-08-24 02:26:50 +0000

    ok

--- conf/local.rb
+++ conf/local.rb
@@ -1,7 +1,10 @@
 class R
 
-  # mount man-handler                on
+  # man-handler                     on
   GET['/man'] = Man                 # path
   GET['man.whats-your.name/'] = Man # host
+
+  # inbox
+  GET['m.whats-your.name/'] = GET['/today']
 
 end
