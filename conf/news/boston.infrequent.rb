722e6417f445faec2a399a5ddf70eaef3ff6e9e7
anonymous <_@whats-your.name>
2015-09-03 15:23:56 +0000

    ok

--- conf/news/boston.infrequent.rb
+++ conf/news/boston.infrequent.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 require 'ww'
 host = ARGV[0]
- src = Pathname.new(File.expand_path File.dirname __FILE__).relative_path_from(Pathname.new `pwd`.chomp).to_s
-
+# relative path to feed list
+src = Pathname.new(File.expand_path File.dirname __FILE__).relative_path_from(Pathname.new `pwd`.chomp).to_s
+# ingest
 (src+'/boston.u').R.getFeeds host
