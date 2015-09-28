b7550089cc4271a232152a4d685efc37bf862520
anonymous <_@whats-your.name>
2015-09-28 00:27:39 +0000

    ok

--- ruby/search.fs.rb
+++ ruby/search.fs.rb
@@ -20,7 +20,7 @@ class R
       else
         cs = e.c # node children
         size = cs.size
-        if size < 86 # inline all the children
+        if size < 256 # inline all the children
           cs.map{|c|c.setEnv e.env}
           e.fileResources.concat cs
         else
