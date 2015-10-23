88caf692ca161048aacff53b9c29d218e5a609b1
anonymous <_@whats-your.name>
2015-10-23 20:27:44 +0000

    ok

--- conf/news/boston.rb
+++ conf/news/boston.rb
@@ -8,6 +8,6 @@ hostname = ARGV[0]
 'http://www.reddit.com/r/boston+dorchester+bikeboston+SouthShore+massachusetts+roxbury+eastie+Southie+QuincyMA+providence+cambridgeMA+somerville/new/.rss?sort=new'.R.getFeed hostname
 
 # twitter
-# find relative path to user-list
+# find relative path to user-list wherever this script is and you're running it from
 scriptDir = (Pathname.new File.expand_path File.dirname __FILE__).relative_path_from Pathname.new `pwd`.chomp
 R[scriptDir.to_s + '/boston.tw'].tw hostname
