898878406736562c579e949730b504a684b7e32c
anonymous <_@whats-your.name>
2015-11-24 06:30:36 +0000

    ok

--- ruby/message.mail.rb
+++ ruby/message.mail.rb
@@ -248,6 +248,72 @@ class R
     triplrCache :triplrMail, @r.do{|r|r.host}, [SIOC+'reply_of'], IndexMail, &f
   end
 
+  ViewGroup[SIOC+'ChatLog'] = ViewGroup[SIOC+'BlogPost'] =  ViewGroup[SIOC+'BoardPost'] = ViewGroup[SIOC+'MailMessage'] = -> d,e {
+    e[:arcs] = []
+    e[:timelabel] = {}
+    prior = {'uri' => '#'}
+    d.values.map{|s|
+      if s[SIOC+'has_parent'] # explicit parent
+        s[SIOC+'has_parent'].justArray.map{|o|
+          d[o.uri].do{|t| # arc target
+            sLabel = s[Creator].justArray[0].do{|c|c.R.fragment}
+            tLabel = t[Creator].justArray[0].do{|c|c.R.fragment}
+            e[:label][sLabel] = true
+            e[:label][tLabel] = true
+            arc = {source: s.uri, target: o.uri, sourceLabel: sLabel, targetLabel: tLabel}
+            s[Mtime].do{|mt| arc[:sourceTime] = mt[0]}
+            t[Mtime].do{|mt| arc[:targetTime] = mt[0]}
+            e[:arcs].push arc }}
+      end
+    }
+
+    # labels
+    (1..15).map{|depth| e[:label]["quote"+depth.to_s] = true}
+
+    # facet-filter properties
+    defaultFilter = e[:thread] ? Creator : 'sioc:addressed_to'
+    e.q['a'] ||= defaultFilter
+    e.q['reverse'] ||= true
+    timegraph = false
+
+    # HTML
+    [H.css('/css/message',true),
+     {class: :msgs,
+      c: [(d.values[0][Title].justArray[0].do{|t|
+             title = t.sub ReExpr, ''
+             {_: :h1, c: CGI.escapeHTML(title)}} if e[:thread]),
+          Facets[d,e], # resources in filterable wrappers
+          e[:Links][:next].do{|n|
+            {_: :a, href: n, c: '&#9660;', class: :nextPage}}]},
+     (#  max/min time-values
+      times = e[:arcs].map{|a|[a[:sourceTime],a[:targetTime]]}.
+              flatten.compact.map(&:to_f)
+      min = times.min || 0
+      max = times.max || 1
+      range = (max - min).min(0.1)
+
+      # scale times to range
+      e[:arcs].map{|a|
+        a[:sourcePos] = (a[:sourceTime].to_f - min) / range
+        a[:targetPos] = (a[:targetTime].to_f - min) / range
+        a.delete :sourceTime
+        a.delete :targetTime }
+      timegraph = e[:arcs].size > 1
+
+      e[:sidebar].push({id: :timegraph,
+                        c: {_: :svg,
+                            c: e[:timelabel].map{|l,_|
+                              pos = (max - l.to_time.to_f) / range * 100
+                              y = pos.to_s + '%'
+                              [{_: :line, stroke: '#333', 'stroke-dasharray' => '2,2', x1: 0, x2: '100%', y1: y, y2: y},
+                               {_: :text, 'font-size'  =>'.8em',c: l.sub('T',' '), dy: -3, x: 0, y: y}
+                              ]}}}) if timegraph
+
+      nil),
+     ([{_: :script, c: "var arcs = #{e[:arcs].to_json};"},
+       H.js('/js/d3.min'),
+       H.js('/js/timegraph',true)] if timegraph)]}
+
   ViewA[SIOC+'BlogPost'] = ViewA[SIOC+'BoardPost'] = ViewA[SIOC+'MailMessage'] = -> r,e {
     localPath = r.uri == r.R.path
     arc = {source: r.uri, target: r.uri, sourceLabel: r[Label], targetLabel: r[Label]}
@@ -280,10 +346,9 @@ class R
 
     # HTML
     {class: :mail, id: r.uri, href: href,
-     c: [(r[Title].justArray[0].do{|t|
-            {class: :title, c: {_: :a, class: :title, href: r.uri, c: CGI.escapeHTML(t)}}} unless e[:thread]),
-         {class: :header,
-          c: [r[To].justArray.map{|o|
+     c: [{class: :header,
+          c: [(r[Title].justArray[0].do{|t| {_: :a, class: :title, href: r.uri, c: CGI.escapeHTML(t)}} unless e[:thread]),
+              r[To].justArray.map{|o|
                 o = o.R
                 {_: :a, class: :to, href: localPath ? (o.dir+'?set=page') : o.uri, c: o.fragment || o.path || o.host}}.intersperse({_: :span, class: :sep, c: ','}),
               # reply-of (direct)
