04909ce3a42dac9111b2f1197232e7d236cd1d79
anonymous <_@whats-your.name>
2015-11-24 08:46:34 +0000

    ok

--- js/timegraph.js
+++ js/timegraph.js
@@ -1,9 +1,8 @@
-
 document.addEventListener("DOMContentLoaded", function(){
     var svg = d3.select("#timegraph > svg")
     var nodes = {};
-    var width = 600;
-    var height = 128;
+    var width = svg[0][0].clientWidth || 600;
+    var height = svg[0][0].clientHeight || 128;
     var center = width / 2;
     var targetCount = {};
     arcs.forEach(function(link) { // bind node-table and link data
@@ -11,12 +10,12 @@ document.addEventListener("DOMContentLoaded", function(){
 	link.source = nodes[link.source] || (
 	    nodes[link.source] = {uri: link.source,
 				  name: link.sourceLabel,
-				  pos: height - link.sourcePos * height,
+				  pos: link.sourcePos * width,
 				 });
 	link.target = nodes[link.target] || (
 	    nodes[link.target] = {uri: link.target,
 				  name: link.targetLabel,
-				  pos: height - link.targetPos * height,
+				  pos: link.targetPos * width,
 				 });
     });
     var force = d3.layout.force()
@@ -50,63 +49,17 @@ document.addEventListener("DOMContentLoaded", function(){
 	.attr('x',8).attr('y',3)
 	.attr("name", function(d) { return d.name; });
 
-    // URI -> item
-    var messages = {}
-    node.each(function(item,index){
-	messages[item.uri] = item;
-    });
-
     function tick() {
-	link.attr("x1", function(d) {
-	    return d.source.x + 2;
+	link.attr("y1", function(d) {
+	    return d.source.y;
 	})
-	    .attr("y1", function(d) { return (d.source.pos || 0); })
-	    .attr("x2", function(d) {
-		return d.target.x + 2;
+	    .attr("x1", function(d) { return (d.source.pos || 0); })
+	    .attr("y2", function(d) {
+		return d.target.y;
 	    })
-	    .attr("y2", function(d) { return (d.target.pos || 0); });
+	    .attr("x2", function(d) { return (d.target.pos || 0); });
 
-	node.attr("transform", function(d) { return "translate(" + d.x + "," + (d.pos || 0) + ")"; });
+	node.attr("transform", function(d) { return "translate(" + (d.pos || 0) + "," + d.y + ")"; });
     }
-
-    // create cursor
-    svg.append('rect').attr('width',16).attr('id','cursor').style('fill','#000').attr('height',3).attr('x',12).attr('y',height);
-    var cursor = svg.select('#cursor')[0][0];
-
-    window.addEventListener("hashchange",function(e){ // move cursor to current focus
-	var id = window.location.hash.slice(1);
-	var vz = messages[id] || messages[window.location.hash];
-	if(vz) {
-	    force.resume();
-	    vz.x = vz.px = center;
-	    cursor.setAttribute('y', vz.pos + 2);
-	}
-    });
-
-    // find nearest node to mouse/tap-point
-    function findNode(event) {
-	var y = null;
-	if (event.targetTouches) {
-	    y = event.targetTouches[0].offsetY;
-	} else {
-	    y = event.offsetY;
-	}
-	var found = null;
-	var distance = width;
-	node.each(function(item,index){
-	    var d = Math.abs(y - item.pos);
-	    if(d <= distance){
-		distance = d;
-		found = item;
-	    }
-	});
-	if(found)
-	    window.location.hash = found.uri;
-    }
-
-    var timegraph = document.getElementById('timegraph');
-    timegraph.addEventListener("mousemove",findNode);
-    timegraph.addEventListener("touchmove",findNode);
-    timegraph.addEventListener("click",findNode);
 
 }, false);
