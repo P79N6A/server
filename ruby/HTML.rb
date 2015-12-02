67e894647ae97addc5b11b361b7c26b2291e3733
anonymous <_@whats-your.name>
2015-12-02 19:57:36 +0000

    ok

--- ruby/HTML.rb
+++ ruby/HTML.rb
@@ -215,10 +215,11 @@ class R
          {class: :contents, c: TabularView[{container.uri => container},e,false,false]}]}}
 
   ViewGroup[Container] = -> g,e {
-    cur = g.delete e.uri # request container
+    cur = g.delete e.uri # main container at request-URI
+    color = R.randomColor
     [({class: :container,
-       c: [{class: :label, c: {_: :a, c: cur.R.basename, href: '?set=page', style: 'background-color:#00f;color:#fff'}, style: 'font-size: 2em;background-color:#00f'},
-           {class: :contents, style: 'padding: .3em;background-color:#00f;color:#fff',
+       c: [{class: :label, c: {_: :a, c: cur.R.basename, href: '?set=page', style: "background-color:#{color};color:#fff"}, style: "font-size: 2em;background-color:#{color}"},
+           {class: :contents, style: "padding: .3em;background-color:#{color};color:#fff",
             c: cur[LDP+'contains'].map{|c|
               g.delete(c.uri).do{|c|ViewA[Container][c,e]}}}]} if cur && cur[LDP+'contains']),
       g.map{|id,c|ViewA[Container][c,e]}]} # other containers
