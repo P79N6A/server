da03dd41715bd519e3c0def002ea31e75d44db8e
anonymous <_@whats-your.name>
2015-12-18 13:32:17 +0000

    ok

--- conf/news/boston.rb
+++ conf/news/boston.rb
@@ -5,7 +5,7 @@ hostname = ARGV[0]
 # blogs are in *.infrequent.rb
 
 # reddit
-'http://www.reddit.com/r/boston+dorchester+bikeboston+massachusetts+roxbury+eastie+Southie+QuincyMA+providence+cambridgeMA+somerville/new/.rss?sort=new'.R.getFeed hostname
+'http://www.reddit.com/r/boston+dorchester+bikeboston+massachusetts+roxbury+QuincyMA+providence/new/.rss?sort=new'.R.getFeed hostname
 
 # twitter
 # find relative path to user-list wherever this script is and you're running it from
