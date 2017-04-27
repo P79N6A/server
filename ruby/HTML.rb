833b2080d20cee3b2ff463ba4060215f326098d2
anonymous <_@whats-your.name>
2017-04-27 10:44:24 +0000

    ok

--- ruby/HTML.rb
+++ ruby/HTML.rb
@@ -195,7 +195,7 @@ class R
                       ({_: :a, class: title ? :title : (loc ? :this : :uri), href: href, c: CGI.escapeHTML(title ? title : (this.fragment||this.basename))} if title||basicResource),
                       (title ? '<br>' : ' '),
                       # links
-                      {class: :links,
+                      {class: l.has_key?(Content) ? :links : '',
                        c: [DC+'link', SIOC+'attachment', DC+'hasFormat'].map{|p|
                          l[p].justArray.sort_by(&:uri).map{|link|
                            [{_: :a, class: :link, id: e.selector, href: link.uri,c: CGI.escapeHTML(link.uri)},' ']}}},
