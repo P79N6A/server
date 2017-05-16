95692e88bae75c23085ebe802bec24f10f13e4d3
anonymous <_@whats-your.name>
2017-05-16 20:55:19 +0000

    ok

--- conf/news/boston.infrequent.rb
+++ conf/news/boston.infrequent.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 require 'ww'
-# find path to feed-list (in same dir as this script) from caller-location, and fetch enumerated feeds
+# locate feed-list relative to caller, and fetch enumerated feeds
 R[Pathname.new(File.expand_path File.dirname __FILE__).relative_path_from(Pathname.new `pwd`.chomp).to_s+'/boston.u'].fetchFeeds # fetch blogs
