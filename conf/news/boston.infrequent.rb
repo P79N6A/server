0a63404adb78322962de428626248a772dfbe25c
anonymous <_@whats-your.name>
2017-05-16 20:56:42 +0000

    ok

--- conf/news/boston.infrequent.rb
+++ conf/news/boston.infrequent.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 require 'ww'
-# locate feed-list relative to caller, and fetch enumerated feeds
-R[Pathname.new(File.expand_path File.dirname __FILE__).relative_path_from(Pathname.new `pwd`.chomp).to_s+'/boston.u'].fetchFeeds # fetch blogs
+# locate feed-list relative to caller, and fetch feeds
+R[Pathname.new(File.expand_path File.dirname __FILE__).relative_path_from(Pathname.new `pwd`.chomp).to_s+'/boston.u'].fetchFeeds
--- conf/news/boston.rb
+++ conf/news/boston.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby
 require 'ww'
 'https://www.reddit.com/r/boston+bikeboston+massachusetts/new/.rss?sort=new'.R.fetchFeed # fetch reddit
-# locate twitter-user list relative to caller, and fetch enumerated feeds
-R[Pathname.new(File.expand_path File.dirname __FILE__).relative_path_from(Pathname.new `pwd`.chomp).to_s+'/boston.tw'].tw # fetch twitter
+# locate twitter-user list relative to caller, and fetch tweets
+R[Pathname.new(File.expand_path File.dirname __FILE__).relative_path_from(Pathname.new `pwd`.chomp).to_s+'/boston.tw'].tw
