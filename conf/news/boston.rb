5385a0a1db61d2fd93a5c214ad13e8d223dd074f
anonymous <_@whats-your.name>
2017-05-16 20:57:47 +0000

    ok

--- conf/news/boston.rb
+++ conf/news/boston.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 require 'ww'
-'https://www.reddit.com/r/boston+bikeboston+massachusetts/new/.rss?sort=new'.R.fetchFeed # fetch reddit
+# fetch reddit
+'https://www.reddit.com/r/boston+bikeboston+massachusetts/new/.rss?sort=new'.R.fetchFeed
 # locate twitter-user list relative to caller, and fetch tweets
 R[Pathname.new(File.expand_path File.dirname __FILE__).relative_path_from(Pathname.new `pwd`.chomp).to_s+'/boston.tw'].tw
