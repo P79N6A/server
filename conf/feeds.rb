4506836973112725c39a1b0891103f9fabe28695
c <carmen@mw.logbook.am>
2017-06-16 14:55:02 +0000

    ok

--- conf/feeds.rb
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env ruby
-require 'ww'
-# locate feed-list relative to caller, and fetch feeds
-R[Pathname.new(File.expand_path File.dirname __FILE__).relative_path_from(Pathname.new `pwd`.chomp).to_s+'/feeds.u'].fetchFeeds
