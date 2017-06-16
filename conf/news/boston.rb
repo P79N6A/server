d4e13bfc7f0fb7209e4ff0e892cbdab2a1596766
carmen <carmen@mw.logbook.am>
2017-06-16 10:08:56 +0000

    ok

--- conf/news/boston.rb
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env ruby
-require 'ww'
-# fetch reddit
-'https://www.reddit.com/r/boston+bikeboston+massachusetts/new/.rss?sort=new'.R.fetchFeed
-# locate twitter-user list relative to caller, and fetch tweets
-R[Pathname.new(File.expand_path File.dirname __FILE__).relative_path_from(Pathname.new `pwd`.chomp).to_s+'/boston.tw'].tw
