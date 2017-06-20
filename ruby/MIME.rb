e9ef79060212cc60f435569cefa8c2c01a335678
c <carmen@mw.logbook.am>
2017-06-20 03:27:13 +0000

    ok

--- ruby/MIME.rb
+++ ruby/MIME.rb
@@ -156,6 +156,30 @@ class R
   def triplrImage &f
     yield uri, Type, R[Image]
   end
+=begin
+  GET['thumbnail'] = -> e {
+    thumb = nil
+    path = e.path.sub /^.thumbnail/, ''
+    i = R['//' + e.host + path]
+    i = R[path] unless i.file? && i.size > 0
+    if i.file? && i.size > 0
+      if i.ext.match /SVG/i
+        thumb = i
+      else
+        thumb = i.dir.child '.' + i.basename + '.png'
+        if !thumb.e
+          if i.mime.match(/^video/)
+            `ffmpegthumbnailer -s 360 -i #{i.sh} -o #{thumb.sh}`
+          else
+            `gm convert #{i.ext.match(/^jpg/) ? 'jpg:' : ''}#{i.sh} -thumbnail "360x360" #{thumb.sh}`
+          end
+        end
+      end
+      thumb && thumb.e && thumb.setEnv(e.env).fileGET || e.notfound
+    else
+      e.notfound
+    end}
+=end
 
   Icons = {
     'uri' => :id, Container => :dir, Content => :pencil, Date => :date, Label => :tag, Title => :title, Sound => :speaker, Image => :img, Size => :size, Mtime => :time, To => :user, Resource => :graph,
