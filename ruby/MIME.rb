c95b25ad3037cc0362a9dc1fc05f6d8855d80554
anonymous <_@whats-your.name>
2017-08-07 18:43:11 +0000

    ok

--- ruby/MIME.rb
+++ ruby/MIME.rb
@@ -195,10 +195,10 @@ class R
     yield s, Date, mt.iso8601
     # preview children using RDF and filesystem metadata
     dirs,files = children.partition{|e|e.node.directory?}
-    dirs.map{|d|yield d.uri + d.uri[-1]=='/' ? '' : '/', Type, R[Container]} # container in container. don't inline recursively here, emit a pointer
-    (R.load files.select &:e).map{|s,r| # leaf nodes. fetch RDF
+    dirs.map{|d| yield d.uri, Type, R[Container]} # another container. don't inline recursively, just emit a pointer
+    (R.load files.select &:e).map{|s,r| # leaf node. load data for listing
       types = r.types
-      unless types.member?(SIOC+'InstantMessage') || types.member?(SIOC+'Tweet') # node-types to drop. dropped classes emit summary-nodes, as seen in triplrChatLog
+      unless types.member?(SIOC+'InstantMessage') || types.member?(SIOC+'Tweet') # node-types to drop. dropped classes can emit summary-nodes, see triplrChatLog
         r.map{|p,o| o.justArray.map{|o| # visit triples
             yield s, p, o # emit summary triples for directory-data graph
           } unless [Content,'uri',DC+'hasFormat'].member? p} # arc types to drop
