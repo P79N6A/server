fa638a1c82c047552765ed2a03bb7cef2d5db2d6
anonymous <_@whats-your.name>
2017-09-27 14:12:27 +0000

    ok

--- ruby/HTML.rb
+++ ruby/HTML.rb
@@ -71,8 +71,6 @@ class R
     prevPage = e[:Links][:prev].do{|p|{_: :a, c: '&#9664;', rel: :prev, href: (CGI.escapeHTML p.to_s)}}
     nextPage = e[:Links][:next].do{|n|{_: :a, c: '&#9654;', rel: :next, href: (CGI.escapeHTML n.to_s)}}
     downPage = e[:Links][:down].do{|d|['<br clear=all>',{_: :a, c: '&#9660;', id: :Down, rel: :down, href: (CGI.escapeHTML d.to_s)}]}
-    images = graph.keys.grep /(jpg|png)$/i
-    template = images.size==graph.keys.size ? Gallery : TabularView
     H ["<!DOCTYPE html>\n",
        {_: :html,
         c: [{_: :head,
@@ -84,8 +82,8 @@ class R
                  e[:Links].do{|links|links.map{|type,uri| {_: :link, rel: type, href: CGI.escapeHTML(uri.to_s)}}},
                 ]},
             {_: :body,
-             c: [upPage, prevPage, nextPage, # page links
-                 template[graph,re],
+             c: [upPage, prevPage, nextPage, # page pointers
+                 TabularView[graph,re], # resources
                  ([{_: :style, c: "body {text-align:center;background-color:##{'%06x' % (rand 16777216)}}"},
                    {_: :span,style: 'font-size:12em;font-weight:bold',c: 404}] if graph.empty?),
                  ([prevPage,nextPage] if graph.keys.size > 8), downPage]}]}]}
@@ -121,20 +119,6 @@ class R
      {_: :style, c: ".focus, .focus a {background-color:##{'%06x' % (rand 16777216)};color:#fff;font-size:1.2em}\n"},
      {_: :style, c: e.env[:label].map{|name,_| "[name=\"#{name}\"] {color:#000;background-color: #{'#%06x' % (rand 16777216)}}\n"}},
      {_: :style, c: "[property=\"#{p}\"] {border-color:#999;border-style: solid; border-width: 0 0 .1em 0}"}]}
-
-  Gallery = -> graph,e,_=true {
-    [{_: :style, c: R['/css/photo.css'].readFile},
-     {_: :style, c: R['/css/misc/default-skin.css'].readFile},
-     {_: :script, c: R['/js/photoswipe.min.js'].readFile},
-     {_: :script, c: R['/js/photoswipe-ui.min.js'].readFile},
-     graph.keys.map{|i|
-       {_: :a, class: :thumb, id: 'a'+rand.to_s.sha2[0..7], href: i.R.basename, c: {_: :img, src: i.R.thumb.basename}}},
-     %q{<!-- Root element of PhotoSwipe. Must have class pswp. --> <div class="pswp" tabindex="-1" role="dialog" aria-hidden="true"> <!-- Background of PhotoSwipe.          It's a separate element as animating opacity is faster than rgba(). --> <div class="pswp__bg"></div> <!-- Slides wrapper with overflow:hidden. --> <div class="pswp__scroll-wrap"> <!-- Container that holds slides.             PhotoSwipe keeps only 3 of them in the DOM to save memory.             Don't modify these 3 pswp__item elements, data is added later on. --> <div class="pswp__container"> <div class="pswp__item"></div> <div class="pswp__item"></div> <div class="pswp__item"></div> </div> <!-- Default (PhotoSwipeUI_Default) interface on top of sliding area. Can be changed. --> <div class="pswp__ui pswp__ui--hidden"> <div class="pswp__top-bar"> <!--  Controls are self-explanatory. Order can be changed. --> <div class="pswp__counter"></div> <button class="pswp__button pswp__button--close" title="Close (Esc)"></button> <button class="pswp__button pswp__button--share" title="Share"></button> <button class="pswp__button pswp__button--fs" title="Toggle fullscreen"></button> <button class="pswp__button pswp__button--zoom" title="Zoom in/out"></button> <!-- Preloader demo http://codepen.io/dimsemenov/pen/yyBWoR --> <!-- element will get class pswp__preloader--active when preloader is running --> <div class="pswp__preloader"> <div class="pswp__preloader__icn"> <div class="pswp__preloader__cut"> <div class="pswp__preloader__donut"></div> </div> </div> </div> </div> <div class="pswp__share-modal pswp__share-modal--hidden pswp__single-tap"> <div class="pswp__share-tooltip"></div> </div> <button class="pswp__button pswp__button--arrow--left" title="Previous (arrow left)"> </button> <button class="pswp__button pswp__button--arrow--right" title="Next (arrow right)"> </button> <div class="pswp__caption"> <div class="pswp__caption__center"></div> </div> </div> </div> </div>},
-     {_: :script, c: "
-      var items = #{graph.keys.map{|k|{src: k.R.basename, msrc: k.R.thumb.basename, w: graph[k][Stat+'width'][0], h: graph[k][Stat+'height'][0]}}.to_json};
-      var gallery = new PhotoSwipe( document.querySelectorAll('.pswp')[0], PhotoSwipeUI_Default, items, {index: #{e.q['start']||0}});
-      gallery.init();
-"}]}
 
   TableRow = -> l,e,sort,direction,keys {
     #id 
