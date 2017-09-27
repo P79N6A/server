2c54c2586bb80178e567ca4cc32582e098d0482a
anonymous <_@whats-your.name>
2017-09-27 13:05:16 +0000

    ok

--- install.sh
+++ install.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
+# install
 cd ruby
 ruby install
 gem install bundler
 bundle config build.nokogiri --use-system-libraries
 bundle install
-cd ..
+# configure
+cd ../config
 mkdir ~/web
-ln -sr config/config.ru ~/web
-ln -sr config/site.css ~/web/.css
-ln -sr config/site.js ~/web/.js
+ln -sr config.ru ~/web
+ln -sr site.css ~/web/.css
+ln -sr site.js ~/web/.js
 pip install --upgrade pygments
