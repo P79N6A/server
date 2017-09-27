f882d76b965dbc225a162caeebbd747869d52b87
anonymous <_@whats-your.name>
2017-09-27 13:31:36 +0000

    ok

--- install.sh
+++ install.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 # install
 cd ruby
+echo "installing our library.."
 ruby install
+echo "installing dependencies.."
 gem install bundler
 bundle config build.nokogiri --use-system-libraries
 bundle install
 # configure
+echo "init server-root"
 cd ../config
 mkdir ~/web
 ln -sr config.ru ~/web
