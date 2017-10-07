f9a255ebbe63ff199c9a42c43b57fb71e376a8ce
anonymous <_@whats-your.name>
2017-10-07 13:30:12 +0000

    ok

--- ruby/ww.rb
+++ ruby/ww.rb
@@ -1,17 +1,25 @@
 # coding: utf-8
 # external dependencies
 %w{cgi csv date digest/sha2 dimensions fileutils json linkeddata mail open-uri pathname rack rdf redcarpet shellwords}.map{|r|require r}
-# this library
 class R < RDF::URI
   def R; self end
   def R.[] uri; R.new uri end
 end
+class RDF::URI
+  def R; R.new to_s end
+end
+class RDF::Node
+  def R; R.new to_s end
+end
+class Pathname
+  def R; R.fromPOSIX to_s.utf8 end
+end
 %w{MIME HTML HTTP}.map{|r|require_relative r}
-# extraclass methods for type-normalizing and conditional code
-# justArray: [a,r,r,a,y] for [a,r,r,a,y], [object] for object, [] for nil
-# R: Resource from anything with URI attribute
-# do: if arg exists yield it to a one-arg code-block
 class Array
+  # normalize input-types to array
+  # [a,r,y] -> [a,r,y]
+  #       a -> [a]
+  #     nil -> []
   def justArray; self end
 end
 class FalseClass
@@ -27,18 +35,10 @@ class NilClass
 end
 class Object
   def id; self end
+  # yield non-nil arg to block
   def do; yield self end
   def justArray; [self] end
   def to_time
     [Time, DateTime].member?(self.class) ? self : Time.parse(self)
   end
-end
-class RDF::URI
-  def R; R.new to_s end
-end
-class RDF::Node
-  def R; R.new to_s end
-end
-class Pathname
-  def R; R.fromPOSIX to_s.utf8 end
 end
