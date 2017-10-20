3d38c5425d6dfc1771d57e28694c33585a4776de
anonymous <_@whats-your.name>
2017-10-20 06:41:00 +0000

    ok

--- ruby/HTML.rb
+++ ruby/HTML.rb
@@ -122,12 +122,12 @@ class R
          {_: :tr, c: {_: :td, colspan: 12, style: 'font-size:1.6em', c: re.basename}}
           ]}
     when :month
-      days = graph.values.select{|r|r.R.basename.match /^[0-1][0-9]$/}.sort_by(&:uri)
+      days = graph.values.select{|r|r.R.path.match /\d{4}\/\d{2}\/\d{2}/}.sort_by(&:uri)
       {_: :table,
        c: [
          {_: :tr, c: days.map{|r|
             size = r[Size].justArray[0]
-            {_: :td, style: 'vertical-align:bottom', c: {onclick: "window.location.href = \"#{r.uri}\";", style: "background-color:green; width: 2em; height:#{size / 2.0}em"}}}},
+            {_: :td, style: 'vertical-align:bottom', c: {id: 'day'+r.R.basename,onclick: "window.location.href = this.getAttribute(\"href\");", href: r.uri, style: "background-color:green; width: 2em; height:#{size / 2.0}em"}}}},
          {_: :tr, c: days.map{|r|{_: :td, style: 'text-align: center', c: {_: :a, href: r.uri, c: r.R.basename}}}},
          {_: :tr, c: {_: :td, colspan: 31, style: 'font-size:1.6em', c: re.uri[1..-2]}}
           ]}
