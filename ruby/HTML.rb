11f5332595ef4a7c3b456b426c765efbca81ff26
anonymous <_@whats-your.name>
2017-10-26 12:25:40 +0000

    ok

--- ruby/HTML.rb
+++ ruby/HTML.rb
@@ -122,8 +122,8 @@ class R
       c: [{_: :tr, c: {_: :td, class: :time, colspan: config[:count],
                        c: [pathParts.map{|part|
                              path.push part
-                             [{_: :a, id: 'p_'+path.join.sha2,
-                               href: path.join('/'), c: part}, ' ']},
+                             [{_: :a, id: 'p_'+path.join.sha2, class: :range,
+                               href: path.join('/') + '/', c: part}, ' ']},
                          {_: :a, class: :clock, href: '/h', id: :uptothetime},
                           ]}},
           {_: :tr, c: segs.map{|r|
