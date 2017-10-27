570b76d845aa9f386f649339a923718a6124db2a
anonymous <_@whats-your.name>
2017-10-27 14:43:36 +0000

    ok

--- ruby/HTML.rb
+++ ruby/HTML.rb
@@ -214,7 +214,7 @@ class R
                q['ascending'] = ''
              end
              href = CGI.escapeHTML R.qs q
-             {_: :th,href: href,property: k,class: k==p ? 'selected' : '',c: {_: :a,href: href,class: Icons[k]||'',c: Icons[k] ? '' : (k.R.fragment||k.R.basename)}}}}]},
+             {_: :th, id: 'sort_'+k.sha2, href: href,property: k,class: k==p ? 'selected' : '',c: {_: :a,href: href,class: Icons[k]||'',c: Icons[k] ? '' : (k.R.fragment||k.R.basename)}}}}]},
      {_: :style, c: e.env[:label].map{|name,_| "[name=\"#{name}\"] {color:#000;background-color: #{'#%06x' % (rand 16777216)}}\n"}},
      {_: :style, c: "[property=\"#{p}\"] {border-color:#444;border-style: solid; border-width: 0 0 .08em 0}"}]}
 
