13a9cd864de9852d7fcab84602862bac1056846e
anonymous <_@whats-your.name>
2017-10-27 10:52:22 +0000

    ok

--- config/site.js
+++ config/site.js
@@ -32,54 +32,56 @@ document.addEventListener("DOMContentLoaded", function(){
     };
 
     // keyboard navigation
-    var selectNext = function(){
-	var cur = null;
-	if(window.location.hash)
-	    cur = document.querySelector(window.location.hash);
-	if(!cur)
-	    cur = last;
-	window.location.hash = cur.attr('next');
-    };
-    var selectPrev = function(){
-	var cur = null;
-	if(window.location.hash)
-	    cur = document.querySelector(window.location.hash);
-	if(!cur)
-	    cur = first;
-	window.location.hash = cur.attr('prev');;
-    };
-    var gotoSelection = function(){
-	if(window.location.hash){
-	    // element
-	    cur = document.querySelector(window.location.hash);
-	    if(cur){
-		// location
-		href = cur.attr('href');
-		// go
-		if(href)
-		    window.location = href;
-	    };
-	};
-    };
     document.addEventListener("keydown",function(e){
-	var jumpDoc = function(direction) {
+	var key = e.keyCode;
+	var selectNext = function(){
+	    var cur = null;
+	    if(window.location.hash)
+		cur = document.querySelector(window.location.hash);
+	    if(!cur)
+		cur = last;
+	    window.location.hash = cur.attr('next');
+	    e.preventDefault();
+	};
+	var selectPrev = function(){
+	    var cur = null;
+	    if(window.location.hash)
+		cur = document.querySelector(window.location.hash);
+	    if(!cur)
+		cur = first;
+	    window.location.hash = cur.attr('prev');;
+	    e.preventDefault();
+	};
+	var gotoLink = function(direction) {
 	    var doc = document.querySelector("head > link[rel='"+direction+"']");
 	    if(doc)
 		window.location = doc.getAttribute('href');
 	};
-	var key = e.keyCode;
-//	console.log(key);
+	var gotoHref = function(){
+	    if(window.location.hash){
+		cur = document.querySelector(window.location.hash);
+		if(cur){
+		    href = cur.attr('href');
+		    if(href)
+			window.location = href;
+		};
+	    };
+	};
 	if(e.getModifierState("Shift")) {
 	    if(key==80) // [p]rev page
-		jumpDoc('prev');
+		gotoLink('prev');
 	    if(key==78) // [n]ext page
-		jumpDoc('next');
+		gotoLink('next');
+	} else {
+	    if(key==38||key==78) // [up] [p]revious element
+		selectPrev();
+	    if(key==40||key==80) // [down] [n]ext element
+		selectNext();
+	    if(key==13) // [enter] goto href
+		gotoHref();
 	};
-	if(key==78) // [up] previous element
-	    selectPrev();
-	if(key==80) // [down] next element
-	    selectNext();
-	if(key==13) // [enter] select
-	    gotoSelection();
+    },false);
+    document.querySelector('form > input').addEventListener("keydown",function(e){
+	e.stopPropagation();
     },false);
 }, false);
