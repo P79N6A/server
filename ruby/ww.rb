060c53874d98e9de74afb0306cd7d8923ff4da1e
anonymous <_@whats-your.name>
2017-11-03 20:03:05 +0000

    ok

--- ruby/ww.rb
+++ ruby/ww.rb
@@ -1,6 +1,5 @@
 # coding: utf-8
 %w{cgi csv date digest/sha2 dimensions fileutils json linkeddata mail open-uri pathname rack rdf redcarpet shellwords}.map{|r|require r} # external dependencies
-
 class R < RDF::URI
   def R; self end
   def R.[] uri; R.new uri end
@@ -9,16 +8,13 @@ end
 class RDF::URI
   def R; R.new to_s end
 end
-
 class RDF::Node
   def R; R.new to_s end
 end
-
 class Hash
   def R; R.new self["uri"] end
   def uri; self["uri"] end
 end
-
 class Pathname
   def R; R.fromPOSIX to_s.utf8 end
 end
@@ -26,27 +22,37 @@ end
 %w{MIME HTML HTTP}.map{|r|require_relative r}
 
 class Array
-  # normalize input-types to array
-  # [a,r,y] -> [a,r,y]
-  #       a -> [a]
-  #     nil -> []
+  #       [a] -> [a]
   def justArray; self end
 end
-
 class FalseClass
   def do; self end
 end
-
 class NilClass
-  def do; self end
+  #       nil -> []
   def justArray; [] end
+  def do; self end
 end
-
 class Object
+  #         a -> [a]
+  def justArray; [self] end
   def id; self end
   def do; yield self end
-  def justArray; [self] end
-  def to_time
-    [Time, DateTime].member?(self.class) ? self : Time.parse(self)
-  end
+  def to_time; [Time, DateTime].member?(self.class) ? self : Time.parse(self) end
 end
+=begin
+  the "monkeypatching" has been pared down.."Everything is a resource", call #R to return an abstract resource of class R
+  #do passes object as first arg to a block of code
+  add R to PATH to call resource methods in a shell context
+  example:
+
+ ~ R https://readwrite.com feeds
+https://readwrite.com/feed/
+https://readwrite.com/comments/feed/
+
+ ~/web R https://readwrite.com/feed/ getFeed
+/2017/11/02/16/56:05.readwrite.com.2017.11.02.iphone.x.coming.ar.apps.booming
+/2017/11/02/12/34:44.readwrite.com.2017.11.02.farm.fridge.iiot.love.story
+/2017/10/25/17/54:01.readwrite.com.2017.10.25.consider.expansion.shenzhen
+
+=end
