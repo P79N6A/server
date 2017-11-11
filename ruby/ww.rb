81ad7e223f913cf776140d567c798ca8df0de58e
anonymous <_@whats-your.name>
2017-11-11 23:25:39 +0000

    ok

--- ruby/ww.rb
+++ ruby/ww.rb
@@ -1,12 +1,17 @@
 # coding: utf-8
 %w{cgi csv date digest/sha2 dimensions fileutils json linkeddata mail nokogiri open-uri pathname rack rdf redcarpet shellwords}.map{|r|require r}
+# derive resource class
 class R < RDF::URI
   def R; self end
   def R.[] uri; R.new uri end
 end
+# now the rest of the library can reopen R
 %w{MIME HTML HTTP}.map{|r|require_relative r}
+# #justArray converts to one-element array for expressions applicable to multiple objects. eliminates mandatory [] wrapping of single RDF-JSON object
+# #R normalizes any type identifiable with a URI to our abstract resource
+# #do passes object to block-arg. Kernel#yield_self in Ruby 2.5 may be faster than "yield self", TODO investigate once widely deployed
 class Array
-  #       [a] -> [a]
+  # [a] -> [a]
   def justArray; self end
   def intersperse i; inject([]){|a,b|a << b << i}[0..-2] end
 end
@@ -19,12 +24,12 @@ class Hash
   def types; self[R::Type].justArray.select{|t|t.respond_to? :uri}.map &:uri end
 end
 class NilClass
-  #       nil -> []
+  # nil -> []
   def justArray; [] end
   def do; self end
 end
 class Object
-  #         a -> [a]
+  # a -> [a]
   def justArray; [self] end
   def id; self end
   def do; yield self end
@@ -39,17 +44,3 @@ end
 class RDF::URI
   def R; R.new to_s end
 end
-=begin
-  "Everything is a resource". call #R to return an abstract resource of class R
-  to call resource-methods from a shell environment add R to PATH:
-
- ~ R https://readwrite.com feeds
-https://readwrite.com/feed/
-https://readwrite.com/comments/feed/
-
- ~/web R https://readwrite.com/feed/ getFeed
-/2017/11/02/16/56:05.readwrite.com.2017.11.02.iphone.x.coming.ar.apps.booming
-/2017/11/02/12/34:44.readwrite.com.2017.11.02.farm.fridge.iiot.love.story
-/2017/10/25/17/54:01.readwrite.com.2017.10.25.consider.expansion.shenzhen
-
-=end
