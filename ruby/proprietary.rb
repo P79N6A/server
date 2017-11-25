661158fa721408e60303bff03f1ba1ef1e63110e
anonymous <_@whats-your.name>
2017-11-25 01:47:30 +0000

    okp

--- ruby/proprietary.rb
+++ ruby/proprietary.rb
@@ -39,7 +39,6 @@ class R
   def twitter
     open(pathPOSIX).readlines.map(&:chomp).shuffle.each_slice(16){|s|
       readURI = Twitter + '/search?f=tweets&vertical=default&q=' + s.map{|u|'from:'+u.chomp}.intersperse('+OR+').join
-      puts readURI
       readURI.R.indexTweets}
   end
 
