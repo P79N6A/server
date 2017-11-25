3ed96d8ed5bf7576787e8ac076dfa8bfe8d5f907
anonymous <_@whats-your.name>
2017-11-25 01:15:18 +0000

    ok

--- ruby/HTML.rb
+++ ruby/HTML.rb
@@ -36,7 +36,6 @@ class R
     if q = re.q['q']
       Grep[graph,q]
     end
-    foot = [{_: :style, c: "body {text-align:center;background-color:##{'%06x' % (rand 16777216)}}"}, {_: :span,style: 'font-size:12em;font-weight:bold',c: 404}, (CGI.escapeHTML e['HTTP_USER_AGENT'])] if graph.empty?
     H ["<!DOCTYPE html>\n",
        {_: :html,
         c: [{_: :head,
@@ -51,8 +50,10 @@ class R
                  (Table[graph,re] unless graph.empty?),
                  {_: :style, c: e[:label].map{|name,_|
                     "[name=\"#{name}\"] {color:#000;background-color: #{'#%06x' % (rand 16777216)}}\n"}},
-                 !graph.empty? && e[:Links][:down].do{|d|{_: :a, id: :down, c: '&#9660;', href: (CGI.escapeHTML d.to_s)}},
-                 foot]}]}]}
+                 !graph.empty? && e[:Links][:down].do{|d|
+                   {_: :a, id: :down, c: '&#9660;', href: (CGI.escapeHTML d.to_s)}},
+                 graph.empty? && [{_: :style, c: "body {text-align:center;background-color:##{'%06x' % (rand 16777216)}}"},
+                                  {_: :span,style: 'font-size:12em;font-weight:bold',c: 404}, (CGI.escapeHTML e['HTTP_USER_AGENT'])]]}]}]}
 
   Search = -> graph,re {
     parts = re.path.split '/'
