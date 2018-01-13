6f1de5a9894eb7e33ddc7ccc55052ba3aea628b7
carmen <carmen@mw.logbook.am>
2018-01-13 04:23:34 +0000

    ok

--- ruby/Visual.rb
+++ ruby/Visual.rb
@@ -15,29 +15,29 @@ class WebResource
         label = 'p'+path.sha2 if nodes.size > 1
         @r[:label][label] = true if label
         tabled = nodes.size < 36
-        size = 0.0
+        sizes = []
         # scale
         nodes.map{|name|
           uri = path + name + '/'
           graph[uri].do{|r|
             r[Size].justArray.map{|sz|
-              size += sz}}} if label
+              sizes.push sz}}} if label
+        maxSize = sizes.max.to_f
         # output
         {_: tabled ? :table : :div, class: :tree, c: [
            {_: tabled ? :tr : :div, class: :nodes, c: nodes.map{|name| # nodes
               this = path + name + '/' # path
               s = graph[this].do{|r|r[Size].justArray[0]} # size
               named = !name.empty?
-              scaled = size > 0 && s && tabled
-              width = scaled && (s / size) # scale
-              {_: tabled ? :td : :div, name: label,
-               style: scaled ? "width:#{width * 100.0}%" : '',
-               c: named ? {_: :a, href: this + q, name: label,
+              scaled = sizes.size > 0 && s && tabled
+              height = scaled && (s / maxSize) # scale
+              {_: tabled ? :td : :div, style: scaled ? 'height: 8em' : '',
+               c: named ? {_: :a, href: this + q, name: label, style: scaled ? "height:#{height * 100.0}%" : '',
                            c: CGI.escapeHTML(URI.unescape name) + (scaled ? '' : '/')} : ''}}.intersperse("\n")},"\n",
            ({_: tabled ? :tr : :div, c: nodes.map{|k| # children
               {_: tabled ? :td : :div, c: (render[t[k], path+k+'/'] if t[k].size > 0)}}.intersperse("\n")} unless !nodes.find{|n|t[n].size > 0})]}}
 
-      # render tree
+      # render
       render[tree]
     end
   end
