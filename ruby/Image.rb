085fbea690b39d480204ef583108ef8492a68573
anonymous <_@whats-your.name>
2018-01-31 13:41:33 +0000

    ok

--- ruby/Image.rb
+++ ruby/Image.rb
@@ -14,4 +14,35 @@ class WebResource
         R[Instagram+ig].indexInstagram}
     end
   end
+  module HTML
+    def tableCellPhoto
+      # scan RDF for not-yet-shown resourcs
+      images = []
+      images.push self if types.member?(Image) # subject of triple
+      self[Image].do{|i|images.concat i}        # object of triple
+      images.map(&:R).select{|i|!@r[:images].member? i}.map{|img| # unvisited
+        link = (@r['REQUEST_PATH'] == path) ? img.uri : uri # link to original context, and then to target image
+        @r[:images].push img                     # visit
+        [{_: :a, class: :thumb, href: link,      # link
+          c: {_: :img, class: :thumb, src: if !img.host || img.host == @r['HTTP_HOST'] # thumbnail if locally-hosted
+               img.path + '?preview'
+             else
+               img.uri
+              end}},'<br>',
+         {_: :a, href: img.uri, c: [{_: :span, class: :host, c: img.host},
+                                    {_: :span, class: :notes, c: (CGI.escapeHTML img.path)}]}]}
+    end
+
+    def tableCellVideo
+      self[Video].map(&:R).map{|video|
+        if video.match /youtu/
+          id = video.q(false)['v'] || video.parts[-1]
+          {_: :iframe, width: 560, height: 315, src: "https://www.youtube.com/embed/#{id}", frameborder: 0, gesture: "media", allow: "encrypted-media", allowfullscreen: :true}
+        else
+          {class: :video,
+           c: [{_: :video, src: video.uri, controls: :true}, '<br>',
+               {_: :span, class: :notes, c: video.basename}]}
+        end}
+    end
+  end
 end
