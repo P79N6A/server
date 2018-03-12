43f720dd14f9986cfcf2810dfe4fccf0b485d17c
anonymous <_@whats-your.name>
2018-03-12 09:36:05 +0000

    p

--- ruby/Tree.rb
+++ ruby/Tree.rb
@@ -6,7 +6,9 @@ class WebResource
       tree = {}
       graph.keys.select{|k|!k.R.host && k[-1]=='/'}.map{|uri|
         c = tree # start at root
-        uri.R.parts.map{|name| # path instructions
+        uri.R.parts.map{|dir|
+          dir.split '-'
+        }.flatten.map{|name| # path instructions
           c = c[name] ||= {}}} # create node and jump cursor
 #     render
       HTML.kv tree
