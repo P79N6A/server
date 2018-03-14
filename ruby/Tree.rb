f25160e62e566e6c806356e97ef5f0cf4ed2b5f2
anonymous <_@whats-your.name>
2018-03-14 22:15:13 +0000

    p

--- ruby/Tree.rb
+++ ruby/Tree.rb
@@ -1,5 +1,28 @@
 class WebResource
   module HTML
+
+    def self.kv hash
+     hash.map{|k,vs|
+       {class: :kv,
+        c: [{class: :k, c: {_: :span, class: :label, c: k}},
+            {class: :v,
+             c: ["\n ",
+                 vs.justArray.map{|v|
+                   c = v.class
+                   if c == Hash
+                     kv v # another kv hash
+                   elsif c == TrueClass
+                     {_: :a, class: :check}
+                   elsif c == FalseClass
+                     {_: :a, class: :ban}
+                   elsif !([Fixnum,String].member? c)
+                     {_: :a, class: :cog, c: c}
+                   else
+                     CGI.escapeHTML v.to_s
+                   end
+                 }.intersperse(' ')]}]}}
+    end
+
     def htmlTree graph
       query = qs
 #     construct
