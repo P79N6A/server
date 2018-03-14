271ab8c6e80d7aa7c568519fb5a22d9ecbedcd54
anonymous <_@whats-your.name>
2018-03-14 22:54:40 +0000

    p

--- ruby/Tree.rb
+++ ruby/Tree.rb
@@ -24,16 +24,15 @@ class WebResource
     end
 
     def htmlTree graph
-      query = qs
-#     construct
       tree = {}
+      query = qs
       graph.keys.select{|k|!k.R.host && k[-1]=='/'}.map{|uri|
         c = tree # start at root
         uri.R.parts.map{|dir|
           dir.split '-'
         }.flatten.map{|name| # path instructions
           c = c[name] ||= {}}} # create node and jump cursor
-#     render
+
       HTML.kv tree
     end
   end
