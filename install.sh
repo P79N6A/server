864c766c6dca6f2b185d94455f3fbfc9017a3012
anonymous <_@whats-your.name>
2018-03-22 07:44:46 +0000

    p

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
-conf=~/web/.conf
-mkdir -p $conf
-rsync -av config/ $conf
-pip install --upgrade pygments
+# pip install --upgrade pygments
+# gem install bundler
+
+# conf=~/web/.conf
+# mkdir -p $conf
+# rsync -av config/ $conf
+
 cd ruby
 ruby install
-gem install bundler
 bundle config build.nokogiri --use-system-libraries
 bundle install
