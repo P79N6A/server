ab659d118f5a7d8984ebba14851f5a3a7e0a037b
rev <_@whats-your.name>
2018-04-30 08:17:27 +0000

    p

--- config/spin.sh
+++ config/spin.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 while true
 do
-    R .conf/hyper.u fetchFeeds
-    R .conf/twitter.com.bu twitter
+    r .conf/hyper.u fetchFeeds
+    r .conf/twitter.com.bu twitter
     date
     sleep 30m
 done
