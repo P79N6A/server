9b1ffc8e8b8d20161998843e569639e2a6fef695
rev <_@whats-your.name>
2018-06-04 07:51:57 +0000

    p

--- install.sh
+++ install.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 
-# install this
+# this
 cd ruby
 ruby install
 
-# install dependencies
+# dependencies
+
+# apt install  graphicsmagick git ruby pkg-config ruby-dev libssl-dev libxml2-dev libxslt1-dev
+# pkg install  graphicsmagick git ruby pkg-config ruby-dev autoconf automake binutils clang grep file findutils libxslt-dev
+# pacman -S    graphicsmagick git ruby base-devel python-pip libxml2 libxslt
+# xbps-install GraphicsMagick git ruby base-devel ruby-devel libxml2-devel libxslt-devel
+
 pip install --upgrade pygments
 gem install bundler
 bundle config build.nokogiri --use-system-libraries
