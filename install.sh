4b0e5992a2f3440a2eee8b5735b085d241b80131
author <dx@mw.logbook.am>
2018-06-21 19:39:12 +0000

    p

--- install.sh
+++ install.sh
@@ -1,16 +1,9 @@
 #!/bin/sh
-
-# this
+# apt install  graphicsmagick git ruby pkg-config ruby-dev libssl-dev libxml2-dev libxslt1-dev
+# pkg install  graphicsmagick git ruby pkg-config ruby-dev grep file findutils libxslt-dev
+# pacman -S    graphicsmagick git ruby base-devel python-pip libxml2 libxslt
 cd ruby
 ruby install
-
-# dependencies
-
-# apt install  graphicsmagick git ruby pkg-config ruby-dev libssl-dev libxml2-dev libxslt1-dev
-# pkg install  graphicsmagick git ruby pkg-config ruby-dev autoconf automake binutils clang grep file findutils libxslt-dev
-# pacman -S    graphicsmagick git ruby base-devel python-pip libxml2 libxslt
-# xbps-install GraphicsMagick git ruby base-devel ruby-devel libxml2-devel libxslt-devel
-
 pip install --upgrade pygments
 gem install bundler
 bundle config build.nokogiri --use-system-libraries
