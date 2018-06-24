ad8235589a02ac2e5c5517634a9595d7b111e428
author <dx@mw.logbook.am>
2018-06-24 15:42:03 +0000

    p

--- install.sh
+++ install.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
-
 # apt install graphicsmagick git ruby ruby-dev libssl-dev libxml2-dev libxslt1-dev
 # pkg install graphicsmagick git ruby ruby-dev grep file findutils libxslt-dev
 # pacman -S   graphicsmagick git ruby python-pip libxml2 libxslt
-
 cd ruby
 ruby install
-pip install --upgrade pygments
 gem install bundler
-bundle config build.nokogiri --use-system-libraries
+pip install --upgrade pygments
 bundle install
