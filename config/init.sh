6ba0b222ac8ba87cc966122ff48e28b037acb0e1
author <c@mw.logbook.am>
2018-07-14 03:03:40 +0000

    delete init.sh, not necessary in termux era

--- config/init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-iptables -t nat -A OUTPUT -p tcp -m owner ! --uid-owner 10095 --dport 443 -j REDIRECT --to-ports 8888
-mount /dev/block/sda2 /data/mnt && cd /data/mnt
-mount -o bind /dev dev
-mount -o bind /dev/pts dev/pts
-mount -o bind /proc proc
-mount -o bind /sys sys
-chroot . /usr/sbin/sshd
