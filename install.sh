342e0d41be088df204976961caf836664883ab2a
author <c@mw.logbook.am>
2018-07-14 11:31:10 +0000

    p

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # apt install graphicsmagick git ruby ruby-dev libssl-dev libxml2-dev libxslt1-dev
-# pkg install graphicsmagick git ruby ruby-dev grep file findutils libxslt-dev
+# pkg install graphicsmagick git ruby ruby-dev grep file findutils pkg-config libxslt-dev
 # pacman -S   graphicsmagick git ruby python-pip libxml2 libxslt
 cd ruby
 ruby install
