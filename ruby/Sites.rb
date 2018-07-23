1a71d1d844c4859c9965c15389232475685d162e
author <c@mw.logbook.am>
2018-07-23 05:15:22 +0000

    p

--- ruby/Sites.rb
+++ ruby/Sites.rb
@@ -2,10 +2,9 @@ class WebResource
   module HTTP
     #host bindings
 
-    # look for app in open-source store 
+    # look for app in FDroid
     Host['play.google.com'] = -> re {
-      [302,
-       {'Location' => "https://f-droid.org/en/packages/#{re.q['id']}/"},[]]}
+      [302,{'Location' => "https://f-droid.org/en/packages/#{re.q['id']}/"},[]]}
 
     # direct to preferred search & maps providers
     Host['www.google.com'] = -> re {
@@ -21,37 +20,45 @@ class WebResource
               else
                 'https://www.openstreetmap.org/'
               end
-        [302,
-         {'Location' => loc},[]]
+        [302,{'Location' => loc},[]]
       when 'search'
         loc = if re.q.has_key? 'q'
                 "https://duckduckgo.com/?q=#{URI.escape re.q['q']}"
               else
                 'https://duckduckgo.com'
               end
-        [302,
-         {'Location' => loc},[]]
+        [302,{'Location' => loc},[]]
       else
         [404,{},[]]
       end}
 
-    # original URL on file with third-party
+    # original URL on file at third-party - network lookup
+
     %w{t.co bit.ly buff.ly bos.gl w.bos.gl dlvr.it ift.tt cfl.re nyti.ms trib.al ow.ly n.pr a.co youtu.be}.map{|host|
       Host[host] = Short}
 
-    # URI is encoded in another URI
+    # URI encoded in another URI - no network lookup
+
     Host['exit.sc'] = Unwrap[:url]
     Host['lookup.t-mobile.com'] = Unwrap[:origURL]
     Host['l.instagram.com'] = Host['images.duckduckgo.com'] = Host['proxy.duckduckgo.com'] = Unwrap[:u]
 
     # CSS and fonts
+
     %w{fonts.googleapis.com fonts.gstatic.com use.typekit.net}.map{|host|
       Host[host] = Font}
 
     # image hosts
+
     Host['snag.gy'] = -> re {
-        [302,
-         {'Location' => '//i.snag.gy'+re.path},[]]}
+        [302,{'Location' => '//i.snag.gy'+re.path},[]]}
+
+    WrappedImage = -> re {
+      img = R['https://'+re.env['HTTP_HOST']+re.path].nokogiri.css('[property="og:image"]')
+      [302,{'Location' => img.attr('content').to_s},[]]
+    }
+
+    Host['imgur.com'] = WrappedImage
 
     Host['twitter.com'] = Host['www.twitter.com'] = -> re {
       [200,{},[]]
