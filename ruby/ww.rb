e16500ba2f43f260d3adaed99c5165acb0978e05
author <c@mw.logbook.am>
2018-12-09 04:37:39 +0000

    p

--- ruby/ww.rb
+++ ruby/ww.rb
@@ -1,32 +1,37 @@
+
 # dependencies
 %w{cgi csv date digest/sha2 dimensions fileutils icalendar json linkeddata mail nokogiri open-uri pathname rack rdf redcarpet shellwords}.map{|r|require r}
+
 # this library
-%w{URI MIME POSIX HTTP HTML JSON RDF Cache Calendar Chat Container Feed Image Mail Post Proxy Search Style Text Time}.map{|i|require_relative i}
-# shorthand alias
+%w{URI HTML HTTP JSON MIME POSIX RDF Cache Calendar Chat Container Feed Image Mail Post Proxy Search Style Text Time}.map{|i|require_relative i}
+
+# class name shorthand
 R = WebResource
-# extend stdlib
+
+# extensions to stdlib classes
 class Array
   # already an array
   def justArray; self end
   def intersperse i; inject([]){|a,b|a << b << i}[0..-2] end
 end
+
 class Object
-  # obj -> [obj]
+  # objA -> [objA]
   def justArray; [self] end
-  # identity function
-  def id; self end
   # arg exists, run block
   def do; yield self end
   # cast to Time object
   def to_time; [Time, DateTime].member?(self.class) ? self : Time.parse(self) end
 end
+
 class FalseClass
-  # arg false, don't do block
+  # return false. no yield, no block execution
   def do; self end
 end
+
 class NilClass
   # empty array
   def justArray; [] end
-  # arg missing, don't do block
+  # null argument: no yield, no block execution
   def do; self end
 end
